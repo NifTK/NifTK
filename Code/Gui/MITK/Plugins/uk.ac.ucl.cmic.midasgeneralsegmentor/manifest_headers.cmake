f4133d16fdd2bd262ac119c5bfb8c266a0030400
Matt Clarkson <m.clarkson@ucl.ac.uk>
2011-12-23 08:48:34 +0000

    #1202, user manual

--- Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.midasgeneralsegmentor/manifest_headers.cmake
+++ Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.midasgeneralsegmentor/manifest_headers.cmake
@@ -1,4 +1,4 @@
-set(Plugin-Name "MIDAS Irregular Volume Editor")
+set(Plugin-Name "CMIC MIDAS Irregular Volume Editor")
 set(Plugin-Version "0.1")
 set(Plugin-Vendor "CMIC, Centre For Medical Image Computing, UCL.")
 set(Plugin-ContactAddress "http://cmic.cs.ucl.ac.uk/")
