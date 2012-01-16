9f29f37ce65368e52baa6ee2cfac18c2c2557028
Matt Clarkson <m.clarkson@ucl.ac.uk>
2012-01-16 11:30:32 +0000

    #1245, tweak to make OpenCV optional

--- Utilities/NifTKUnixBuild.sh
+++ Utilities/NifTKUnixBuild.sh
@@ -36,11 +36,11 @@ function run_command()
   fi
 }
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 5 ]; then
   echo "Simple bash script to run a full automated build. "
   echo "Does a two pass checkout. It checks out NifTK at the time you run it, then does a svn update with proper revision ID etc. to run unit tests"
   echo "Assumes svn, git, qt, cmake, svn credentials, valgrind, in fact everything are already present and valid in the current shell."
-  echo "Usage: UnixBuild.sh [Debug|Release] <number_of_threads> [ON|OFF to control coverage] [ON|OFF to control valgrind]"
+  echo "Usage: UnixBuild.sh [Debug|Release] <number_of_threads> [ON|OFF to control coverage] [ON|OFF to control valgrind] [ON|OFF to build OpenCV]"
   exit -1
 fi
 
@@ -48,6 +48,7 @@ TYPE=$1
 THREADS=$2
 COVERAGE=$3
 MEMCHECK=$4
+OPENCV=$5
 
 if [ "${TYPE}" != "Debug" -a "${TYPE}" != "Release" ]; then
   echo "First argument after UnixBuild.sh must be either Debug or Release."
@@ -70,6 +71,10 @@ if [ "${COVERAGE}" = "ON" ]; then
   COVERAGE_ARG="-DNIFTK_CHECK_COVERAGE=ON"
 fi
 
+if [ "${OPENCV} = "ON" ]; then
+  OPENCV_ARG="-DBUILD_OPENCV=ON"
+fi
+
 if [ "${MEMCHECK}" = "ON" ]; then
   BUILD_COMMAND="make clean ; ctest -D NightlyStart ; ctest -D NightlyUpdate ; ctest -D NightlyConfigure ; ctest -D NightlyBuild ; ctest -D NightlyTest ; ctest -D NightlyCoverage ; ctest -D NightlyMemCheck ; ctest -D NightlySubmit"
 else
@@ -79,7 +84,7 @@ fi
 run_command "svn co https://cmicdev.cs.ucl.ac.uk/svn/cmic/trunk/NifTK --non-interactive"
 run_command "mkdir ${FOLDER}"
 run_command "cd ${FOLDER}"
-run_command "cmake ../NifTK -DCMAKE_BUILD_TYPE=${TYPE} -DBUILD_GUI=ON -DBUILD_TESTING=ON -DBUILD_COMMAND_LINE_PROGRAMS=ON -DBUILD_COMMAND_LINE_SCRIPTS=ON -DBUILD_NIFTYLINK=ON -DBUILD_OPENCV=ON -DBUILD_OPENIGTLINK=ON -DNIFTK_GENERATE_DOXYGEN_HELP=ON ${COVERAGE_ARG}"
+run_command "cmake ../NifTK -DCMAKE_BUILD_TYPE=${TYPE} -DBUILD_GUI=ON -DBUILD_TESTING=ON -DBUILD_COMMAND_LINE_PROGRAMS=ON -DBUILD_COMMAND_LINE_SCRIPTS=ON -DBUILD_NIFTYLINK=ON -DBUILD_OPENIGTLINK=ON -DNIFTK_GENERATE_DOXYGEN_HELP=ON ${COVERAGE_ARG} ${OPENCV_ARG}"
 run_command "make -j ${THREADS}"
 run_command "cd NifTK-build"
 run_command "${BUILD_COMMAND} OFF"  # The submit task fails due to http timeout, so we want to carry on regardles.
