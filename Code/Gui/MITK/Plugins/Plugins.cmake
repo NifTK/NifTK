0f5c23107a90890abe671513bb33f9bb5de4a465
Matt Clarkson <m.clarkson@ucl.ac.uk>
2012-02-14 12:49:14 +0000

    #1309, turn off MIDAS General Segmentor for now

--- Code/Gui/MITK/Plugins/Plugins.cmake
+++ Code/Gui/MITK/Plugins/Plugins.cmake
@@ -1,11 +1,11 @@
 set(PROJECT_PLUGINS
-  Plugins/uk.ac.ucl.cmic.gui.qt.niftyview:OFF
+  Plugins/uk.ac.ucl.cmic.midasgeneralsegmentor:OFF
   Plugins/uk.ac.ucl.cmic.niftyreg:OFF
   Plugins/uk.ac.ucl.cmic.niftyseg:OFF
   Plugins/uk.ac.ucl.cmic.gui.qt.common:ON
+  Plugins/uk.ac.ucl.cmic.gui.qt.niftyview:ON
   Plugins/uk.ac.ucl.cmic.imagelookuptables:ON
   Plugins/uk.ac.ucl.cmic.midasmorphologicalsegmentor:ON
-  Plugins/uk.ac.ucl.cmic.midasgeneralsegmentor:ON
   Plugins/uk.ac.ucl.cmic.affinetransform:ON
   Plugins/uk.ac.ucl.cmic.thumbnail:ON
   Plugins/uk.ac.ucl.cmic.midasnavigation:ON
