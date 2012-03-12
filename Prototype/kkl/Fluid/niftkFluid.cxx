a13d5c9b5703bc0c4bd3f2491639a7eba20bda4e
Kelvin Leung <leung@drc.ion.ucl.ac.uk>
2012-03-12 14:00:34 +0000

    #1358: kkl prototype: niftkFluid.cxx: show time used

--- Prototype/kkl/Fluid/niftkFluid.cxx
+++ Prototype/kkl/Fluid/niftkFluid.cxx
@@ -25,7 +25,13 @@
 
 int main(int argc, char** argv)
 {
-  return fluid_main<3, short>(argc, argv); 
+  time_t start = clock(); 
+  
+  int returnValue = fluid_main<3, short>(argc, argv); 
+  time_t timeUsed = clock()-start; 
+  std::cerr << std::endl << "Total time used=" << (timeUsed/CLOCKS_PER_SEC)/60 << "min " << (timeUsed/CLOCKS_PER_SEC)%60 << "s" << std::endl; 
+      
+  return returnValue; 
 }
 
 
