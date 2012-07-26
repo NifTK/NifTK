942379f45d0cab852e6e1ae134d03cc3e8727d2d
Matt Clarkson <m.clarkson@ucl.ac.uk>
2012-07-26 06:56:44 +0000

    #1691 - remove MITK perspective as redundant

--- Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.gui.qt.niftyview/files.cmake
+++ Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.gui.qt.niftyview/files.cmake
@@ -8,7 +8,6 @@ SET(INTERNAL_CPP_FILES
   QmitkNiftyViewApplicationPlugin.cpp
   QmitkNiftyViewIGIPerspective.cpp
   QmitkNiftyViewMIDASPerspective.cpp
-  QmitkNiftyViewMITKPerspective.cpp
 )
 
 SET(MOC_H_FILES
@@ -17,7 +16,6 @@ SET(MOC_H_FILES
   src/internal/QmitkNiftyViewApplicationPlugin.h
   src/internal/QmitkNiftyViewIGIPerspective.h
   src/internal/QmitkNiftyViewMIDASPerspective.h
-  src/internal/QmitkNiftyViewMITKPerspective.h
 )
 
 SET(CACHED_RESOURCE_FILES
@@ -29,7 +27,6 @@ SET(CACHED_RESOURCE_FILES
   resources/icon_cmic.xpm
   resources/icon_ion.xpm
   resources/icon_ucl.xpm
-  resources/icon_mitk.xpm
 )
 
 SET(QRC_FILES
