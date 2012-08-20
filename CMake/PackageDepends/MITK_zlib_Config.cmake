4b51b7915ac5c0b3c19d7485b9076b4cbbb77c1d
Miklos Espak <espakm@gmail.com>
2012-08-20 17:13:41 +0100

    Bug #1654: zlib package dependencies fixed

--- CMake/PackageDepends/MITK_zlib_Config.cmake
+++ CMake/PackageDepends/MITK_zlib_Config.cmake
@@ -1,9 +1,11 @@
 
 set(zlib_INCLUDE_DIR ${CMAKE_BINARY_DIR}/../CMakeExternals/Source/zlib)
+set(zlib_LIBRARY_DIR ${CMAKE_BINARY_DIR}/../zlib-build)
+
 list(APPEND ALL_INCLUDE_DIRECTORIES ${zlib_INCLUDE_DIR})
+list(APPEND ALL_INCLUDE_DIRECTORIES ${zlib_LIBRARY_DIR})
 include_directories(${zlib_INCLUDE_DIR})
 
-set(zlib_LIBRARY_DIR ${CMAKE_BINARY_DIR}/../zlib-build)
 link_directories(${zlib_LIBRARY_DIR})
 list(APPEND ALL_LIBRARY_DIRS ${zlib_LIBRARY_DIR})
 
