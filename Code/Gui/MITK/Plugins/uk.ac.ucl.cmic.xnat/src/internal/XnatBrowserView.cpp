ccbf0485462bc0d2689b108b9535299335049e35
Miklos Espak <espakm@gmail.com>
2012-08-21 15:47:27 +0100

    Bug #1750: QtScript and QtNetwork dependency removed

--- Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.xnat/src/internal/XnatBrowserView.cpp
+++ Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.xnat/src/internal/XnatBrowserView.cpp
@@ -27,18 +27,6 @@
 #include <berryIPreferencesService.h>
 #include <berryIBerryPreferences.h>
 
-#include <QtNetwork>
-#include <QNetworkAccessManager>
-#include <QLabel>
-#include <QVBoxLayout>
-#include <QUrl>
-#include <QNetworkRequest>
-#include <QNetworkReply>
-#include <QImageReader>
-#include <QMessageBox>
-#include <QScriptEngine>
-#include <QStringBuilder>
-
 #include "XnatBrowserWidget.h"
 #include "XnatPluginPreferencePage.h"
 #include "XnatPluginSettings.h"
@@ -49,8 +37,7 @@ class XnatBrowserViewPrivate
 {
 public:
   XnatPluginSettings* settings;
-
-  QNetworkAccessManager* networkAccessManager;
+  XnatBrowserWidget* xnatBrowserWidget;
 };
 
 XnatBrowserView::XnatBrowserView()
@@ -58,18 +45,18 @@ XnatBrowserView::XnatBrowserView()
 , m_Parent(0)
 , d_ptr(new XnatBrowserViewPrivate())
 {
-  Q_D(XnatBrowserView);
-  d->networkAccessManager = new QNetworkAccessManager(this);
 }
 
 XnatBrowserView::~XnatBrowserView()
 {
   Q_D(XnatBrowserView);
-  if (m_Controls)
-  {
-    delete d->settings;
-    delete m_Controls;
-  }
+
+  delete d->settings;
+
+//  if (m_Controls)
+//  {
+//    delete m_Controls;
+//  }
 }
 
 void XnatBrowserView::CreateQtPartControl(QWidget *parent)
@@ -79,153 +66,29 @@ void XnatBrowserView::CreateQtPartControl(QWidget *parent)
   // setup the basic GUI of this view
   m_Parent = parent;
 
-  if (!m_Controls)
-  {
-    // Create UI
+  d->settings = new XnatPluginSettings(GetPreferences());
+
+  d->xnatBrowserWidget = new XnatBrowserWidget(parent);
+  QVBoxLayout* layout = new QVBoxLayout();
+  layout->addWidget(d->xnatBrowserWidget);
+  parent->setLayout(layout);
+  d->xnatBrowserWidget->setSettings(d->settings);
+  d->xnatBrowserWidget->setDataStorage(GetDataStorage());
+
+//  if (!m_Controls)
+//  {
+//    // Create UI
 //    m_Controls = new Ui::XnatBrowserView();
-    XnatBrowserWidget* xnatBrowserWidget = new XnatBrowserWidget(parent);
-    d->settings = new XnatPluginSettings(GetPreferences());
-    xnatBrowserWidget->setSettings(d->settings);
-    xnatBrowserWidget->setDataStorage(GetDataStorage());
-    QVBoxLayout* layout = new QVBoxLayout();
-    layout->addWidget(xnatBrowserWidget);
-    parent->setLayout(layout);
 //    m_Controls->setupUi(parent);
-
-    // Create connections after setting defaults, so you don't trigger stuff when setting defaults.
-//    CreateConnections();
-  }
-}
-
-void XnatBrowserView::CreateConnections()
-{
-//  Q_D(XnatBrowserView);
 //
-//  connect(m_Controls->connectButton, SIGNAL(clicked()), this, SLOT(on_connectButton_clicked()));
-//  connect(d->networkAccessManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(onNetworkReply(QNetworkReply*)));
-////  connect(d->networkAccessManager, SIGNAL(authenticationRequired(QNetworkReply*, QAuthenticator*)),
-////          this, SLOT(onAuthenticationRequired(QNetworkReply*, QAuthenticator*)));
-//#ifndef QT_NO_OPENSSL
-//  connect(d->networkAccessManager, SIGNAL(sslErrors(QNetworkReply*, QList<QSslError>)),
-//          this, SLOT(onSslErrors(QNetworkReply*, QList<QSslError>)));
-//#endif
+//    m_Controls->xnatBrowserWidget->setSettings(d->settings);
+//    m_Controls->xnatBrowserWidget->setDataStorage(GetDataStorage());
+//  }
 }
 
 void XnatBrowserView::SetFocus()
 {
-//  m_Controls->connectButton->setFocus();
+  Q_D(XnatBrowserView);
+//  m_Controls->xnatBrowserWidget->setFocus();
+  d->xnatBrowserWidget->setFocus();
 }
-
-void XnatBrowserView::on_connectButton_clicked()
-{
-//  XnatConnectionDialog* connectionDialog = new XnatConnectionDialog(m_Parent);
-//
-//  if (connectionDialog->exec() == QDialog::Accepted)
-//  {
-//    QString serverUri = connectionDialog->serverUri();
-//    QString username = connectionDialog->username();
-//    QString password = connectionDialog->password();
-//    MITK_INFO << "server uri: " << serverUri.toStdString();
-//    MITK_INFO << "username: " << username.toStdString();
-//    MITK_INFO << "password: " << password.toStdString();
-//
-//    serverUri.append("/data/archive/projects");
-//
-//    QUrl projectsQuery(serverUri);
-//
-//    QNetworkRequest request;
-//    request.setRawHeader("Authorization", "Basic " +
-//        QByteArray(QString("%1:%2").arg(username).arg(password).toAscii()).toBase64());
-//    request.setUrl(projectsQuery);
-//
-//    MITK_INFO << "projectsQuery: " << projectsQuery.toString().toStdString();
-//
-////    QNetworkReply* reply = d->networkAccessManager->get(request);
-//    // NOTE: Store QNetworkReply pointer (maybe into caller).
-//    // When this HTTP request is finished you will receive this same
-//    // QNetworkReply as response parameter.
-//    // By the QNetworkReply pointer you can identify request and response.
-//  }
-//
-//  delete connectionDialog;
-}
-
-void XnatBrowserView::onNetworkReply(QNetworkReply* reply)
-{
-//  // Reading attributes of the reply
-//  // e.g. the HTTP status code
-//  QVariant statusCodeV = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute);
-//  // Or the target URL if it was a redirect:
-//  QVariant redirectionTargetUrl = reply->attribute(QNetworkRequest::RedirectionTargetAttribute);
-//  // see CS001432 on how to handle this
-//
-//  // no error received?
-//  if (reply->error() == QNetworkReply::NoError)
-//  {
-//    // read data from QNetworkReply here
-//
-//    // Example 1: Creating QImage from the reply
-////    QImageReader imageReader(reply);
-////    QImage pic = imageReader.read();
-//
-//    // Example 2: Reading bytes form the reply
-//    QByteArray bytes = reply->readAll();  // bytes
-//
-//    QString result = QString::fromUtf8(bytes); // string
-//    MITK_INFO << "response: " << result.toStdString();
-//    QScriptValue sc;
-//    QScriptEngine engine;
-//    sc = engine.evaluate(result); // In new versions it may need to look like engine.evaluate("(" + QString(result) + ")");
-//
-////    if (sc.property("result").isArray())
-////    {
-////      QStringList items;
-////      qScriptValueToSequence(sc.property("result"), items);
-////
-////      foreach (QString str, items)
-////      {
-////        qDebug("value %s",str.toStdString().c_str());
-////      }
-////    }
-//  }
-//  // Some http error received
-//  else
-//  {
-//    // handle errors here
-//    QMessageBox::critical(m_Parent, "Network error", reply->errorString());
-//
-//  }
-//
-//  // We receive ownership of the reply object
-//  // and therefore need to handle deletion.
-//  reply->deleteLater();
-}
-
-void XnatBrowserView::onAuthenticationRequired(QNetworkReply* reply, QAuthenticator* authenticator)
-{
-//  MITK_INFO << "void XnatBrowserView::onAuthenticationRequired(QNetworkReply* reply, QAuthenticator* authenticator)";
-//  authenticator->setUser(QString("espakm"));
-//  authenticator->setPassword(QString("demo"));
-}
-
-#ifndef QT_NO_OPENSSL
-void XnatBrowserView::onSslErrors(QNetworkReply* reply, const QList<QSslError>& errors)
-{
-//  QString errorString;
-//  foreach (const QSslError& error, errors)
-//  {
-//    if (!errorString.isEmpty())
-//    {
-//      errorString.append(", ");
-//    }
-//    errorString.append(error.errorString());
-//  }
-//
-//  if (QMessageBox::warning(m_Parent, tr("HTTP"),
-//          tr("One or more SSL errors has occurred: %1").arg(errorString),
-//          QMessageBox::Ignore | QMessageBox::Abort) == QMessageBox::Ignore)
-//  {
-//    reply->ignoreSslErrors();
-//  }
-}
-#endif
