7b564af1efcfe7263567162883ff1faf6862688a
Matt Clarkson <m.clarkson@ucl.ac.uk>
2012-10-03 11:40:26 +0100

    Bug #1815: Turn property list view back on

--- Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.gui.qt.niftyview/src/QmitkNiftyViewWorkbenchWindowAdvisor.cpp
+++ Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.gui.qt.niftyview/src/QmitkNiftyViewWorkbenchWindowAdvisor.cpp
@@ -72,7 +72,6 @@ void QmitkNiftyViewWorkbenchWindowAdvisor::PreWindowOpen()
   viewExcludeList.push_back("org.blueberry.views.helpcontents");
   viewExcludeList.push_back("org.blueberry.views.helpindex");
   viewExcludeList.push_back("org.blueberry.views.helpsearch");
-  viewExcludeList.push_back("org.mitk.views.propertylistview");
   this->SetViewExcludeList(viewExcludeList);
 }
 
