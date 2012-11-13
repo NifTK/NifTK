4a95266b416353a408abe21998fa9db8748d3914
MattClarkson <m.clarkson@ucl.ac.uk>
2012-11-13 07:16:57 +0000

    Bug #1796: Fix Mac Build for MACOSX_BUNDLE_NAMES, but MITK not resolving excluded plugins correctly at this point

--- CMake/niftkMacroCreateApplication.cmake
+++ CMake/niftkMacroCreateApplication.cmake
@@ -21,14 +21,19 @@
 #  PURPOSE.  See the above copyright notices for more information.
 #
 #=================================================================================*/
-MACRO(NIFTK_CREATE_APPLICATION APPLICATION_NAME_IN)
-  MACRO_PARSE_ARGUMENTS(APP
-                        "EXCLUDE_PLUGINS"
-                        "SHOW_CONSOLE"
-                        ${ARGN})
-                        
-  SET(MY_APP_NAME ${APPLICATION_NAME_IN})
+MACRO(NIFTK_CREATE_APPLICATION)
+  MACRO_PARSE_ARGUMENTS(_APP
+                        "NAME;INCLUDE_PLUGINS;EXCLUDE_PLUGINS"
+                        ""
+                        ${ARGN}
+                        )
 
+  IF(NOT _APP_NAME)
+    MESSAGE(FATAL_ERROR "NAME argument cannot be empty.")
+  ENDIF()
+                        
+  SET(MY_APP_NAME ${_APP_NAME})
+  
   MITK_USE_MODULE(niftkMitkExt)
   MITK_USE_MODULE(qtsingleapplication)
 
@@ -42,18 +47,41 @@ MACRO(NIFTK_CREATE_APPLICATION APPLICATION_NAME_IN)
   SET(app_sources ${MY_APP_NAME}.cpp ${OSX_ICON_FILES} ${OSX_LOGO_FILES} )
 
   SET(_app_options)
-  IF(${APP_SHOW_CONSOLE})
+  IF(${NIFTK_SHOW_CONSOLE_WINDOW})
     LIST(APPEND _app_options SHOW_CONSOLE)
   ENDIF()
 
+  SET(_include_plugins
+    ${_APP_INCLUDE_PLUGINS}
+  )
   SET(_exclude_plugins
-    ${APP_EXCLUDE_PLUGINS}
+    ${_APP_EXCLUDE_PLUGINS}
   )
 
+  #############################################################################
+  # Watch out for this:
+  # In the top level CMakeLists, MACOSX_BUNDLE_NAMES will contain all the apps
+  # that we have available in this project. This is so that when you create a
+  # Module, or a Plugin, under the hood, you are using an MITK macro, which 
+  # takes care of copying said Module, Plugin into ALL bundles. HOWEVER, within
+  # the FunctionCreateBlueBerryApplication, this has the side effect of
+  # copying all MITK and CTK plugins into ALL bundles. This breaks the build at
+  # install time, as you package up the first application and then when the 
+  # second application is created, this function will again copy all the MITK 
+  # and CTK plugins back into the first app, which overwrites all the library
+  # @executable path settings leading to an invalid bundle for all but the 
+  # last executable. So, we save the variable here, and restore it at the end
+  # of this macro.
+  #############################################################################
+  
+  SET(TMP_MACOSX_BUNDLE_NAMES ${MACOSX_BUNDLE_NAMES})
+  SET(MACOSX_BUNDLE_NAMES ${MY_APP_NAME})
+  
   FunctionCreateBlueBerryApplication(
     NAME ${MY_APP_NAME}
     SOURCES ${app_sources}
     DESCRIPTION "${PROJECT_NAME} - ${MY_APP_NAME} Application"
+    PLUGINS ${_include_plugins}
     EXCLUDE_PLUGINS ${_exclude_plugins}
     ${_app_options}
   )
@@ -87,4 +115,9 @@ MACRO(NIFTK_CREATE_APPLICATION APPLICATION_NAME_IN)
     )
   ENDIF(WIN32)
 
+  #############################################################################
+  # Restore this MACOSX_BUNDLE_NAMES variable. See long-winded note above.
+  #############################################################################
+  SET(MACOSX_BUNDLE_NAMES ${TMP_MACOSX_BUNDLE_NAMES})
+  
 ENDMACRO(NIFTK_CREATE_APPLICATION)
