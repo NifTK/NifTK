d9a630f6bccbdea0f04989307987b7709e57bbb3
Matt Clarkson <m.clarkson@ucl.ac.uk>
2012-11-22 15:25:07 +0000

    Bug #1796: Copy command line scripts and apps to separate bundles

--- CMake/PackageDepends/MITK_Boost_Config.cmake
+++ CMake/PackageDepends/MITK_Boost_Config.cmake
@@ -1,24 +1,6 @@
-if(MITK_USE_Boost)
-
-  if(NOT MITK_USE_SYSTEM_Boost)
-    set(Boost_NO_SYSTEM_PATHS 1)
-  endif()
-
-  set(Boost_USE_MULTITHREADED 1)
-  set(Boost_USE_STATIC_LIBS 0)
-  set(Boost_USE_STATIC_RUNTIME 0)
-
-  if(MITK_USE_Boost_LIBRARIES)
-    find_package(Boost 1.40.0 REQUIRED COMPONENTS ${MITK_USE_Boost_LIBRARIES})
-  else()
-    find_package(Boost 1.40.0 REQUIRED)
-  endif()
-
+if(Boost_FOUND)
   list(APPEND ALL_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
-  if(Boost_LIBRARIES)
-    list(APPEND ALL_LIBRARIES ${Boost_LIBRARIES})
-    link_directories(${Boost_LIBRARY_DIRS})
-  endif()
-
-endif(MITK_USE_Boost)
+  list(APPEND ALL_LIBRARIES ${Boost_LIBRARIES})
+  link_directories(${Boost_LIBRARY_DIRS})
+endif()
 
