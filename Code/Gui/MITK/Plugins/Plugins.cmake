028ed2a5e6d014dfdc7b09293c0a10dd863ccdd6
Steve Thompson <s.thompson@ucl.ac.uk>
2012-12-05 12:40:31 +0000

    Feature #1939 Started a single view editor (currently a copy of the stdmultiview editor

--- Code/Gui/MITK/Plugins/Plugins.cmake
+++ Code/Gui/MITK/Plugins/Plugins.cmake
@@ -20,4 +20,5 @@ set(PROJECT_PLUGINS
   Plugins/uk.ac.ucl.cmic.niftyreg:ON                      # Must be after the xnat plugin
   Plugins/uk.ac.ucl.cmic.niftyseg:OFF                     # Not ready yet.
   Plugins/uk.ac.ucl.cmic.breastsegmentation:OFF           # Under development
+  Plugins/uk.ac.ucl.cmic.singlewidgeteditor:ON           # Under development
 )
---
+++ Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.singlewidgeteditor/src/internal/QmitkSingleWidgetEditorPreferencePage.h
@@ -0,0 +1,85 @@
+/*===================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center, 
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without 
+even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
+
+
+#ifndef QMITKSINGLEWIDGETEDITORPREFERENCEPAGE_H_
+#define QMITKSINGLEWIDGETEDITORPREFERENCEPAGE_H_
+
+#include "berryIQtPreferencePage.h"
+#include <berryIPreferences.h>
+
+class QWidget;
+class QCheckBox;
+class QPushButton;
+class QWidgetAction;
+
+struct QmitkSingleWidgetEditorPreferencePage : public QObject, public berry::IQtPreferencePage
+{
+  Q_OBJECT
+  Q_INTERFACES(berry::IPreferencePage)
+
+public:
+  QmitkSingleWidgetEditorPreferencePage();
+
+  void Init(berry::IWorkbench::Pointer workbench);
+
+  void CreateQtControl(QWidget* widget);
+
+  QWidget* GetQtControl() const;
+
+  ///
+  /// \see IPreferencePage::PerformOk()
+  ///
+  virtual bool PerformOk();
+
+  ///
+  /// \see IPreferencePage::PerformCancel()
+  ///
+  virtual void PerformCancel();
+
+  ///
+  /// \see IPreferencePage::Update()
+  ///
+  virtual void Update();
+
+public slots:
+  void FirstColorChanged();
+
+  void SecondColorChanged();
+
+  void UseGradientBackgroundSelected();
+
+  void ColorActionChanged();
+
+  void ResetColors();
+
+protected:
+  QWidget* m_MainControl;
+  QCheckBox* m_EnableFlexibleZooming;
+  QCheckBox* m_ShowLevelWindowWidget;
+  QCheckBox* m_UseGradientBackground;
+  QCheckBox* m_ChangeBackgroundColors;
+  QCheckBox* m_PACSLikeMouseMode;
+  QPushButton* m_ColorButton1;
+  QPushButton* m_ColorButton2;
+  std::string m_FirstColor;
+  std::string m_SecondColor;
+  QString m_FirstColorStyleSheet;
+  QString m_SecondColorStyleSheet;
+  berry::IPreferences::Pointer m_SingleWidgetEditorPreferencesNode;
+};
+
+#endif /* QMITKDATAMANAGERPREFERENCEPAGE_H_ */
