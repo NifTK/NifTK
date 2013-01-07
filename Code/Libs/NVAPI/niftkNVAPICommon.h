5ce3a9c74d32a8064729aaaeae78e8d7b7347206
Matt Clarkson <m.clarkson@ucl.ac.uk>
2013-01-07 08:14:21 +0000

    Bug #1986: Renamed NVidia library folder, and tweaked build

---
+++ Code/Libs/NVAPI/niftkNVAPICommon.h
@@ -0,0 +1,33 @@
+/*=============================================================================
+
+ NifTK: An image processing toolkit jointly developed by the
+             Dementia Research Centre, and the Centre For Medical Image Computing
+             at University College London.
+
+ See:        http://dementia.ion.ucl.ac.uk/
+             http://cmic.cs.ucl.ac.uk/
+             http://www.ucl.ac.uk/
+
+ Last Changed      : $Date: 2010-08-11 08:28:23 +0100 (Wed, 11 Aug 2010) $
+ Revision          : $Revision: 6628 $
+ Last modified by  : $Author: ad $
+
+ Original author   : m.clarkson@cs.ucl.ac.uk
+
+ Copyright (c) UCL : See LICENSE.txt in the top level directory for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
+
+ ============================================================================*/
+#ifndef NIFTKNVAPICOMMON_H
+#define NIFTKNVAPICOMMON_H
+
+#include "NifTKConfigure.h"
+#include "niftkNVAPIWin32ExportHeader.h"
+
+extern "C" NIFTKNVAPI_WINEXPORT void TestFunction();
+
+#endif
+
