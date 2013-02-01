5f1c57c6e50a7978ed108528b7220d9456675b38
MattClarkson <m.clarkson@ucl.ac.uk>
2013-02-01 16:24:47 +0000

    Bug #2041: Added niftkOpenCVTest program

--- Code/Gui/MITK/Modules/niftkOpenCV/files.cmake
+++ Code/Gui/MITK/Modules/niftkOpenCV/files.cmake
@@ -2,4 +2,5 @@ set(CPP_FILES
   CameraCalibration/mitkCameraCalibrationFacade.cpp
   CameraCalibration/mitkCameraCalibrationFromDirectory.cpp
   CameraCalibration/mitkStereoCameraCalibrationFromTwoDirectories.cpp
+  demo/mitkOpenCVTest.cpp
 )
