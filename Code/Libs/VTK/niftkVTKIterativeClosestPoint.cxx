1f9d7acd3c4c37ebc0ed79ccc3c1f1f5d3adf709
Steve Thompson <s.thompson@ucl.ac.uk>
2013-03-06 14:24:58 +0000

    Feature #2111 added VTK ICP libarary

---
+++ Code/Libs/VTK/niftkVTKIterativeClosestPoint.cxx
@@ -0,0 +1,89 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "niftkVTKIterativeClosestPoint.h"
+
+#include <vtkLandmarkTransform.h>
+#include <vtkPolyData.h>
+#include <vtkIterativeClosestPointTransform.h>
+#include <vtkTransformPolyDataFilter.h>
+
+namespace niftk
+{
+
+ 
+
+// ---------------------------------------------------------------------------
+// Create a reference image corresponding to a given control point grid image
+// ---------------------------------------------------------------------------
+
+  IterativeClosestPoint::IterativeClosestPoint()
+    : m_Source(NULL),
+    m_Target(NULL),
+    m_TransformMatrix(NULL),
+    m_MaxLandmarks(50),
+    m_MaxIterations(100)
+  {
+    m_icp = vtkSmartPointer<vtkIterativeClosestPointTransform>::New();
+    m_icp->GetLandmarkTransform()->SetModeToRigidBody();
+    m_icp->SetMaximumNumberOfLandmarks(m_MaxLandmarks);
+    m_icp->SetMaximumNumberOfIterations(m_MaxIterations);
+  }
+
+  bool IterativeClosestPoint::Run()
+  {
+   if ( m_Source == NULL || m_Target == NULL ) 
+   {
+     return false;
+   }
+   m_icp->SetSource(m_Source);
+   m_icp->SetTarget(m_Target);
+   m_icp->Modified();
+   m_icp->Update();
+   m_TransformMatrix = m_icp->GetMatrix();
+   return true;
+  }
+
+  vtkSmartPointer<vtkMatrix4x4> IterativeClosestPoint::GetTransform()
+  {
+    return m_TransformMatrix;
+  }
+
+  bool IterativeClosestPoint::TransformSource()
+  {
+   if ( m_Source == NULL && m_TransformMatrix == NULL ) 
+   {
+    return false;
+   }
+    vtkSmartPointer<vtkTransformPolyDataFilter> icpTransformFilter =
+          vtkSmartPointer<vtkTransformPolyDataFilter>::New();
+#if VTK_MAJOR_VERSION <= 5
+    icpTransformFilter->SetInputData(m_Source);
+#else
+    icpTransformFilter->SetInputData(m_Source);
+#endif
+    icpTransformFilter->SetTransform(m_icp);
+    icpTransformFilter->Update();
+    return true;
+  }
+
+  bool IterativeClosestPoint::TransformTarget()
+  {
+    return true;
+  }
+
+
+
+} // end namespace niftk
+
---
+++ Code/Libs/VTK/niftkVTKIterativeClosestPoint.h
@@ -0,0 +1,64 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef __NIFTKVTKITERATIVECLOSESTPOINT_H
+#define __NIFTKVTKITERATIVECLOSESTPOINT_H
+
+
+#include "NifTKConfigure.h"
+#include "niftkCommonWin32ExportHeader.h"
+#include "niftkVTKWin32ExportHeader.h"
+
+#include <ostream>
+#include <stdio.h>
+#include <string>
+
+
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkMatrix4x4.h>
+#include <vtkIterativeClosestPointTransform.h>
+
+
+namespace niftk
+{
+
+  class NIFTKVTK_WINEXPORT IterativeClosestPoint {
+
+    public:
+      IterativeClosestPoint();
+    /* \brief
+    * Perform a vtkIterative closest point registration on the two data sets
+    */
+      bool Run();
+
+      vtkSmartPointer<vtkMatrix4x4> GetTransform();
+      bool TransformSource();
+      bool TransformTarget();
+
+
+    private:
+      vtkSmartPointer<vtkIterativeClosestPointTransform> m_icp;
+      vtkSmartPointer<vtkPolyData> m_Source;
+      vtkSmartPointer<vtkPolyData> m_Target;
+      vtkSmartPointer<vtkMatrix4x4> m_TransformMatrix;
+      unsigned int m_MaxLandmarks;
+      unsigned int m_MaxIterations;
+  };
+
+
+
+} // end namespace niftk
+
+#endif  //__NIFTKVTKITERATIVECLOSESTPOINT_H
