2f3ac6ad8c88533132f52a5096bc89d8f80e7bc7
Matt Clarkson <m.clarkson@ucl.ac.uk>
2013-03-11 14:09:49 +0000

    Bug #2127: First pass, simple Continuous.cmake for running off an existing folder

--- CMake/Continuous.cmake
+++ CMake/Continuous.cmake
@@ -12,54 +12,20 @@
 #
 #============================================================================*/
 
+SET (CTEST_BRANCH_NAME "dev")
+SET (CTEST_SOURCE_DIRECTORY "$ENV{BASE}/NifTK")
+SET (CTEST_BINARY_DIRECTORY "$ENV{BASE}/NifTK-SuperBuild-Release/NifTK-build")
+SET (CTEST_COMMAND "ctest -D Continuous")
+SET (CTEST_CMAKE_COMMAND "cmake")
+FIND_PROGRAM(CTEST_GIT_COMMAND NAMES git)
+SET(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND} pull origin ${CTEST_BRANCH_NAME}")
+SET(NIFTK_CACHE_FILE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt")
+SET(INITIAL_CMAKECACHE_OPTIONS "${INITIAL_CMAKECACHE_OPTIONS} NIFTK_CACHE_FILE:INTERNAL=${NIFTK_CACHE_FILE}")
 
-SET (CTEST_SOURCE_DIRECTORY "$ENV{HOME}/wc/static/NifTK")
-SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/wc/static/NifTK-build")
-SET (CTEST_COMMAND "$ENV{HOME}/niftk/cmake/bin/ctest -D Nightly")
-SET (CTEST_CMAKE_COMMAND "$ENV{HOME}/niftk/cmake/bin/cmake")
-SET (CTEST_SVN_COMMAND "/usr/bin/svn")
+# build for 900 minutes eg. 06:00 am to 21:00 pm, 15 hours.
+SET (CTEST_CONTINUOUS_DURATION 900)
 
-# If you switch from Debug to Release, the tests arent built!
-SET (CTEST_INITIAL_CACHE "
-BUILDNAME:STRING=Linux-Centos-g++-4.1.2-20080704-static
-SITE:STRING=cmicdev
-BUILD_SHARED_LIBS:STRING=OFF
-BUILD_GUI:STRING=ON
-CMAKE_BUILD_TYPE:STRING=Debug
-BUILD_NIFTK_PROTOTYPE:STRING=OFF
-USE_QT:STRING=ON
-USE_VTK:STRING=ON
-NIFTK_BOOST_GENERIC_PATH:FILEPATH=$ENV{HOME}/niftk/static/boost
-NIFTK_ITK_GENERIC_PATH:FILEPATH=$ENV{HOME}/niftk/static/itk
-NIFTK_FFTWINSTALL:FILEPATH=$ENV{HOME}/niftk/static/fftw
-NIFTK_LOG4CPLUSINSTALL:FILEPATH=$ENV{HOME}/niftk/static/log4cplus
-NIFTK_BOOSTINSTALL:FILEPATH=$ENV{HOME}/niftk/static/boost
-NIFTK_INSTALL_PREFIX:FILEPATH=$ENV{HOME}/niftk/static
-NIFTK_LINK_PREFIX:FILEPATH=$ENV{HOME}/niftk/static
-NIFTK_BASE_NAME:STRING=niftk
-Log4cplus_INCLUDE_DIR:PATH=$ENV{HOME}/niftk/static/log4cplus/include
-Log4cplus_LIBRARIES:FILEPATH=$ENV{HOME}/niftk/static/log4cplus/lib/liblog4cplus.a
-ITK_DIR:PATH=$ENV{HOME}/niftk/static/itk/lib/InsightToolkit
-VTK_DIR:PATH=$ENV{HOME}/niftk/static/vtk/lib/vtk-5.6
-FFTW_INCLUDE_DIR:PATH=$ENV{HOME}/niftk/static/fftw/include
-FFTW_LIBRARIES:FILEPATH=$ENV{HOME}/niftk/static/fftw/lib/libfftw3.a
-Boost_DATE_TIME_LIBRARY:FILEPATH=$ENV{HOME}/niftk/static/boost/lib/libboost_date_time.a
-Boost_DATE_TIME_LIBRARY_RELEASE:FILEPATH=$ENV{HOME}/niftk/static/boost/lib/libboost_date_time.a
-Boost_FILESYSTEM_LIBRARY:FILEPATH=$ENV{HOME}/niftk/static/boost/lib/libboost_filesystem.a
-Boost_FILESYSTEM_LIBRARY_RELEASE:FILEPATH=$ENV{HOME}/niftk/static/boost/lib/libboost_filesystem.a
-Boost_INCLUDE_DIR:PATH=$ENV{HOME}/niftk/static/boost/include
-Boost_LIBRARY_DIRS:PATH=$ENV{HOME}/niftk/static/boost/lib
-Boost_PROGRAM_OPTIONS_LIBRARY:FILEPATH=$ENV{HOME}/niftk/static/boost/lib/libboost_program_options.a
-Boost_PROGRAM_OPTIONS_LIBRARY_RELEASE:FILEPATH=$ENV{HOME}/niftk/static/boost/lib/libboost_program_options.a
-Boost_SYSTEM_LIBRARY:FILEPATH=$ENV{HOME}/niftk/static/boost/lib/libboost_system.a
-Boost_SYSTEM_LIBRARY_RELEASE:FILEPATH=$ENV{HOME}/niftk/static/boost/lib/libboost_system.a
-")
-
-
-# build for 15 minutes
-SET (CTEST_CONTINUOUS_DURATION 15)
-
-# check every 15 minutes
+# check every 5 minutes
 SET (CTEST_CONTINUOUS_MINIMUM_INTERVAL 15)
 
 # Wipe binaries each time
