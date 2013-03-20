1e4d43ba3603f018c3558402caeb5cae6276b70e
Johannes Totz <j.totz@ucl.ac.uk>
2013-03-20 16:04:28 +0000

    Bug #2164: put some info into IGINVidiaDataType

--- Code/Gui/MITK/Modules/niftkNVidia/DataManagement/mitkIGINVidiaDataType.cpp
+++ Code/Gui/MITK/Modules/niftkNVidia/DataManagement/mitkIGINVidiaDataType.cpp
@@ -19,12 +19,20 @@ namespace mitk
 
 //-----------------------------------------------------------------------------
 IGINVidiaDataType::IGINVidiaDataType()
+  : magic_cookie(0), sequence_number(0), gpu_arrival_time(0)
 {
 }
 
 //-----------------------------------------------------------------------------
 IGINVidiaDataType::~IGINVidiaDataType()
 {
+}
+
+void IGINVidiaDataType::set_values(unsigned int cookie, unsigned int sn, unsigned __int64 gputime)
+{
+  magic_cookie = cookie;
+  sequence_number = sn;
+  gpu_arrival_time = gputime;
 }
 
 } // end namespace
