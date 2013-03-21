f21eb9b887088d48d4a1b294f59ccbef9544ede7
Johannes Totz <j.totz@ucl.ac.uk>
2013-03-21 19:37:00 +0000

    Bug #2164: libvideo can now use a ringbuffer of texture objects to keep the last x frames around

--- Code/Libs/NVAPI/video/video/sdiinput.h
+++ Code/Libs/NVAPI/video/video/sdiinput.h
@@ -18,73 +18,78 @@ class LIBVIDEO_DLL_EXPORTS SDIInput
 private:
 
 protected:
-	SDIInputImpl*		pimpl;
+    SDIInputImpl*   pimpl;
 
 
 public:
-	// what to do if format is interlaced
-	// these are ignored if incoming video is progressive
-	enum InterlacedBehaviour
-	{
-		// interlaced video is treated the same way as progressive
-		DO_NOTHING_SPECIAL,
-		// one of the fields (not specifying which one) is dropped
-		DROP_ONE_FIELD,
-		// both fields are captured and stacked vertically
-		STACK_FIELDS
-	};
+    // what to do if format is interlaced
+    // these are ignored if incoming video is progressive
+    enum InterlacedBehaviour
+    {
+        // interlaced video is treated the same way as progressive
+        DO_NOTHING_SPECIAL,
+        // one of the fields (not specifying which one) is dropped
+        DROP_ONE_FIELD,
+        // both fields are captured and stacked vertically
+        STACK_FIELDS
+    };
 
 
 public:
-	SDIInput(SDIDevice* dev, InterlacedBehaviour interlaced = DO_NOTHING_SPECIAL);
-	~SDIInput();
+    SDIInput(SDIDevice* dev, InterlacedBehaviour interlaced = DO_NOTHING_SPECIAL, int ringbuffersize = 0);
+    ~SDIInput();
 
 private:
-	// not implemented
-	SDIInput(const SDIInput& copyme);
-	SDIInput& operator=(const SDIInput& assignme);
+    // not implemented
+    SDIInput(const SDIInput& copyme);
+    SDIInput& operator=(const SDIInput& assignme);
 
 
 public:
-	// these may be different from the reported capture format
-	//  if we are dropping a field, for example
-	int get_width() const;
-	int get_height() const;
+    // these may be different from the reported capture format
+    //  if we are dropping a field, for example
+    int get_width() const;
+    int get_height() const;
+
+
+protected:
+    // if true then capture() will prepare the textures, otherwise data is only in pbos
+    bool  preparetextures;
 
 
 public:
-	// format is always RGBA!
-	// returns zero if no stream with that index
-	int get_texture_id(int streamno) const;
+    // format is always RGBA!
+    // returns zero if no stream with that index
+    int get_texture_id(int streamno) const;
 
-	/**
-	 * @brief Transfers one set of frames over all streams into texture objects.
-	 * @throws std::runtime_error if capture setup has become invalid
-	 * @post Texture binding on the currently active unit will have changed
-	 * @detail This method will actively try to prevent the system from entering any power-savings mode!
-	 */
-	FrameInfo capture();
+    /**
+     * @brief Transfers one set of frames over all streams into texture objects.
+     * @throws std::runtime_error if capture setup has become invalid
+     * @post Texture binding on the currently active unit will have changed
+     * @detail This method will actively try to prevent the system from entering any power-savings mode!
+     */
+    FrameInfo capture();
 
 
-	/**
-	 * @warning Caveat: this will not throw if the capture state has become invalid,
-	 * it will simply continue returning false! So if too much time has passed since last frame
-	 * you may want to call capture() anyway to see whether there still is anything connected.
-	 */
-	bool has_frame() const;
+    /**
+     * @warning Caveat: this will not throw if the capture state has become invalid,
+     * it will simply continue returning false! So if too much time has passed since last frame
+     * you may want to call capture() anyway to see whether there still is anything connected.
+     */
+    bool has_frame() const;
 
 
 #pragma warning(push)
 #pragma warning(disable: 4251)      //  class '...' needs to have dll-interface to be used by clients of class '...'
 
 protected:
-	std::string		logfilename;
+    std::string     logfilename;
 
 #pragma warning(pop)
 
 
 public:
-	void set_log_filename(const std::string& fn);
+    void set_log_filename(const std::string& fn);
 };
 
 
