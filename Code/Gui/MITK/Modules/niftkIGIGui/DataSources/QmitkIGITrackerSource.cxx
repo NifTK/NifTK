2f1c9cda36c038a2010d3c4317a06482b676d7b9
Matt Clarkson <m.clarkson@ucl.ac.uk>
2013-04-22 07:24:35 +0100

    Bug #2277: Added new QmitkIGITrackerSource and GUI

---
+++ Code/Gui/MITK/Modules/niftkIGIGui/DataSources/QmitkIGITrackerSource.cxx
@@ -0,0 +1,280 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "QmitkIGITrackerSource.h"
+#include <QFile>
+#include <QDir>
+#include <QString>
+#include <mitkDataNode.h>
+#include "QmitkIGINiftyLinkDataType.h"
+#include "QmitkIGIDataSourceMacro.h"
+
+//NIFTK_IGISOURCE_MACRO(NIFTKIGIGUI_EXPORT, QmitkIGITrackerTool, "IGI Tracker Tool");
+
+//-----------------------------------------------------------------------------
+QmitkIGITrackerSource::QmitkIGITrackerSource(mitk::DataStorage* storage, NiftyLinkSocketObject * socket)
+: QmitkIGINiftyLinkDataSource(storage, socket)
+{
+}
+
+
+//-----------------------------------------------------------------------------
+QmitkIGITrackerSource::~QmitkIGITrackerSource()
+{
+}
+
+
+//-----------------------------------------------------------------------------
+void QmitkIGITrackerSource::InterpretMessage(NiftyLinkMessage::Pointer msg)
+{
+  if (msg->GetMessageType() == QString("STRING"))
+  {
+    QString str = static_cast<NiftyLinkStringMessage::Pointer>(msg)->GetString();
+    if (str.isEmpty() || str.isNull())
+    {
+      return;
+    }
+    QString type = XMLBuilderBase::ParseDescriptorType(str);
+    if (type == QString("TrackerClientDescriptor"))
+    {
+      ClientDescriptorXMLBuilder* clientInfo = new TrackerClientDescriptor();
+      clientInfo->SetXMLString(str);
+
+      if (!clientInfo->IsMessageValid())
+      {
+        delete clientInfo;
+        return;
+      }
+
+      // A single source can have multiple tracked tools.
+      QStringList trackerTools = dynamic_cast<TrackerClientDescriptor*>(clientInfo)->GetTrackerTools();
+      QString tool;
+      this->SetNumberOfTools(trackerTools.length());
+      std::list<std::string> StringList;
+      foreach (tool , trackerTools)
+      {
+        std::string String;
+        String = tool.toStdString();
+        StringList.push_back(String);
+      }
+      if ( StringList.size() > 0 )
+      {
+        this->SetToolStringList(StringList);
+      }
+      this->ProcessClientInfo(clientInfo);
+    }
+    else
+    {
+      return;
+    }
+  }
+  else if (msg.data() != NULL
+           && (msg->GetMessageType() == QString("TRANSFORM") || msg->GetMessageType() == QString("TDATA"))
+     )
+  {
+    // Check the tool name
+    NiftyLinkTrackingDataMessage::Pointer trMsg;
+    trMsg = static_cast<NiftyLinkTrackingDataMessage::Pointer>(msg);
+
+    QString messageToolName = trMsg->GetTrackerToolName();
+    QString sourceToolName = QString::fromStdString(this->GetDescription());
+    if ( messageToolName == sourceToolName ) 
+    {
+      QmitkIGINiftyLinkDataType::Pointer wrapper = QmitkIGINiftyLinkDataType::New();
+      wrapper->SetMessage(msg.data());
+      wrapper->SetDataSource("QmitkIGITrackerTool");
+      wrapper->SetTimeStampInNanoSeconds(GetTimeInNanoSeconds(msg->GetTimeCreated()));
+      wrapper->SetDuration(1000000000); // nanoseconds
+
+      this->AddData(wrapper.GetPointer());
+      this->SetStatus("Receiving");
+    }
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+bool QmitkIGITrackerSource::CanHandleData(mitk::IGIDataType* data) const
+{
+  bool canHandle = false;
+  std::string className = data->GetNameOfClass();
+
+  if (data != NULL && className == std::string("QmitkIGINiftyLinkDataType"))
+  {
+    QmitkIGINiftyLinkDataType::Pointer dataType = dynamic_cast<QmitkIGINiftyLinkDataType*>(data);
+    if (dataType.IsNotNull())
+    {
+      NiftyLinkMessage* pointerToMessage = dataType->GetMessage();
+      if (pointerToMessage != NULL
+          && (pointerToMessage->GetMessageType() == QString("TRANSFORM")
+              || pointerToMessage->GetMessageType() == QString("TDATA"))
+          )
+      {
+        canHandle = true;
+      }
+    }
+  }
+  return canHandle;
+}
+
+
+//-----------------------------------------------------------------------------
+bool QmitkIGITrackerSource::Update(mitk::IGIDataType* data)
+{
+  bool result = false;
+
+  QmitkIGINiftyLinkDataType::Pointer dataType = static_cast<QmitkIGINiftyLinkDataType*>(data);
+  if (dataType.IsNotNull())
+  {
+
+    NiftyLinkMessage* pointerToMessage = dataType->GetMessage();
+    if (pointerToMessage != NULL)
+    {
+      this->HandleTrackerData(pointerToMessage);
+      this->DisplayTrackerData(pointerToMessage);
+      result = true;
+    }
+    else
+    {
+      MITK_ERROR << "QmitkIGITrackerSource::Update is receiving messages with no data ... this is wrong!" << std::endl;
+    }
+  }
+  return result;
+}
+
+
+//-----------------------------------------------------------------------------
+void QmitkIGITrackerSource::HandleTrackerData(NiftyLinkMessage* msg)
+{
+  if (msg->GetMessageType() == QString("TDATA"))
+  {
+    NiftyLinkTrackingDataMessage* trMsg;
+    trMsg = static_cast<NiftyLinkTrackingDataMessage*>(msg);
+
+    QString toolName = trMsg->GetTrackerToolName();
+
+    mitk::DataStorage* ds = this->GetDataStorage();
+    if (ds == NULL)
+    {
+      QString message("ERROR: QmitkIGITrackerSource, DataStorage Access Error: Could not access DataStorage!");
+      emit StatusUpdate(message);
+      return;
+    }
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+void QmitkIGITrackerSource::DisplayTrackerData(NiftyLinkMessage* msg)
+{
+  if (msg->GetMessageType() == QString("TRANSFORM"))
+  {
+    NiftyLinkTransformMessage* trMsg;
+    trMsg = static_cast<NiftyLinkTransformMessage*>(msg);
+
+    if (trMsg != NULL)
+    {
+      // Print stuff
+      QString header;
+      header.append("Message from: ");
+      header.append(trMsg->GetHostName());
+      header.append(", messageId=");
+      header.append(QString::number(trMsg->GetId()));
+      header.append("\n");
+
+      QString matrix = trMsg->GetMatrixAsString();
+      matrix.append("\n");
+
+      QString message = header + matrix;
+
+      emit StatusUpdate(message);
+    }
+  }
+  else if (msg->GetMessageType() == QString("TDATA"))
+  {
+    NiftyLinkTrackingDataMessage* trMsg;
+    trMsg = static_cast<NiftyLinkTrackingDataMessage*>(msg);
+
+    if (trMsg != NULL)
+    {
+      // Print stuff
+      QString header;
+      header.append("Message from: ");
+      header.append(trMsg->GetHostName());
+      header.append(", messageId=");
+      header.append(QString::number(trMsg->GetId()));
+      header.append(", toolId=");
+      header.append(trMsg->GetTrackerToolName());
+      header.append("\n");
+
+      QString matrix = trMsg->GetMatrixAsString();
+      matrix.append("\n");
+
+      QString message = header + matrix;
+
+      emit StatusUpdate(message);
+    }
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+bool QmitkIGITrackerSource::SaveData(mitk::IGIDataType* data, std::string& outputFileName)
+{
+  bool success = false;
+  outputFileName = "";
+
+  QmitkIGINiftyLinkDataType::Pointer dataType = static_cast<QmitkIGINiftyLinkDataType*>(data);
+  if (dataType.IsNotNull())
+  {
+    NiftyLinkMessage* pointerToMessage = dataType->GetMessage();
+    if (pointerToMessage != NULL)
+    {
+      NiftyLinkTrackingDataMessage* trMsg = static_cast<NiftyLinkTrackingDataMessage*>(pointerToMessage);
+      if (trMsg != NULL)
+      {
+        QString directoryPath = QString::fromStdString(this->GetSavePrefix()) + QDir::separator() + QString("QmitkIGITrackerTool") + QDir::separator() + QString::fromStdString(this->GetDescription());
+        QDir directory(directoryPath);
+        if (directory.mkpath(directoryPath))
+        {
+          QString fileName =  directoryPath + QDir::separator() + tr("%1.txt").arg(data->GetTimeStampInNanoSeconds());
+
+          float matrix[4][4];
+          trMsg->GetMatrix(matrix);
+
+          QFile matrixFile(fileName);
+          matrixFile.open(QIODevice::WriteOnly | QIODevice::Text);
+
+          if (!matrixFile.error())
+          {
+            QTextStream matout(&matrixFile);
+            matout.setRealNumberPrecision(10);
+            matout.setRealNumberNotation(QTextStream::FixedNotation);
+
+            matout << matrix[0][0] << " " << matrix[0][1] << " " << matrix[0][2] << " " << matrix[0][3]  << "\n";
+            matout << matrix[1][0] << " " << matrix[1][1] << " " << matrix[1][2] << " " << matrix[1][3]  << "\n";
+            matout << matrix[2][0] << " " << matrix[2][1] << " " << matrix[2][2] << " " << matrix[2][3]  << "\n";
+            matout << matrix[3][0] << " " << matrix[3][1] << " " << matrix[3][2] << " " << matrix[3][3]  << "\n";
+
+            matrixFile.close();
+
+            outputFileName = fileName.toStdString();
+            success = true;
+          }
+        }
+      }
+    }
+  }
+  return success;
+}
---
+++ Code/Gui/MITK/Modules/niftkIGIGui/DataSources/QmitkIGITrackerSource.h
@@ -0,0 +1,88 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef QmitkIGITrackerSource_h
+#define QmitkIGITrackerSource_h
+
+#include "niftkIGIGuiExports.h"
+#include "QmitkIGINiftyLinkDataSource.h"
+#include <mitkDataNode.h>
+#include <mitkPointSet.h>
+
+/**
+ * \class QmitkIGITrackerSource
+ * \brief Base class for IGI Tracker Tools.
+ */
+class NIFTKIGIGUI_EXPORT QmitkIGITrackerSource : public QmitkIGINiftyLinkDataSource
+{
+  Q_OBJECT
+
+public:
+
+  mitkClassMacro(QmitkIGITrackerSource, QmitkIGINiftyLinkDataSource);
+  mitkNewMacro2Param(QmitkIGITrackerSource, mitk::DataStorage*, NiftyLinkSocketObject *);
+
+  /**
+   * \brief Defined in base class, so we check that the data is in fact a NiftyLinkMessageType containing tracking data.
+   * \see mitk::IGIDataSource::CanHandleData()
+   */
+  virtual bool CanHandleData(mitk::IGIDataType* data) const;
+
+  /**
+   * \brief Defined in base class, this is the method where we do any update based on the available data.
+   * \see mitk::IGIDataSource::Update()
+   */
+  virtual bool Update(mitk::IGIDataType* data);
+
+signals:
+
+  void StatusUpdate(QString statusUpdateMessage);
+
+protected:
+
+  QmitkIGITrackerSource(mitk::DataStorage* storage, NiftyLinkSocketObject* socket); // Purposefully hidden.
+  virtual ~QmitkIGITrackerSource(); // Purposefully hidden.
+
+  QmitkIGITrackerSource(const QmitkIGITrackerSource&); // Purposefully not implemented.
+  QmitkIGITrackerSource& operator=(const QmitkIGITrackerSource&); // Purposefully not implemented.
+
+  /**
+   * \brief \see IGIDataSource::SaveData();
+   */
+  virtual bool SaveData(mitk::IGIDataType* data, std::string& outputFileName);
+
+protected slots:
+
+  /**
+   * \brief Main message handler routine for this tool, called by the signal from the socket.
+   */
+  virtual void InterpretMessage(NiftyLinkMessage::Pointer msg);
+
+private:
+
+  /**
+   * \brief Takes a message and extracts a matrix/transform and applies it.
+   */
+  void HandleTrackerData(NiftyLinkMessage* msg);
+
+  /**
+   * \brief Used to feedback a message of either coordinates, or matrices to
+   * the GUI consolve via the StatusUpdate method, and also writes to console.
+   */
+  void DisplayTrackerData(NiftyLinkMessage* msg);
+  
+}; // end class
+
+#endif
+
