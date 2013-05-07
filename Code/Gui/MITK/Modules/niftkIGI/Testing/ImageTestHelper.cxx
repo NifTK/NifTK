64431e099bfc0cfff8de2fbe9dfdc1f2f8a68ab9
Johannes Totz <j.totz@ucl.ac.uk>
2013-05-07 17:02:12 +0100

    Bug #2284: add (unnecessary) test code for checking incompatible pixel types.

--- Code/Gui/MITK/Modules/niftkIGI/Testing/ImageTestHelper.cxx
+++ Code/Gui/MITK/Modules/niftkIGI/Testing/ImageTestHelper.cxx
@@ -46,7 +46,6 @@ bool AreImagesTheSame(const typename boost::gil::image<A, false>::const_view_t& 
 //-----------------------------------------------------------------------------
 static void TestAreImagesTheSameRGB()
 {
-  // FIXME: check different pixel types
   boost::gil::rgb8_image_t  zero(123, 45);
   boost::gil::rgb8_image_t  one(zero.dimensions());
   boost::gil::rgb8_image_t  wrongsize(45, 123);
@@ -69,7 +68,6 @@ static void TestAreImagesTheSameRGB()
 //-----------------------------------------------------------------------------
 static void TestAreImagesTheSameRGBA()
 {
-  // FIXME: check different pixel types
   boost::gil::rgba8_image_t  zero(123, 45);
   boost::gil::rgba8_image_t  one(zero.dimensions());
   boost::gil::rgba8_image_t  wrongsize(45, 123);
@@ -90,8 +88,38 @@ static void TestAreImagesTheSameRGBA()
 
 
 //-----------------------------------------------------------------------------
+// GIL makes sure compilation fails if we are comparing different pixel types.
+// So we do not need this method. But leave it here for now to document that
+// we thought about this case.
+static void TestAreImagesWithDifferingTypes()
+{
+  // same size/content but different types
+  boost::gil::rgba8_image_t  zero1(123, 45);
+  boost::gil::rgb8_image_t   zero2(zero1.dimensions());
+  // not actually zero... doesnt matter
+  // but explicit rgba alpha has same value as non-existing implicit rgb alpha: zero
+  boost::gil::fill_pixels(boost::gil::view(zero1), boost::gil::rgba8_pixel_t(3, 7, 9, 0));
+  boost::gil::fill_pixels(boost::gil::view(zero2), boost::gil::rgb8_pixel_t(3, 7, 9));
+
+//  MITK_TEST_CONDITION(
+//    !(AreImagesTheSame<boost::gil::rgba8_pixel_t, boost::gil::rgb8_pixel_t>(boost::gil::const_view(zero1), boost::gil::const_view(zero2))), 
+//    "Checking test code: similar RGB and RGBA images are not the same");
+
+
+  // same number of components (and swizzled content) but different layout
+  boost::gil::bgr8_image_t  zero3(zero2.dimensions());
+  boost::gil::fill_pixels(boost::gil::view(zero3), boost::gil::rgb8_pixel_t(3, 7, 9));
+
+//  MITK_TEST_CONDITION(
+//    !(AreImagesTheSame<boost::gil::rgb8_pixel_t, boost::gil::bgr8_pixel_t>(boost::gil::const_view(zero2), boost::gil::const_view(zero3))), 
+//    "Checking test code: similar RGB and BGR images are not the same");
+}
+
+
+//-----------------------------------------------------------------------------
 static void TestAreImagesTheSame()
 {
   TestAreImagesTheSameRGB();
   TestAreImagesTheSameRGBA();
+  TestAreImagesWithDifferingTypes();
 }
