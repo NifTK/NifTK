9b10544096cdf5c2e5a5e70281f4da3d74a76187
Steve Thompson <s.thompson@ucl.ac.uk>
2013-06-07 13:45:48 +0100

    Feature #2440 implemented pointset to polydata function

--- Code/Gui/MITK/Modules/niftkIGI/SurfaceBasedRegistration/mitkSurfaceBasedRegistration.cxx
+++ Code/Gui/MITK/Modules/niftkIGI/SurfaceBasedRegistration/mitkSurfaceBasedRegistration.cxx
@@ -13,7 +13,9 @@
 =============================================================================*/
 
 #include "mitkSurfaceBasedRegistration.h"
-#include "niftkVTKIterativeClosestPoint.h"
+#include <niftkVTKIterativeClosestPoint.h>
+#include <vtkCellArray.h>
+#include <vtkPoints.h>
 
 namespace mitk
 {
@@ -74,11 +76,22 @@ void SurfaceBasedRegistration::Update(const mitk::PointSet::Pointer fixedNode,
   if ( m_Method == DEFORM )
   {
     //Not Implenented
-   }
+  }
 }
 
 void PointSetToPolyData (const  mitk::PointSet::Pointer PointsIn, vtkPolyData* PolyOut )
 {
+  vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+  vtkSmartPointer<vtkCellArray> verts = vtkSmartPointer<vtkCellArray>::New();
+  int numberOfPoints = PointsIn->GetSize();
+  for ( int i = 0 ; i < numberOfPoints ; i++ )
+  {
+    mitk::Point3D point = PointsIn->GetPoint(i);
+    vtkIdType id = points->InsertNextPoint(point[0],point[1],point[2]);
+    verts->InsertNextCell(1,&id);
+  }
+  PolyOut->SetPoints(points);
+  PolyOut->SetVerts(verts);
 }
 
 } // end namespace
