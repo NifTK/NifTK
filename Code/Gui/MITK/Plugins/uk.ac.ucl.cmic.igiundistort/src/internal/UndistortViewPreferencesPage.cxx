2e2b470e41eb1dde456667296b0fe74e8b4d3cf6
Johannes Totz <j.totz@ucl.ac.uk>
2013-06-13 18:21:42 +0100

    Bug #2485: plug in the basics for the prefs page

--- Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.igiundistort/src/internal/UndistortViewPreferencesPage.cxx
+++ Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.igiundistort/src/internal/UndistortViewPreferencesPage.cxx
@@ -19,30 +19,25 @@
 #include <QLabel>
 #include <QCheckBox>
 #include <QMessageBox>
+#include <QFileDialog>
 #include <QSpinBox>
 #include "ui_UndistortViewPreferencePage.h"
 #include <berryIPreferencesService.h>
 #include <berryPlatform.h>
 
-#if 0
-const std::string ImageStatisticsViewPreferencesPage::AUTO_UPDATE_NAME("auto update");
-const std::string ImageStatisticsViewPreferencesPage::ASSUME_BINARY_NAME("assume binary");
-const std::string ImageStatisticsViewPreferencesPage::REQUIRE_SAME_SIZE_IMAGE_NAME("require same size image");
-const std::string ImageStatisticsViewPreferencesPage::BACKGROUND_VALUE_NAME("background value");
-#endif
 
+//-----------------------------------------------------------------------------
+const char* UndistortViewPreferencesPage::s_DefaultCalibrationFilePathPrefsName   = "default calib file path";
+
+
+//-----------------------------------------------------------------------------
 UndistortViewPreferencesPage::UndistortViewPreferencesPage()
-#if 0
-: m_MainControl(0)
-, m_AutoUpdate(0)
-, m_AssumeBinary(0)
-, m_RequireSameSizeImage(0)
-, m_BackgroundValue(0)
-#endif
 {
 
 }
 
+
+//-----------------------------------------------------------------------------
 UndistortViewPreferencesPage::UndistortViewPreferencesPage(const UndistortViewPreferencesPage& other)
 : berry::Object(), QObject()
 {
@@ -50,75 +45,72 @@ UndistortViewPreferencesPage::UndistortViewPreferencesPage(const UndistortViewPr
   throw std::runtime_error("Copy constructor not implemented");
 }
 
+
+//-----------------------------------------------------------------------------
 UndistortViewPreferencesPage::~UndistortViewPreferencesPage()
 {
 
 }
 
+
+//-----------------------------------------------------------------------------
 void UndistortViewPreferencesPage::Init(berry::IWorkbench::Pointer )
 {
-
 }
 
+
+//-----------------------------------------------------------------------------
+void UndistortViewPreferencesPage::OnDefaultPathBrowseButtonClicked()
+{
+  QString   file = QFileDialog::getExistingDirectory(GetQtControl(), "Calibration File Path", m_DefaultCalibrationFilePath);
+  if (!file.isEmpty())
+  {
+    m_DefaultCalibrationFilePath = file;
+    m_DefaultFilePathLineEdit->setText(m_DefaultCalibrationFilePath);
+  }
+}
+
+
+//-----------------------------------------------------------------------------
 void UndistortViewPreferencesPage::CreateQtControl(QWidget* parent)
 {
   setupUi(parent);
-#if 0
-  berry::IPreferencesService::Pointer prefService
-    = berry::Platform::GetServiceRegistry()
-      .GetServiceById<berry::IPreferencesService>(berry::IPreferencesService::ID);
+  connect(m_DefaultFilePathBrowseButton, SIGNAL(clicked()), this, SLOT(OnDefaultPathBrowseButtonClicked()));
 
-  m_ImageStatisticsPreferencesNode = prefService->GetSystemPreferences()->Node("/uk.ac.ucl.cmic.imagestatistics");
+  berry::IPreferencesService::Pointer prefService = berry::Platform::GetServiceRegistry().GetServiceById<berry::IPreferencesService>(berry::IPreferencesService::ID);
+  m_UndistortPreferencesNode = prefService->GetSystemPreferences()->Node("/uk.ac.ucl.cmic.igiundistort");
 
-  m_MainControl = new QWidget(parent);
-
-  m_AutoUpdate = new QCheckBox(m_MainControl);
-  m_AssumeBinary = new QCheckBox(m_MainControl);
-  m_RequireSameSizeImage = new QCheckBox(m_MainControl);
-  m_RequireSameSizeImage->setVisible(false); // TODO - make it work by interpolating millimetre positions.
-  m_BackgroundValue = new QSpinBox(m_MainControl);
-  m_BackgroundValue->setMinimum(-10000);
-  m_BackgroundValue->setMaximum(10000);
-  m_BackgroundValue->setValue(0);
-
-  QFormLayout *formLayout = new QFormLayout;
-  formLayout->addRow( "auto-update:", m_AutoUpdate );
-  formLayout->addRow( "assume a binary mask:", m_AssumeBinary );
-  //formLayout->addRow( "require same size image and mask:", m_RequireSameSizeImage ); TODO  - make it work by interpolating millimetre positions.
-  formLayout->addRow( "background value:", m_BackgroundValue);
-
-  m_MainControl->setLayout(formLayout);
-  this->Update();
-#endif
+  // read prefs and stuff it into controls
+  Update();
 }
 
+
+//-----------------------------------------------------------------------------
 QWidget* UndistortViewPreferencesPage::GetQtControl() const
 {
   return UndistortViewPreferencePageWidget;
 }
 
+
+//-----------------------------------------------------------------------------
 bool UndistortViewPreferencesPage::PerformOk()
 {
-#if 0
-  m_ImageStatisticsPreferencesNode->PutBool(AUTO_UPDATE_NAME, m_AutoUpdate->isChecked());
-  m_ImageStatisticsPreferencesNode->PutBool(ASSUME_BINARY_NAME, m_AssumeBinary->isChecked());
-  m_ImageStatisticsPreferencesNode->PutBool(REQUIRE_SAME_SIZE_IMAGE_NAME, m_RequireSameSizeImage->isChecked());
-  m_ImageStatisticsPreferencesNode->PutInt(BACKGROUND_VALUE_NAME, m_BackgroundValue->value());
-#endif
+  m_UndistortPreferencesNode->Put(s_DefaultCalibrationFilePathPrefsName, m_DefaultFilePathLineEdit->text().toStdString());
+
   return true;
 }
 
+
+//-----------------------------------------------------------------------------
 void UndistortViewPreferencesPage::PerformCancel()
 {
 
 }
 
+
+//-----------------------------------------------------------------------------
 void UndistortViewPreferencesPage::Update()
 {
-#if 0
-  m_AutoUpdate->setChecked(m_ImageStatisticsPreferencesNode->GetBool(AUTO_UPDATE_NAME, false));
-  m_AssumeBinary->setChecked(m_ImageStatisticsPreferencesNode->GetBool(ASSUME_BINARY_NAME, true));
-  m_RequireSameSizeImage->setChecked(m_ImageStatisticsPreferencesNode->GetBool(REQUIRE_SAME_SIZE_IMAGE_NAME, true));
-  m_BackgroundValue->setValue(m_ImageStatisticsPreferencesNode->GetInt(BACKGROUND_VALUE_NAME, 0));
-#endif
+  m_DefaultCalibrationFilePath = QString::fromStdString(m_UndistortPreferencesNode->Get(s_DefaultCalibrationFilePathPrefsName, ""));
+  m_DefaultFilePathLineEdit->setText(m_DefaultCalibrationFilePath);
 }
