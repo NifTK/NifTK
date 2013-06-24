8b9d523a4fc8a805c158696065f65f8811ff3fcb
Matt Clarkson <m.clarkson@ucl.ac.uk>
2013-06-24 06:22:59 +0100

    Bug #2490: First tag tracker using image properties

--- Code/Gui/MITK/Modules/niftkOpenCV/Tracking/mitkStereoTagExtractor.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCV/Tracking/mitkStereoTagExtractor.cxx
@@ -15,7 +15,9 @@
 #include "mitkStereoTagExtractor.h"
 #include "mitkTagTrackingFacade.h"
 #include <mitkImageToOpenCVImageFilter.h>
+#include <mitkCameraIntrinsicsProperty.h>
 #include <cv.h>
+#include <Undistortion.h>
 
 namespace mitk {
 
@@ -44,10 +46,78 @@ void StereoTagExtractor::ExtractPoints(const mitk::Image::Pointer leftImage,
 {
   CvMat* leftCameraIntrinsics  = cvCreateMat(3, 3, CV_32FC1);
   CvMat* rightCameraIntrinsics  = cvCreateMat(3, 3, CV_32FC1);
+  CvMat* rightToLeftRotationMatrix  = cvCreateMat(3, 3, CV_32FC1);
   CvMat* rightToLeftRotationVector  = cvCreateMat(1, 3, CV_32FC1);
   CvMat* rightToLeftTranslationVector  = cvCreateMat(1, 3, CV_32FC1);
 
+  itk::Matrix<float, 4, 4> txf;
+  txf.SetIdentity();
 
+  niftk::Undistortion::MatrixProperty::Pointer leftToRightMatrixProp = niftk::Undistortion::MatrixProperty::New(txf);
+  mitk::CameraIntrinsicsProperty::Pointer leftIntrinsicsProp = mitk::CameraIntrinsicsProperty::New();
+  mitk::CameraIntrinsicsProperty::Pointer rightIntrinsicsProp = mitk::CameraIntrinsicsProperty::New();
+
+  leftIntrinsicsProp = dynamic_cast<mitk::CameraIntrinsicsProperty*>(leftImage->GetProperty(niftk::Undistortion::s_CameraCalibrationPropertyName).GetPointer());
+  rightIntrinsicsProp = dynamic_cast<mitk::CameraIntrinsicsProperty*>(rightImage->GetProperty(niftk::Undistortion::s_CameraCalibrationPropertyName).GetPointer());
+  leftToRightMatrixProp = dynamic_cast<niftk::Undistortion::MatrixProperty*>(rightImage->GetProperty(niftk::Undistortion::s_StereoRigTransformationPropertyName).GetPointer());
+
+  mitk::CameraIntrinsics::Pointer leftCam = mitk::CameraIntrinsics::New();
+  mitk::CameraIntrinsics::Pointer rightCam = mitk::CameraIntrinsics::New();
+
+  if (leftIntrinsicsProp.IsNotNull()
+    && rightIntrinsicsProp.IsNotNull()
+    && leftToRightMatrixProp.IsNotNull()
+  )
+  {
+    leftCam = leftIntrinsicsProp->GetValue();
+    rightCam = leftIntrinsicsProp->GetValue();
+    txf = leftToRightMatrixProp->GetValue();
+  }
+  else
+  {
+    // invent some stuff based on image dimensions
+    // ToDo: Undistortion has a bit like this.
+    unsigned int w = leftImage->GetDimension(0);
+    unsigned int h = leftImage->GetDimension(1);
+
+    mitk::Point3D::ValueType  focal[3] = {(float) std::max(w, h), (float) std::max(w, h), 1};
+    mitk::Point3D::ValueType  princ[3] = {(float) w / 2, (float) h / 2, 1};
+    mitk::Point4D::ValueType  disto[4] = {0, 0, 0, 0};
+
+    leftCam->SetIntrinsics(mitk::Point3D(focal), mitk::Point3D(princ), mitk::Point4D(disto));
+    rightCam->SetIntrinsics(mitk::Point3D(focal), mitk::Point3D(princ), mitk::Point4D(disto));
+    txf[0][3] = 10;
+  }
+
+  CV_MAT_ELEM(*leftCameraIntrinsics, float, 0, 0) = leftCam->GetFocalLengthX();
+  CV_MAT_ELEM(*leftCameraIntrinsics, float, 0, 1) = 0;
+  CV_MAT_ELEM(*leftCameraIntrinsics, float, 0, 2) = leftCam->GetPrincipalPointX();
+  CV_MAT_ELEM(*leftCameraIntrinsics, float, 1, 0) = 0;
+  CV_MAT_ELEM(*leftCameraIntrinsics, float, 1, 1) = leftCam->GetFocalLengthY();
+  CV_MAT_ELEM(*leftCameraIntrinsics, float, 1, 2) = leftCam->GetPrincipalPointY();
+  CV_MAT_ELEM(*leftCameraIntrinsics, float, 2, 0) = 0;
+  CV_MAT_ELEM(*leftCameraIntrinsics, float, 2, 1) = 0;
+  CV_MAT_ELEM(*leftCameraIntrinsics, float, 2, 2) = 1;
+
+  CV_MAT_ELEM(*rightCameraIntrinsics, float, 0, 0) = rightCam->GetFocalLengthX();
+  CV_MAT_ELEM(*rightCameraIntrinsics, float, 0, 1) = 0;
+  CV_MAT_ELEM(*rightCameraIntrinsics, float, 0, 2) = rightCam->GetPrincipalPointX();
+  CV_MAT_ELEM(*rightCameraIntrinsics, float, 1, 0) = 0;
+  CV_MAT_ELEM(*rightCameraIntrinsics, float, 1, 1) = rightCam->GetFocalLengthY();
+  CV_MAT_ELEM(*rightCameraIntrinsics, float, 1, 2) = rightCam->GetPrincipalPointY();
+  CV_MAT_ELEM(*rightCameraIntrinsics, float, 2, 0) = 0;
+  CV_MAT_ELEM(*rightCameraIntrinsics, float, 2, 1) = 0;
+  CV_MAT_ELEM(*rightCameraIntrinsics, float, 2, 2) = 1;
+
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j  = 0; j < 3; j++)
+    {
+      CV_MAT_ELEM(*rightToLeftRotationMatrix, float, i, j) = txf[i][j];
+    }
+    CV_MAT_ELEM(*rightToLeftTranslationVector, float, 0, i) = txf[i][3];
+  }
+  cvRodrigues2(rightToLeftRotationMatrix, rightToLeftRotationVector);
 
   this->ExtractPoints(leftImage, rightImage,
                       minSize, maxSize,
@@ -58,6 +128,7 @@ void StereoTagExtractor::ExtractPoints(const mitk::Image::Pointer leftImage,
   cvReleaseMat(&leftCameraIntrinsics);
   cvReleaseMat(&rightCameraIntrinsics);
   cvReleaseMat(&rightToLeftRotationVector);
+  cvReleaseMat(&rightToLeftRotationMatrix);
   cvReleaseMat(&rightToLeftTranslationVector);
 }
 
