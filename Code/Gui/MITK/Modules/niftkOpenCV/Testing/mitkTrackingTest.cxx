2cc3cdd444839428d6780c7cbf02baeae13ef37a
Steve Thompson <s.thompson@ucl.ac.uk>
2013-07-18 12:08:30 +0100

    Feature #2632 Got image display running and started work on corner extraction pipeline

--- Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkTrackingTest.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkTrackingTest.cxx
@@ -45,8 +45,76 @@ int mitkTrackingTest ( int argc, char * argv[] )
 
 
   //get the video and show it
-  CvCapture *capture;
-  capture=cvCreateFileCapture(argv[1]);
+  CvCapture *capture = 0 ;
+  MITK_INFO << "Opening " << inputVideo;
+  capture=cvCreateFileCapture(inputVideo.c_str());
+  //capture=cvCreateCameraCapture(CV_CAP_V4L);
+  
+  if ( ! capture ) 
+  {
+    MITK_WARN << "Failed to open " << inputVideo;
+  }
+  else 
+  {
+    MITK_INFO << "Opened OK";
+  }
 
+  cvNamedWindow( "Left Channel",CV_WINDOW_AUTOSIZE);
+  cvNamedWindow( "Right Channel",CV_WINDOW_AUTOSIZE);
+  cvNamedWindow( "Left Processed",CV_WINDOW_AUTOSIZE);
+
+  int key = 0 ;
+  IplImage *leftframe;
+  IplImage *rightframe;
+  IplImage *smallleft;
+  IplImage *smallright;
+
+  IplImage *leftprocessed;
+  IplImage *rightprocessed;
+  IplImage *leftprocessed_temp;
+  IplImage *rightprocessed_temp;
+  IplImage *smallleftprocessed;
+  IplImage *smallrightprocessed;
+
+  smallleft = cvCreateImage( cvSize(640,360), 8, 3 );
+  smallright = cvCreateImage( cvSize(640,360), 8, 3 );
+  smallleftprocessed = cvCreateImage( cvSize(640,360), 8, 1 );
+  smallrightprocessed = cvCreateImage( cvSize(640,360), 8, 1 );
+  leftprocessed = cvCreateImage( cvSize(1920,540), 8, 1 );
+  rightprocessed = cvCreateImage( cvSize(1920,540), 8, 1 );
+  leftprocessed_temp = cvCreateImage( cvSize(1920,540), 8, 1 );
+  rightprocessed_temp = cvCreateImage( cvSize(1920,540), 8, 1 );
+  while ( key != 'q' )
+  {
+    leftframe = cvQueryFrame(capture);
+    rightframe = cvQueryFrame(capture);
+
+    cvCvtColor( leftframe, leftprocessed, CV_BGR2GRAY );
+    cvCvtColor( rightframe, rightprocessed, CV_BGR2GRAY );
+    
+                                 
+    cvEqualizeHist( leftprocessed, leftprocessed_temp );
+    leftprocessed = leftprocessed_temp;
+    cvEqualizeHist( rightprocessed, rightprocessed_temp );
+    rightprocessed = rightprocessed_temp;
+                                              
+
+    //cvArr* leftCorners = new cvArr;
+    //cvPreCornerDetect (leftframe, leftCorners);
+    
+    cvResize (leftframe, smallleft,CV_INTER_NN);
+    cvResize (rightframe, smallright,CV_INTER_NN);
+    
+    cvResize (leftprocessed, smallleftprocessed,CV_INTER_NN);
+
+    cvShowImage("Left Channel", smallleft);
+    cvShowImage("Right Channel", smallright);
+    cvShowImage("Left Processed", smallleftprocessed);
+    key = cvWaitKey (50);
+  }
+  
+  cvDestroyWindow("Left Channel");
+  cvDestroyWindow("Right Channel");
+  cvReleaseCapture (&capture);
   return 0;
 }
