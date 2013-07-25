879f6fa43b04f6798a93ec6b3401d0431246287e
Steve Thompson <s.thompson@ucl.ac.uk>
2013-07-25 13:24:14 +0100

    Feature #2641 implemented initialisation and timing error test

--- Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkVideoTrackerMatchingTest.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkVideoTrackerMatchingTest.cxx
@@ -18,6 +18,11 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/core_c.h>
 
+bool CheckTrackerMatrix (cv::Mat)
+{
+  double Error = 0.0;
+}
+
 
 //-----------------------------------------------------------------------------
 int mitkVideoTrackerMatchingTest(int argc, char** argv)
@@ -26,5 +31,13 @@ int mitkVideoTrackerMatchingTest(int argc, char** argv)
   mitk::VideoTrackerMatching::Pointer Matcher = mitk::VideoTrackerMatching::New();
   Matcher->Initialise(argv[1]);
 
+  //check it initialised, check it gets the right matrix with the right time error
+  MITK_TEST_CONDITION_REQUIRED (Matcher->IsReady() , "Testing that VideoTrackerMatcherInitialised OK"); 
+  
+  long *TimingError = new long;
+  cv::Mat TrackingMatrix = Matcher->GetTrackerMatrix(20, TimingError, 0);
+
+  //1374066239681720400-1374066239683720400 = -2000000
+  MITK_TEST_CONDITION_REQUIRED(*TimingError == -2000000, "Testing Timing error");
   return EXIT_SUCCESS;
 }
