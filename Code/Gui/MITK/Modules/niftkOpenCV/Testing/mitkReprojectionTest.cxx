8b18df4800b6ca346ed337d5536c81a4408552fb
Steve Thompson <s.thompson@ucl.ac.uk>
2013-08-06 20:02:38 +0100

    Feature #2654 further testing

--- Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkReprojectionTest.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkReprojectionTest.cxx
@@ -39,7 +39,7 @@ int mitkReprojectionTest ( int argc, char * argv[] )
   cv::Mat rightCameraIntrinsic = cv::Mat(3,3,CV_32FC1);
   cv::Mat rightCameraDistortion = cv::Mat(5,1,CV_32FC1);
   cv::Mat rightToLeftRotationMatrix = cv::Mat(3,3,CV_32FC1);
-  cv::Mat rightToLeftTranslationVector = cv::Mat(1,3,CV_32FC1);
+  cv::Mat rightToLeftTranslationVector = cv::Mat(3,1,CV_32FC1);
   cv::Mat leftCameraToTracker = cv::Mat(4,4,CV_32FC1);
   
   mitk::LoadStereoCameraParametersFromDirectory (argv[1],
@@ -52,9 +52,21 @@ int mitkReprojectionTest ( int argc, char * argv[] )
   CvMat* output2DPointsLeft = NULL ;
   CvMat* output2DPointsRight = NULL;
   
-  cv::Mat leftCameraWorldPoints = cv::Mat (20,3,CV_32FC1);
-  cv::Mat leftCameraWorldNormals = cv::Mat (20,3,CV_32FC1);
-
+  cv::Mat leftCameraWorldPoints = cv::Mat (25,3,CV_32FC1);
+  cv::Mat leftCameraWorldNormals = cv::Mat (25,3,CV_32FC1);
+  
+  for ( int row = 0 ; row < 5 ; row ++ ) 
+  {
+    for ( int col = 0 ; col < 5 ; col ++ )
+    {
+      leftCameraWorldPoints.at<float>(row * 5 + col, 0) = -100 + (col * 50);
+      leftCameraWorldPoints.at<float>(row * 5 + col, 1) = -100 + (row * 50);
+      leftCameraWorldPoints.at<float>(row * 5 + col, 2) = 150 + (row + col) * 1.0;
+      leftCameraWorldNormals.at<float>(row*5 + col, 0 ) = 0;
+      leftCameraWorldNormals.at<float>(row*5 + col, 1 ) = 0;
+      leftCameraWorldNormals.at<float>(row*5 + col, 2 ) = -1.0;
+    }
+  }
   std::vector<int> Points = mitk::ProjectVisible3DWorldPointsToStereo2D
     ( leftCameraWorldPoints,leftCameraWorldNormals,
       leftCameraPositionToFocalPointUnitVector,
@@ -65,6 +77,20 @@ int mitkReprojectionTest ( int argc, char * argv[] )
       outputLeftCameraWorldNormalsIn3D,
       output2DPointsLeft,
       output2DPointsRight);
+
+  for ( int row = 0 ; row < 5 ; row ++ ) 
+  {
+    for ( int col = 0 ; col < 5 ; col ++ )
+    {
+      MITK_INFO << "[" << leftCameraWorldPoints.at<float>(row*5 + col,0) << "," 
+        << leftCameraWorldPoints.at<float>(row*5 + col,1) << ","
+        << leftCameraWorldPoints.at<float>(row*5 + col,2) << "] => ("  
+        << CV_MAT_ELEM (*output2DPointsLeft ,float, row*5 + col,0) << "," 
+        << CV_MAT_ELEM (*output2DPointsLeft, float,row*5 + col,1) << ") (" 
+        << CV_MAT_ELEM (*output2DPointsRight,float,row*5 + col,0) << "," 
+        << CV_MAT_ELEM (*output2DPointsRight,float,row*5 + col,1) << ") => "; 
+    }
+  }
 
   return 0;
 }
