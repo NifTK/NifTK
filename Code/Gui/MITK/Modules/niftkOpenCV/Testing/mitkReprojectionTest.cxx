5d41afb4591def4662d5a626e830a960e918edf9
Steve Thompson <s.thompson@ucl.ac.uk>
2013-08-07 13:45:06 +0100

    Feature #2654 test fairly complete, currently failing

--- Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkReprojectionTest.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkReprojectionTest.cxx
@@ -31,7 +31,7 @@
 
 int mitkReprojectionTest ( int argc, char * argv[] )
 {
-
+  MITK_TEST_BEGIN("mitkReprojectionTest");
   cv::Mat leftCameraPositionToFocalPointUnitVector = cv::Mat(1,3,CV_32FC1);
   cv::Mat leftCameraIntrinsic = cv::Mat(3,3,CV_32FC1);
   cv::Mat leftCameraDistortion = cv::Mat(5,1,CV_32FC1);
@@ -166,5 +166,66 @@ int mitkReprojectionTest ( int argc, char * argv[] )
     }
   }
 
-  return 0;
+  float xErrorMean_m1 = 0.0;
+  float yErrorMean_m1 = 0.0;
+  float zErrorMean_m1 = 0.0;
+  float xErrorMean_m2 = 0.0;
+  float yErrorMean_m2 = 0.0;
+  float zErrorMean_m2 = 0.0;
+  float errorRMS_m1 = 0.0;
+  float errorRMS_m2 = 0.0;
+
+  for ( int i = 0 ; i < numberOfPoints ; i ++ ) 
+  {
+    float xError_m1 = CV_MAT_ELEM (*leftCameraTriangulatedWorldPoints_m1,float, i, 0) - 
+      leftCameraWorldPoints.at<float>(i,0);
+    float yError_m1 = CV_MAT_ELEM (*leftCameraTriangulatedWorldPoints_m1,float, i, 1) - 
+      leftCameraWorldPoints.at<float>(i,1);
+    float zError_m1 = CV_MAT_ELEM (*leftCameraTriangulatedWorldPoints_m1,float, i, 2) - 
+      leftCameraWorldPoints.at<float>(i,2);
+    float xError_m2 = leftCameraTriangulatedWorldPoints_m2[i].x -  
+      leftCameraWorldPoints.at<float>(i,0);
+    float yError_m2 = leftCameraTriangulatedWorldPoints_m2[i].y -  
+      leftCameraWorldPoints.at<float>(i,1);
+    float zError_m2 = leftCameraTriangulatedWorldPoints_m2[i].z -  
+      leftCameraWorldPoints.at<float>(i,2);
+    float error_m1 = (xError_m1 * xError_m1 + yError_m1 * yError_m1 + zError_m1 * zError_m1);
+    float error_m2 = (xError_m2 * xError_m2 + yError_m2 * yError_m2 + zError_m2 * zError_m2);
+    
+    xErrorMean_m1 += xError_m1;
+    yErrorMean_m1 += yError_m1;
+    zErrorMean_m1 += zError_m1;
+    xErrorMean_m2 += xError_m2;
+    yErrorMean_m2 += yError_m2;
+    zErrorMean_m2 += zError_m2;
+    errorRMS_m1 += error_m1;
+    errorRMS_m2 += error_m2;
+  }
+  xErrorMean_m1 /= numberOfPoints;
+  yErrorMean_m1 /= numberOfPoints;
+  zErrorMean_m1 /= numberOfPoints;
+  xErrorMean_m2 /= numberOfPoints;
+  yErrorMean_m2 /= numberOfPoints;
+  zErrorMean_m2 /= numberOfPoints;
+  errorRMS_m1 = sqrt(errorRMS_m1/numberOfPoints);
+  errorRMS_m2 = sqrt(errorRMS_m2/numberOfPoints);
+  
+  MITK_INFO << "Mean x error c wrapper = " <<  xErrorMean_m1; 
+  MITK_INFO << "Mean y error c wrapper = " <<  yErrorMean_m1; 
+  MITK_INFO << "Mean z error c wrapper = " <<  zErrorMean_m1; 
+  MITK_INFO << "RMS error c+wrapper = " <<  errorRMS_m1; 
+  MITK_INFO << "Mean x error c++ wrapper = " <<  xErrorMean_m2; 
+  MITK_INFO << "Mean y error c++ wrapper = " <<  yErrorMean_m2; 
+  MITK_INFO << "Mean z error c++ wrapper = " <<  zErrorMean_m2; 
+  MITK_INFO << "RMS error c++ wrapper = " <<  errorRMS_m2; 
+  MITK_TEST_CONDITION (fabs(xErrorMean_m1) < 1e-3 , "Testing x error mean value for c wrapper method");
+  MITK_TEST_CONDITION (fabs(yErrorMean_m1) < 1e-3 , "Testing y error mean value for c wrapper method");
+  MITK_TEST_CONDITION (fabs(zErrorMean_m1) < 1e-3 , "Testing z error mean value for c wrapper method");
+  MITK_TEST_CONDITION (errorRMS_m1 < 1.0 , "Testing RMS error value for c method");
+  MITK_TEST_CONDITION (fabs(xErrorMean_m2) < 1e-3 , "Testing x error mean value for c++ method");
+  MITK_TEST_CONDITION (fabs(yErrorMean_m2) < 1e-3 , "Testing y error mean value for c++ method");
+  MITK_TEST_CONDITION (fabs(zErrorMean_m2) < 1e-3 , "Testing z error mean value for c++ method");
+  MITK_TEST_CONDITION (errorRMS_m2 < 1.0 , "Testing RMS error value for c++ method");
+
+  MITK_TEST_END();
 }
