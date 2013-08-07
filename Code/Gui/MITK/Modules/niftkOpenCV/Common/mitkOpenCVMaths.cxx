27504a703a792d71cf6035871e24ba7004de71f0
Matt Clarkson <m.clarkson@ucl.ac.uk>
2013-08-07 17:38:15 +0100

    Bug #2667: Convert matrix functions to C++ style, and double precision consistently

--- Code/Gui/MITK/Modules/niftkOpenCV/Common/mitkOpenCVMaths.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCV/Common/mitkOpenCVMaths.cxx
@@ -87,9 +87,9 @@ std::vector<cv::Point3d> PointSetToVector(const mitk::PointSet::Pointer& pointSe
 
 
 //-----------------------------------------------------------------------------
-bool IsCloseToZero(const double& value)
+bool IsCloseToZero(const double& value, const double& tolerance)
 {
-  if (fabs(value) < 0.000001)
+  if (fabs(value) < tolerance)
   {
     return true;
   }
@@ -318,112 +318,79 @@ void CopyToOpenCVMatrix(const vtkMatrix4x4& matrix, cv::Matx44d& openCVMatrix)
 
 
 //-----------------------------------------------------------------------------
-void GenerateEulerRxMatrix(const double& rx, CvMat &matrix3x3)
+void GenerateEulerRxMatrix(const double& rx, cv::Matx33d& matrix3x3)
 {
   double cosRx = cos(rx);
   double sinRx = sin(rx);
-  cvSetIdentity(&matrix3x3);
-  CV_MAT_ELEM(matrix3x3, float, 1, 1) = cosRx;
-  CV_MAT_ELEM(matrix3x3, float, 1, 2) = sinRx;
-  CV_MAT_ELEM(matrix3x3, float, 2, 1) = -sinRx;
-  CV_MAT_ELEM(matrix3x3, float, 2, 2) = cosRx;
+  matrix3x3.eye();
+  matrix3x3(1, 1) = cosRx;
+  matrix3x3(1, 2) = sinRx;
+  matrix3x3(2, 1) = -sinRx;
+  matrix3x3(2, 2) = cosRx;
 }
 
 
 //-----------------------------------------------------------------------------
-void GenerateEulerRyMatrix(const double& ry, CvMat &matrix3x3)
+void GenerateEulerRyMatrix(const double& ry, cv::Matx33d& matrix3x3)
 {
   double cosRy = cos(ry);
   double sinRy = sin(ry);
-  cvSetIdentity(&matrix3x3);
-  CV_MAT_ELEM(matrix3x3, float, 0, 0) = cosRy;
-  CV_MAT_ELEM(matrix3x3, float, 0, 2) = -sinRy;
-  CV_MAT_ELEM(matrix3x3, float, 2, 0) = sinRy;
-  CV_MAT_ELEM(matrix3x3, float, 2, 2) = cosRy;
+  matrix3x3.eye();
+  matrix3x3(0, 0) = cosRy;
+  matrix3x3(0, 2) = -sinRy;
+  matrix3x3(2, 0) = sinRy;
+  matrix3x3(2, 2) = cosRy;
 }
 
 
 //-----------------------------------------------------------------------------
-void GenerateEulerRzMatrix(const double& rz, CvMat &matrix3x3)
+void GenerateEulerRzMatrix(const double& rz, cv::Matx33d& matrix3x3)
 {
   double cosRz = cos(rz);
   double sinRz = sin(rz);
-  cvSetIdentity(&matrix3x3);
-  CV_MAT_ELEM(matrix3x3, float, 0, 0) = cosRz;
-  CV_MAT_ELEM(matrix3x3, float, 0, 1) = sinRz;
-  CV_MAT_ELEM(matrix3x3, float, 1, 0) = -sinRz;
-  CV_MAT_ELEM(matrix3x3, float, 1, 1) = cosRz;
+  matrix3x3.eye();
+  matrix3x3(0, 0) = cosRz;
+  matrix3x3(0, 1) = sinRz;
+  matrix3x3(1, 0) = -sinRz;
+  matrix3x3(1, 1) = cosRz;
 }
 
 
 //-----------------------------------------------------------------------------
-void GenerateEulerRotationMatrix(const double& rx, const double& ry, const double& rz, CvMat &matrix3x3)
+void GenerateEulerRotationMatrix(const double& rx, const double& ry, const double& rz, cv::Matx33d& matrix3x3)
 {
-  CvMat *rotationAboutX = cvCreateMat(3, 3, CV_32FC1);
-  CvMat *rotationAboutY = cvCreateMat(3, 3, CV_32FC1);
-  CvMat *rotationAboutZ = cvCreateMat(3, 3, CV_32FC1);
-  CvMat *rotationRyRx   = cvCreateMat(3, 3, CV_32FC1);
+  cv::Matx33d rotationAboutX;
+  cv::Matx33d rotationAboutY;
+  cv::Matx33d rotationAboutZ;
 
-  GenerateEulerRxMatrix(rx, *rotationAboutX);
-  GenerateEulerRyMatrix(ry, *rotationAboutY);
-  GenerateEulerRzMatrix(rz, *rotationAboutZ);
+  GenerateEulerRxMatrix(rx, rotationAboutX);
+  GenerateEulerRyMatrix(ry, rotationAboutY);
+  GenerateEulerRzMatrix(rz, rotationAboutZ);
 
-  cvGEMM(rotationAboutY, rotationAboutX, 1, NULL, 0, rotationRyRx);
-  cvGEMM(rotationAboutZ, rotationRyRx, 1, NULL, 0, &matrix3x3);
-
-  cvReleaseMat(&rotationAboutX);
-  cvReleaseMat(&rotationAboutY);
-  cvReleaseMat(&rotationAboutZ);
-  cvReleaseMat(&rotationRyRx);
+  matrix3x3 = (rotationAboutZ * (rotationAboutY * rotationAboutX));
 }
 
 
 //-----------------------------------------------------------------------------
-CvMat* ConvertEulerToRodrigues(
+cv::Matx13d ConvertEulerToRodrigues(
     const double& rx,
     const double& ry,
     const double& rz
     )
 {
-  CvMat *rotationMatrix = cvCreateMat(3, 3, CV_32FC1);
-  CvMat *rotationVector = cvCreateMat(1, 3, CV_32FC1);
 
-  GenerateEulerRotationMatrix(rx, ry, rz, *rotationMatrix);
-  cvRodrigues2(rotationMatrix, rotationVector);
+  cv::Matx33d rotationMatrix;
+  cv::Matx13d rotationVector;
 
-  cvReleaseMat(&rotationMatrix);
+  GenerateEulerRotationMatrix(rx, ry, rz, rotationMatrix);
+  cv::Rodrigues(rotationMatrix, rotationVector);
+
   return rotationVector;
 }
 
 
 //-----------------------------------------------------------------------------
-CvMat* ConvertRodriguesToEuler(
-    const CvMat& rotationVector
-    )
-{
-  CvPoint3D64f vec;
-  CvMat *result = cvCreateMat(1, 3, CV_32FC1);
-  CvMat *rotationMatrix = cvCreateMat(3, 3, CV_32FC1);
-  CvMat *r = cvCreateMat(3, 3, CV_32FC1);
-  CvMat *q = cvCreateMat(3, 3, CV_32FC1);
-
-  cvRodrigues2(&rotationVector, rotationMatrix);
-  cvRQDecomp3x3(rotationMatrix, r, q, NULL, NULL, NULL, &vec);
-
-  CV_MAT_ELEM(*result, float, 0, 0) = vec.x;
-  CV_MAT_ELEM(*result, float, 0, 1) = vec.y;
-  CV_MAT_ELEM(*result, float, 0, 2) = vec.z;
-
-  cvReleaseMat(&rotationMatrix);
-  cvReleaseMat(&r);
-  cvReleaseMat(&q);
-
-  return result;
-}
-
-
-//-----------------------------------------------------------------------------
-CvMat* Construct4x4TransformationMatrix(
+cv::Matx44d Construct4x4TransformationMatrix(
     const double& rx,
     const double& ry,
     const double& rz,
@@ -432,31 +399,30 @@ CvMat* Construct4x4TransformationMatrix(
     const double& tz
     )
 {
-  CvMat *result = cvCreateMat(4, 4, CV_32FC1);
-  CvMat *rotationMatrix = cvCreateMat(3, 3, CV_32FC1);
+  cv::Matx44d transformation;
+  cv::Matx33d rotationMatrix;
 
-  GenerateEulerRotationMatrix(rx, ry, rz, *rotationMatrix);
+  GenerateEulerRotationMatrix(rx, ry, rz, rotationMatrix);
 
-  cvSetIdentity(result);
+  transformation.eye();
+
   for (int i = 0; i < 3; i++)
   {
     for (int j = 0; j < 3; j++)
     {
-      CV_MAT_ELEM(*result, float, i, j) = CV_MAT_ELEM(*rotationMatrix, float, i, j);
+      transformation(i, j) = rotationMatrix(i, j);
     }
   }
-  CV_MAT_ELEM(*result, float, 0, 3) = tx;
-  CV_MAT_ELEM(*result, float, 1, 3) = ty;
-  CV_MAT_ELEM(*result, float, 2, 3) = tz;
+  transformation(0, 3) = tx;
+  transformation(1, 3) = ty;
+  transformation(2, 3) = tz;
 
-  cvReleaseMat(&rotationMatrix);
-
-  return result;
+  return transformation;
 }
 
 
 //-----------------------------------------------------------------------------
-CvMat* Construct4x4TransformationMatrixFromDegrees(
+cv::Matx44d Construct4x4TransformationMatrixFromDegrees(
     const double& rx,
     const double& ry,
     const double& rz,
@@ -473,52 +439,6 @@ CvMat* Construct4x4TransformationMatrixFromDegrees(
   radians[2] = rz * pi / 180;
 
   return Construct4x4TransformationMatrix(radians[0], radians[1], radians[2], tx, ty, tz);
-}
-
-
-//-----------------------------------------------------------------------------
-void TransformBy4x4Matrix(
-    const CvMat &input3D,
-    const CvMat &matrix4x4,
-    const bool &isNormals,
-    CvMat& output3DPoints
-    )
-{
-  CvMat *rotationMatrix = cvCreateMat(3, 3, CV_32FC1);
-  cvSetIdentity(rotationMatrix);
-
-  CvMat *translationMatrix = cvCreateMat(1, 3, CV_32FC1);
-  cvSetZero(translationMatrix);
-
-  CvMat *transposedTransformedPoints = cvCreateMat(input3D.cols, input3D.rows, CV_32FC1);
-
-  for (int i = 0; i < 3; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      CV_MAT_ELEM(*rotationMatrix, float, i, j) = CV_MAT_ELEM(matrix4x4, float, i, j);
-    }
-  }
-  CV_MAT_ELEM(*translationMatrix, float, 0, 0) = CV_MAT_ELEM(matrix4x4, float, 0, 3);
-  CV_MAT_ELEM(*translationMatrix, float, 0, 1) = CV_MAT_ELEM(matrix4x4, float, 1, 3);
-  CV_MAT_ELEM(*translationMatrix, float, 0, 2) = CV_MAT_ELEM(matrix4x4, float, 2, 3);
-
-  cvGEMM(rotationMatrix, &input3D, 1, NULL, 0, transposedTransformedPoints, CV_GEMM_B_T); // ie. [3x3][Nx3]^T = [3xN].
-  cvTranspose(transposedTransformedPoints, &output3DPoints);
-
-  if (!isNormals) // if its a normal, we would subtract the transformed origin, so
-  {
-    for (int i = 0; i < input3D.rows; i++)
-    {
-      CV_MAT_ELEM(output3DPoints, float, i, 0) = CV_MAT_ELEM(output3DPoints, float, i, 0) + CV_MAT_ELEM(*translationMatrix, float, 0, 0);
-      CV_MAT_ELEM(output3DPoints, float, i, 1) = CV_MAT_ELEM(output3DPoints, float, i, 1) + CV_MAT_ELEM(*translationMatrix, float, 0, 1);
-      CV_MAT_ELEM(output3DPoints, float, i, 2) = CV_MAT_ELEM(output3DPoints, float, i, 2) + CV_MAT_ELEM(*translationMatrix, float, 0, 2);
-    }
-  }
-
-  cvReleaseMat(&rotationMatrix);
-  cvReleaseMat(&translationMatrix);
-  cvReleaseMat(&transposedTransformedPoints);
 }
 
 //-----------------------------------------------------------------------------
--- Code/Gui/MITK/Modules/niftkOpenCV/Common/mitkOpenCVMaths.h
+++ Code/Gui/MITK/Modules/niftkOpenCV/Common/mitkOpenCVMaths.h
@@ -30,74 +30,87 @@ namespace mitk {
 /**
  * \brief Calculates the centroid of a vector of points.
  */
-cv::Point3d GetCentroid(const std::vector<cv::Point3d>& points);
+extern "C++" NIFTKOPENCV_EXPORT cv::Point3d GetCentroid(const std::vector<cv::Point3d>& points);
+
 
 /**
  * \brief Subtracts a point (e.g. the centroid) from a list of points.
  */
-std::vector<cv::Point3d> SubtractPointFromPoints(const std::vector<cv::Point3d> listOfPoints, const cv::Point3d& point);
+extern "C++" NIFTKOPENCV_EXPORT std::vector<cv::Point3d> SubtractPointFromPoints(const std::vector<cv::Point3d> listOfPoints, const cv::Point3d& point);
+
 
 /**
- * \brief Converts mitk::PointSet to vector of cv::Point3d, but you lose the point ID.
+ * \brief Converts mitk::PointSet to vector of cv::Point3d, but you lose the point ID contained within the mitk::PointSet.
  */
-std::vector<cv::Point3d> PointSetToVector(const mitk::PointSet::Pointer& pointSet);
+extern "C++" NIFTKOPENCV_EXPORT std::vector<cv::Point3d> PointSetToVector(const mitk::PointSet::Pointer& pointSet);
+
 
 /**
- * \brief Returns true if fabs(value) is less than a small tolerance (see code).
+ * \brief Returns true if fabs(value) is less than a small tolerance, which defaults to 0.000001.
  */
-bool IsCloseToZero(const double& value);
+extern "C++" NIFTKOPENCV_EXPORT bool IsCloseToZero(const double& value, const double& tolerance = 0.000001);
+
 
 /**
  * \brief Haven't found a direct method to do this yet.
  */
-void MakeIdentity(cv::Matx44d& outputMatrix);
+extern "C++" NIFTKOPENCV_EXPORT void MakeIdentity(cv::Matx44d& outputMatrix);
+
 
 /**
  * \brief Calculates 1/N Sum (q_i * qPrime_i^t) where q_i and qPrime_i are column vectors, so the product is a 3x3 matrix.
  * \see Least-Squares Fitting of two, 3-D Point Sets, Arun, 1987, DOI=10.1109/TPAMI.1987.4767965, matrix H.
  */
-cv::Matx33d CalculateCrossCovarianceH(const std::vector<cv::Point3d>& q, const std::vector<cv::Point3d>& qPrime);
+extern "C++" NIFTKOPENCV_EXPORT cv::Matx33d CalculateCrossCovarianceH(const std::vector<cv::Point3d>& q, const std::vector<cv::Point3d>& qPrime);
+
 
 /**
  * \brief Helper method to do the main point based registration, and handle error conditions.
  */
-bool DoSVDPointBasedRegistration(const std::vector<cv::Point3d>& fixedPoints,
-                                 const std::vector<cv::Point3d>& movingPoints,
-                                 cv::Matx33d& H,
-                                 cv::Point3d &p,
-                                 cv::Point3d& pPrime,
-                                 cv::Matx44d& outputMatrix,
-                                 double &fiducialRegistrationError);
+extern "C++" NIFTKOPENCV_EXPORT bool DoSVDPointBasedRegistration(const std::vector<cv::Point3d>& fixedPoints,
+  const std::vector<cv::Point3d>& movingPoints,
+  cv::Matx33d& H,
+  cv::Point3d &p,
+  cv::Point3d& pPrime,
+  cv::Matx44d& outputMatrix,
+  double &fiducialRegistrationError
+  );
+
 
 /**
  * \brief Calculates Fiducial Registration Error by multiplying the movingPoints by the matrix, and comparing with fixedPoints.
  */
-double CalculateFiducialRegistrationError(const std::vector<cv::Point3d>& fixedPoints,
-                                          const std::vector<cv::Point3d>& movingPoints,
-                                          const cv::Matx44d& matrix
-                                          );
+extern "C++" NIFTKOPENCV_EXPORT double CalculateFiducialRegistrationError(const std::vector<cv::Point3d>& fixedPoints,
+  const std::vector<cv::Point3d>& movingPoints,
+  const cv::Matx44d& matrix
+  );
+
 
 /**
  * \brief Converts format of input to call the other CalculateFiducialRegistrationError method.
  */
-NIFTKOPENCV_EXPORT double CalculateFiducialRegistrationError(const mitk::PointSet::Pointer& fixedPointSet,
-                                                             const mitk::PointSet::Pointer& movingPointSet,
-                                                             vtkMatrix4x4& vtkMatrix);
+extern "C++" NIFTKOPENCV_EXPORT double CalculateFiducialRegistrationError(const mitk::PointSet::Pointer& fixedPointSet,
+  const mitk::PointSet::Pointer& movingPointSet,
+  vtkMatrix4x4& vtkMatrix
+  );
+
 
 /**
  * \brief Simply copies the translation vector and rotation matrix into the 4x4 matrix.
  */
-void Setup4x4Matrix(const cv::Matx31d& translation, const cv::Matx33d& rotation, cv::Matx44d& matrix);
+extern "C++" NIFTKOPENCV_EXPORT void Setup4x4Matrix(const cv::Matx31d& translation, const cv::Matx33d& rotation, cv::Matx44d& matrix);
+
 
 /**
  * \brief Copies matrix to vtkMatrix.
  */
-void CopyToVTK4x4Matrix(const cv::Matx44d& matrix, vtkMatrix4x4& vtkMatrix);
+extern "C++" NIFTKOPENCV_EXPORT void CopyToVTK4x4Matrix(const cv::Matx44d& matrix, vtkMatrix4x4& vtkMatrix);
+
 
 /**
  * \brief Copies matrix to openCVMatrix.
  */
-void CopyToOpenCVMatrix(const vtkMatrix4x4& matrix, cv::Matx44d& openCVMatrix);
+extern "C++" NIFTKOPENCV_EXPORT void CopyToOpenCVMatrix(const vtkMatrix4x4& matrix, cv::Matx44d& openCVMatrix);
 
 
 /**
@@ -105,7 +118,7 @@ void CopyToOpenCVMatrix(const vtkMatrix4x4& matrix, cv::Matx44d& openCVMatrix);
  * \param rx angle in radians
  * \param matrix3x3 pre-allocated [3x3] matrix
  */
-void GenerateEulerRxMatrix(const double& rx, CvMat &matrix3x3);
+extern "C++" NIFTKOPENCV_EXPORT void GenerateEulerRxMatrix(const double& rx, cv::Matx33d& matrix3x3);
 
 
 /**
@@ -113,7 +126,7 @@ void GenerateEulerRxMatrix(const double& rx, CvMat &matrix3x3);
  * \param ry angle in radians
  * \param matrix3x3 pre-allocated [3x3] matrix
  */
-void GenerateEulerRyMatrix(const double& ry, CvMat &matrix3x3);
+extern "C++" NIFTKOPENCV_EXPORT void GenerateEulerRyMatrix(const double& ry, cv::Matx33d& matrix3x3);
 
 
 /**
@@ -121,7 +134,7 @@ void GenerateEulerRyMatrix(const double& ry, CvMat &matrix3x3);
  * \param rz angle in radians
  * \param matrix3x3 pre-allocated [3x3] matrix
  */
-void GenerateEulerRzMatrix(const double& rz, CvMat &matrix3x3);
+extern "C++" NIFTKOPENCV_EXPORT void GenerateEulerRzMatrix(const double& rz, cv::Matx33d& matrix3x3);
 
 
 /**
@@ -131,30 +144,21 @@ void GenerateEulerRzMatrix(const double& rz, CvMat &matrix3x3);
  * \param rz angle in radians
  * \param matrix3x3 pre-allocated [3x3] matrix
  */
-void GenerateEulerRotationMatrix(const double& rx, const double& ry, const double& rz, CvMat &matrix3x3);
+extern "C++" NIFTKOPENCV_EXPORT void GenerateEulerRotationMatrix(const double& rx, const double& ry, const double& rz, cv::Matx33d& matrix3x3);
+
 
 /**
  * \brief Converts Euler angles in radians to the Rodrigues rotation vector (axis-angle convention) mentioned in OpenCV.
  * \param rx Euler angle rotation about x-axis in radians
  * \param ry Euler angle rotation about y-axis in radians
  * \param rz Euler angle rotation about z-axis in radians
- * \return A new [1x3] matrix that the caller must then de-allocate.
+ * \return A new [1x3] matrix
  */
-CvMat* ConvertEulerToRodrigues(
-    const double& rx,
-    const double& ry,
-    const double& rz
-    );
-
-
-/**
- * \brief Converts from the Rodrigues rotation vector mentioned in OpenCV to 3 Euler angles in radians.
- * \param rotationVector a [1x3] Rodrigues rotation vector
- * \return A new [1x3] matrix that the caller must then de-allocate containing rx, ry, rz in radians, as a result of calling OpenCV's RQDecomp3x3.
- */
-CvMat* ConvertRodriguesToEuler(
-    const CvMat& rotationVector
-    );
+extern "C++" NIFTKOPENCV_EXPORT cv::Matx13d ConvertEulerToRodrigues(
+  const double& rx,
+  const double& ry,
+  const double& rz
+  );
 
 
 /**
@@ -165,44 +169,30 @@ CvMat* ConvertRodriguesToEuler(
  * \param tx translation in millimetres along x-axis
  * \param ty translation in millimetres along y-axis
  * \param tz translation in millimetres along z-axis
- * \return a new [4x4] matrix that the caller must then de-allocate.
+ * \return a new [4x4] matrix
  */
-CvMat* Construct4x4TransformationMatrix(
-    const double& rx,
-    const double& ry,
-    const double& rz,
-    const double& tx,
-    const double& ty,
-    const double& tz
-    );
+extern "C++" NIFTKOPENCV_EXPORT cv::Matx44d Construct4x4TransformationMatrix(
+  const double& rx,
+  const double& ry,
+  const double& rz,
+  const double& tx,
+  const double& ty,
+  const double& tz
+  );
 
 
 /**
  * \brief From rotations in degrees (+/- 180), converts to radians, then passes on to Construct4x4TransformationMatrix.
- * \return a new [4x4] matrix that the caller must then de-allocate.
+ * \return a new [4x4] matrix
  */
-CvMat* Construct4x4TransformationMatrixFromDegrees(
-    const double& rx,
-    const double& ry,
-    const double& rz,
-    const double& tx,
-    const double& ty,
-    const double& tz
-    );
-
-/**
- * \brief Transforms a [Nx3] matrix of points (or normals) by a [4x4] matrix.
- * \param input3D [Nx3] matrix of points/normals
- * \param matrix4x4 [4x4] transformation matrix
- * \param isNormals if true, will assume that the input3D are normals, and if false, just 3D points.
- * \param output3DPoints [Nx3] matrix of points as output.
- */
-void TransformBy4x4Matrix(
-    const CvMat &input3D,
-    const CvMat &matrix4x4,
-    const bool &isNormals,
-    CvMat& output3DPoints
-    );
+extern "C++" NIFTKOPENCV_EXPORT cv::Matx44d Construct4x4TransformationMatrixFromDegrees(
+  const double& rx,
+  const double& ry,
+  const double& rz,
+  const double& tx,
+  const double& ty,
+  const double& tz
+  );
 
 } // end namespace
 
