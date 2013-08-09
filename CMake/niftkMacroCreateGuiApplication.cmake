8c663c777e0257a6284ee3759ee4ccc43e56df6f
Miklos Espak <m.espak@ucl.ac.uk>
2013-08-09 15:21:40 +0100

    qRestAPI library dir for the installers

--- CMake/niftkMacroCreateGuiApplication.cmake
+++ CMake/niftkMacroCreateGuiApplication.cmake
@@ -59,7 +59,14 @@ macro(NIFTK_CREATE_GUI_APPLICATION)
     ${zlib_LIBRARY_DIR}
     ${aruco_DIR}/lib
   )
-  
+
+  # Temporary workaround for CTK bug of not exposing external project library dirs.
+  # Should be removed as soon as this is fixed in CTK. (espakm)
+  if(EXISTS "${CTK_DIR}/qRestAPI-build/qRestAPIConfig.cmake")
+    include(${CTK_DIR}/qRestAPI-build/qRestAPIConfig.cmake)
+    list(APPEND _library_dirs ${qRestAPI_LIBRARY_DIRS})
+  endif()
+
   #############################################################################
   # Watch out for this:
   # In the top level CMakeLists, MACOSX_BUNDLE_NAMES will contain all the apps
