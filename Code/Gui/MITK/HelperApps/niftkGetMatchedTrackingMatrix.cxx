eb67b39093e86b39ccdac83a125cbe16dfc9fca4
Steve Thompson <s.thompson@ucl.ac.uk>
2013-08-13 15:12:54 +0100

    Feature #2669 added helper app for video frame matching

---
+++ Code/Gui/MITK/HelperApps/niftkGetMatchedTrackingMatrix.cxx
@@ -0,0 +1,56 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include <cstdlib>
+#include <limits>
+#include <mitkVideoTrackerMatching.h>
+#include <niftkGetMatchedTrackingMatrixCLP.h>
+
+int main(int argc, char** argv)
+{
+  PARSE_ARGS;
+  int returnStatus = EXIT_FAILURE;
+
+  if ( trackingInputDirectory.length() == 0 )
+  {
+    std::cout << trackingInputDirectory.length() << std::endl;
+    commandLine.getOutput()->usage(commandLine);
+    return returnStatus;
+  }
+ 
+  bool FlipTracking = ! DontFlipTracking;
+
+  try
+  {
+    mitk::VideoTrackerMatching::Pointer trackerMatcherObject = mitk::VideoTrackerMatching::New();
+    trackerMatcherObject->SetFlipMatrices(FlipTracking);
+    trackerMatcherObject->Initialise(trackingInputDirectory);
+    long long* timingError = new long long;
+    std::cout << trackerMatcherObject->GetTrackerMatrix(frameNumber, timingError, trackerIndex);
+
+    returnStatus = EXIT_SUCCESS;
+  }
+  catch (std::exception& e)
+  {
+    MITK_ERROR << "Caught std::exception:" << e.what();
+    returnStatus = -1;
+  }
+  catch (...)
+  {
+    MITK_ERROR << "Caught unknown exception:";
+    returnStatus = -2;
+  }
+
+  return returnStatus;
+}
