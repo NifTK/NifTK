9a4fb83f3f7e53d4e1eb731c7747beb6a1ec535e
Matt Clarkson <m.clarkson@ucl.ac.uk>
2013-08-15 15:41:02 +0100

    Bug #2687: Added scale factors to pref page

--- Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.igitrackedimage/src/internal/TrackedImageViewPreferencePage.cxx
+++ Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.igitrackedimage/src/internal/TrackedImageViewPreferencePage.cxx
@@ -18,6 +18,7 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QCheckBox>
+#include <QDoubleSpinBox>
 #include <QMessageBox>
 #include <QPushButton>
 #include <ctkPathLineEdit.h>
@@ -27,12 +28,16 @@
 
 const std::string TrackedImageViewPreferencePage::PREFERENCES_NODE_NAME("/uk.ac.ucl.cmic.igitrackedimage");
 const std::string TrackedImageViewPreferencePage::CALIBRATION_FILE_NAME("calibration file name");
+const std::string TrackedImageViewPreferencePage::X_SCALING("scaling in x (vertical) direction");
+const std::string TrackedImageViewPreferencePage::Y_SCALING("scaling in y (vertical) direction");
 
 //-----------------------------------------------------------------------------
 TrackedImageViewPreferencePage::TrackedImageViewPreferencePage()
 : m_MainControl(0)
 , m_CalibrationFileName(0)
 , m_Initializing(false)
+, m_XScaling(0)
+, m_YScaling(0)
 , m_TrackedImageViewPreferencesNode(0)
 {
 }
@@ -77,6 +82,20 @@ void TrackedImageViewPreferencePage::CreateQtControl(QWidget* parent)
   m_CalibrationFileName = new ctkPathLineEdit();
   formLayout->addRow("calibration matrix file name", m_CalibrationFileName);
 
+  m_XScaling = new QDoubleSpinBox();
+  m_XScaling->setMinimum(0.00001);
+  m_XScaling->setMaximum(10000);
+  m_XScaling->setDecimals(4);
+  m_XScaling->setSingleStep(0.001);
+  formLayout->addRow("scaling in x (horizontal) direction (mm/pix)", m_XScaling);
+
+  m_YScaling = new QDoubleSpinBox();
+  m_YScaling->setMinimum(0.00001);
+  m_YScaling->setMaximum(10000);
+  m_YScaling->setDecimals(4);
+  m_YScaling->setSingleStep(0.001);
+  formLayout->addRow("scaling in y (vertical) direction (mm/pix)", m_YScaling);
+
   m_MainControl->setLayout(formLayout);
   this->Update();
 
@@ -95,6 +114,8 @@ QWidget* TrackedImageViewPreferencePage::GetQtControl() const
 bool TrackedImageViewPreferencePage::PerformOk()
 {
   m_TrackedImageViewPreferencesNode->Put(CALIBRATION_FILE_NAME, m_CalibrationFileName->currentPath().toStdString());
+  m_TrackedImageViewPreferencesNode->PutDouble(X_SCALING, m_XScaling->value());
+  m_TrackedImageViewPreferencesNode->PutDouble(Y_SCALING, m_YScaling->value());
   return true;
 }
 
@@ -110,4 +131,6 @@ void TrackedImageViewPreferencePage::PerformCancel()
 void TrackedImageViewPreferencePage::Update()
 {
   m_CalibrationFileName->setCurrentPath(QString(m_TrackedImageViewPreferencesNode->Get(CALIBRATION_FILE_NAME, "").c_str()));
+  m_XScaling->setValue(m_TrackedImageViewPreferencesNode->GetDouble(X_SCALING, 1));
+  m_YScaling->setValue(m_TrackedImageViewPreferencesNode->GetDouble(Y_SCALING, 1));
 }
