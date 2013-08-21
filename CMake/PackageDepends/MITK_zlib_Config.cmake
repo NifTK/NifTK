2d979b6a5c20a3b03def146f65480a154188dd47
Miklos Espak <m.espak@ucl.ac.uk>
2013-08-21 08:52:28 +0100

    Bug #2386: Use system zlib

--- CMake/PackageDepends/MITK_zlib_Config.cmake
+++ CMake/PackageDepends/MITK_zlib_Config.cmake
@@ -12,14 +12,18 @@
 #
 #============================================================================*/
 
-set(zlib_INCLUDE_DIR ${ITK_SOURCE_DIR}/Utilities/itkzlib)
-set(zlib_LIBRARY_DIR ${ITK_BINARY_DIR})
+#set(ITK_SOURCE_DIR ${CMAKE_BINARY_DIR}/../CMakeExternals/Source/ITK)
+#set(zlib_INCLUDE_DIR ${ITK_SOURCE_DIR}/Modules/ThirdParty/ZLIB)
+#set(zlib_LIBRARY_DIR ${ITK_DIR}/lib)
 
-list(APPEND ALL_INCLUDE_DIRECTORIES ${zlib_INCLUDE_DIR})
-list(APPEND ALL_INCLUDE_DIRECTORIES ${zlib_LIBRARY_DIR})
-include_directories(${zlib_INCLUDE_DIR})
+#list(APPEND ALL_INCLUDE_DIRECTORIES ${zlib_INCLUDE_DIR})
+#list(APPEND ALL_INCLUDE_DIRECTORIES ${zlib_LIBRARY_DIR})
+#include_directories(${zlib_INCLUDE_DIR})
+#message("!!! include_directories(${zlib_INCLUDE_DIR})")
 
-link_directories(${zlib_LIBRARY_DIR})
-list(APPEND ALL_LIBRARY_DIRS ${zlib_LIBRARY_DIR})
+#link_directories(${zlib_LIBRARY_DIR})
+#message("!!! link_directories(${zlib_LIBRARY_DIR})")
+#list(APPEND ALL_LIBRARY_DIRS ${zlib_LIBRARY_DIR})
 
-list(APPEND ALL_LIBRARIES itkzlib)
+#list(APPEND ALL_LIBRARIES itkzlib)
+list(APPEND ALL_LIBRARIES z)
