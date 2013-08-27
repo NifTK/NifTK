ac7e28e02038c67e276f22b3ba85754c865abdf5
Matt Clarkson <m.clarkson@ucl.ac.uk>
2013-08-27 22:20:19 +0100

    Bug #2386: Make DataTypes**, DataTypeKey* and DataTypeSizes* not depend on external symbols

--- Code/Gui/MITK/Modules/niftkCore/IO/itkAnalyzeImageIO3160.cxx
+++ Code/Gui/MITK/Modules/niftkCore/IO/itkAnalyzeImageIO3160.cxx
@@ -46,6 +46,34 @@ const char *const AnalyzeImageIO3160::ANALYZE_GLMIN = "ANALYZE_GLMIN";
 const char *const AnalyzeImageIO3160::ANALYZE_AUX_FILE_NAME = "ANALYZE_AUX_FILE_NAME";
 const char *const AnalyzeImageIO3160::ANALYZE_CALIBRATIONUNITS = "ANALYZE_CALIBRATIONUNITS";
 
+//An array of the Analyze v7.5 known DataTypes
+const char DataTypes[12][10] =
+  {
+  "UNKNOWN", "BINARY", "CHAR", "SHORT", "INT", "FLOAT",
+  "COMPLEX", "DOUBLE", "RGB", "ALL", "USHORT", "UINT"
+  };
+
+//An array with the corresponding number of bits for each image type.
+//NOTE: the following two line should be equivalent.
+const short int DataTypeSizes[12] = { 0, 1, 8, 16, 32, 32, 64, 64, 24, 0, 16, 32 };
+
+//An array with Data type key sizes
+const short int DataTypeKey[12] =
+  {
+  ANALYZE_DT_UNKNOWN,
+  ANALYZE_DT_BINARY,
+  ANALYZE_DT_UNSIGNED_CHAR,
+  ANALYZE_DT_SIGNED_SHORT,
+  ANALYZE_DT_SIGNED_INT,
+  ANALYZE_DT_FLOAT,
+  ANALYZE_DT_COMPLEX,
+  ANALYZE_DT_DOUBLE,
+  ANALYZE_DT_RGB,
+  ANALYZE_DT_ALL,
+  SPMANALYZE_DT_UNSIGNED_SHORT,
+  SPMANALYZE_DT_UNSIGNED_INT
+  };
+
 static std::string
 GetExtension( const std::string& filename )
 {
