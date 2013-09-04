78f659e258a58e0d41691370dad6ee8d7e008779
Steve Thompson <s.thompson@ucl.ac.uk>
2013-09-04 17:19:46 +0100

    Feature #2669 added handeye from diretory helper app to replace deleted unit test

---
+++ Code/Gui/MITK/HelperApps/niftkHandeyeCalibrationFromDirectory.cxx
@@ -0,0 +1,71 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include <cstdlib>
+#include <limits>
+#include <mitkHandeyeCalibrationFromDirectory.h>
+#include <niftkHandeyeCalibrationFromDirectoryCLP.h>
+
+int main(int argc, char** argv)
+{
+  PARSE_ARGS;
+  int returnStatus = EXIT_FAILURE;
+  std::vector<double> ReprojectionError;
+
+  if ( trackingInputDirectory.length() == 0 ||
+    ! ( ( extrinsicInputDirectory.length() == 0 ) != ( extrinsicInputFile.length() == 0 )) )
+  {
+    std::cout << trackingInputDirectory.length() << " " << extrinsicInputDirectory.length() << " " << extrinsicInputFile.length() << std::endl;
+    commandLine.getOutput()->usage(commandLine);
+    return returnStatus;
+  }
+ 
+  bool FlipTracking = ! DontFlipTracking;
+  bool FlipExtrin = FlipExtrinsics;
+  bool SortByDistance = ! ( DontSortByDistance || SortByAngle );
+
+  try
+  {
+    mitk::HandeyeCalibrate::Pointer calibrationObject = mitk::HandeyeCalibrate::New();
+    calibrationObject->SetFlipTracking(FlipTracking);
+    calibrationObject->SetFlipExtrinsic(FlipExtrin);
+    calibrationObject->SetSortByDistance(SortByDistance);
+    calibrationObject->SetSortByAngle(SortByAngle);
+    if ( extrinsicInputDirectory.length() == 0 )
+    {
+      ReprojectionError = calibrationObject->Calibrate(trackingInputDirectory,
+        extrinsicInputFile);
+    }
+    else
+    {
+      ReprojectionError = calibrationObject->Calibrate(trackingInputDirectory,
+        extrinsicInputDirectory);
+    }
+
+    returnStatus = EXIT_SUCCESS;
+  }
+  catch (std::exception& e)
+  {
+    MITK_ERROR << "Caught std::exception:" << e.what();
+    returnStatus = -1;
+  }
+  catch (...)
+  {
+    MITK_ERROR << "Caught unknown exception:";
+    returnStatus = -2;
+  }
+
+  std::cout << "Reprojection error=" << ReprojectionError[0] << ", return status = " << returnStatus << std::endl;
+  return returnStatus;
+}
