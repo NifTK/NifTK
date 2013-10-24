6158dd0b2813619a1d28048b569369b133e78593
Steve Thompson <s.thompson@ucl.ac.uk>
2013-10-24 11:22:03 +0100

    Bug #2849 changed distortion vectors from 5,1 to 1,4

--- Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkReprojectionTest.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkReprojectionTest.cxx
@@ -34,9 +34,9 @@ int mitkReprojectionTest ( int argc, char * argv[] )
   MITK_TEST_BEGIN("mitkReprojectionTest");
   cv::Mat leftCameraPositionToFocalPointUnitVector = cv::Mat(1,3,CV_64FC1);
   cv::Mat leftCameraIntrinsic = cv::Mat(3,3,CV_64FC1);
-  cv::Mat leftCameraDistortion = cv::Mat(1,5,CV_64FC1);
+  cv::Mat leftCameraDistortion = cv::Mat(1,4,CV_64FC1);
   cv::Mat rightCameraIntrinsic = cv::Mat(3,3,CV_64FC1);
-  cv::Mat rightCameraDistortion = cv::Mat(1,5,CV_64FC1);
+  cv::Mat rightCameraDistortion = cv::Mat(1,4,CV_64FC1);
   cv::Mat rightToLeftRotationMatrix = cv::Mat(3,3,CV_64FC1);
   cv::Mat rightToLeftTranslationVector = cv::Mat(3,1,CV_64FC1);
   cv::Mat leftCameraToTracker = cv::Mat(4,4,CV_64FC1);
