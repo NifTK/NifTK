9315588c5050fe16a30d23f0396df9f6149130c4
Matt Clarkson <m.clarkson@ucl.ac.uk>
2013-11-08 20:35:52 +0000

    Bug #2915: Remove common legacy plugin as no-one should use it

--- Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.gui.qt.commonlegacy/manifest_headers.cmake
+++
@@ -1,5 +0,0 @@
-set(Plugin-Name "CMIC Qt Legacy Common Base Classes")
-set(Plugin-Version "0.1")
-set(Plugin-Vendor "CMIC, Centre For Medical Image Computing, UCL.")
-set(Plugin-ContactAddress "http://cmic.cs.ucl.ac.uk/")
-set(Require-Plugin org.mitk.gui.qt.common org.mitk.gui.qt.stdmultiwidgeteditor)
