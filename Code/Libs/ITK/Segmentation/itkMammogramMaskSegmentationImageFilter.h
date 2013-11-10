6602887a3ff7a882e35a33ec3e60458642078525
John Hipwell <j.hipwell@ucl.ac.uk>
2013-11-10 17:19:59 +0000

    Ticket #2870: niftkMammogramMaskSegmentation and new class itkMammogramMaskSegmentationImageFilter created and  modified to compute BIFs

---
+++ Code/Libs/ITK/Segmentation/itkMammogramMaskSegmentationImageFilter.h
@@ -0,0 +1,160 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef itkMammogramMaskSegmentationImageFilter_h
+#define itkMammogramMaskSegmentationImageFilter_h
+
+#include <itkImageToImageFilter.h>
+
+#include <vnl/vnl_double_2.h>
+
+namespace itk {
+  
+/** \class MammogramMaskSegmentationImageFilter
+ * \brief 2D image filter class to segment the breast area from a mammogram.
+ *
+ */
+
+template<class TInputImage, class TOutputImage>
+class ITK_EXPORT MammogramMaskSegmentationImageFilter:
+    public ImageToImageFilter< TInputImage, TOutputImage >
+{
+public:
+  /** Standard class typedefs. */
+  typedef MammogramMaskSegmentationImageFilter           Self;
+  typedef ImageToImageFilter< TInputImage,TOutputImage > Superclass;
+  typedef SmartPointer< Self >                           Pointer;
+  typedef SmartPointer< const Self >                     ConstPointer;
+  
+  /** Run-time type information (and related methods).   */
+  itkTypeMacro( MammogramMaskSegmentationImageFilter, ImageToImageFilter );
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Image dimension. */
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+
+  /** Type of the input image */
+  typedef TInputImage                           InputImageType;
+  typedef typename InputImageType::Pointer      InputImagePointer;
+  typedef typename InputImageType::ConstPointer InputImageConstPointer;
+  typedef typename InputImageType::RegionType   InputImageRegionType;
+  typedef typename InputImageType::PixelType    InputImagePixelType;
+  typedef typename InputImageType::SpacingType  InputImageSpacingType;
+  typedef typename InputImageType::PointType    InputImagePointType;
+
+  typedef typename NumericTraits<InputImagePixelType>::RealType    RealType;
+
+  /** Type of the output image */
+  typedef TOutputImage                          OutputImageType;
+  typedef typename OutputImageType::Pointer     OutputImagePointer;
+  typedef typename OutputImageType::RegionType  OutputImageRegionType;
+  typedef typename OutputImageType::PixelType   OutputImagePixelType;
+  typedef typename OutputImageType::IndexType   OutputImageIndexType;
+  typedef typename OutputImageType::PointType   OutputImagePointType;
+
+  typedef OutputImagePointType OriginType;
+
+
+  /** Define the image type for internal computations 
+      RealType is usually 'double' in NumericTraits. 
+      Here we prefer float in order to save memory.  */
+
+  typedef float InternalRealType;
+
+  typedef Image< InternalRealType, TInputImage::ImageDimension > RealImageType;
+
+  typedef typename RealImageType::Pointer RealImagePointer;
+
+#ifdef ITK_USE_CONCEPT_CHECKING
+  /** Begin concept checking */
+  itkConceptMacro(DimensionShouldBe2,
+		  (Concept::SameDimension<itkGetStaticConstMacro(InputImageDimension),2>));
+  itkConceptMacro(InputHasNumericTraitsCheck,
+                  (Concept::HasNumericTraits<InputImagePixelType>));
+  itkConceptMacro(OutputHasPixelTraitsCheck,
+                  (Concept::HasPixelTraits<OutputImagePixelType>));
+  /** End concept checking */
+#endif
+
+  /// For debugging purposes, set single threaded execution
+  void SetSingleThreadedExecution(void) {m_FlagMultiThreadedExecution = false;}
+
+protected:
+  MammogramMaskSegmentationImageFilter();
+  virtual ~MammogramMaskSegmentationImageFilter();
+  void PrintSelf(std::ostream& os, Indent indent) const;
+  
+  /** If an imaging filter needs to perform processing after the buffer
+   * has been allocated but before threads are spawned, the filter can
+   * can provide an implementation for BeforeThreadedGenerateData(). The
+   * execution flow in the default GenerateData() method will be:
+   *      1) Allocate the output buffer
+   *      2) Call BeforeThreadedGenerateData()
+   *      3) Spawn threads, calling ThreadedGenerateData() in each thread.
+   *      4) Call AfterThreadedGenerateData()
+   * Note that this flow of control is only available if a filter provides
+   * a ThreadedGenerateData() method and NOT a GenerateData() method. */
+  virtual void BeforeThreadedGenerateData(void);
+  
+  /** If an imaging filter needs to perform processing after all
+   * processing threads have completed, the filter can can provide an
+   * implementation for AfterThreadedGenerateData(). The execution
+   * flow in the default GenerateData() method will be:
+   *      1) Allocate the output buffer
+   *      2) Call BeforeThreadedGenerateData()
+   *      3) Spawn threads, calling ThreadedGenerateData() in each thread.
+   *      4) Call AfterThreadedGenerateData()
+   * Note that this flow of control is only available if a filter provides
+   * a ThreadedGenerateData() method and NOT a GenerateData() method. */
+  virtual void AfterThreadedGenerateData(void);
+  
+  /** Single threaded execution, for debugging purposes ( call
+  SetSingleThreadedExecution() ) */
+  void GenerateData();
+  
+  /** MammogramMaskSegmentationImageFilter can be implemented as a multithreaded filter.
+   * Therefore, this implementation provides a ThreadedGenerateData()
+   * routine which is called for each processing thread. The output
+   * image data is allocated automatically by the superclass prior to
+   * calling ThreadedGenerateData().  ThreadedGenerateData can only
+   * write to the portion of the output image specified by the
+   * parameter "outputRegionForThread"
+   *
+   * \sa ImageToImageFilter::ThreadedGenerateData(),
+   *     ImageToImageFilter::GenerateData() */
+  void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
+                            ThreadIdType threadId );
+
+  // Override since the filter produces the entire dataset
+  void EnlargeOutputRequestedRegion(DataObject *output);
+
+  /// Flag to turn multithreading on or off
+  bool m_FlagMultiThreadedExecution;
+
+
+private:
+  MammogramMaskSegmentationImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkMammogramMaskSegmentationImageFilter.txx"
+#endif
+
+#endif
---
+++ Code/Libs/ITK/Segmentation/itkMammogramMaskSegmentationImageFilter.txx
@@ -0,0 +1,205 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef __itkMammogramMaskSegmentationImageFilter_txx
+#define __itkMammogramMaskSegmentationImageFilter_txx
+
+#include "itkMammogramMaskSegmentationImageFilter.h"
+
+#include <itkBasicImageFeaturesImageFilter.h>
+#include <itkImageRegionIteratorWithIndex.h>
+#include <itkImageRegionConstIteratorWithIndex.h>
+#include <itkImageFileWriter.h>
+#include <itkImageDuplicator.h>
+#include <itkProgressReporter.h>
+
+#include <niftkConversionUtils.h>
+
+#include <vnl/vnl_double_2x2.h>
+
+#include <itkUCLMacro.h>
+
+
+namespace itk
+{
+/* -----------------------------------------------------------------------
+   Constructor
+   ----------------------------------------------------------------------- */
+
+template<class TInputImage, class TOutputImage>
+MammogramMaskSegmentationImageFilter<TInputImage,TOutputImage>
+::MammogramMaskSegmentationImageFilter()
+{
+
+  // Multi-threaded execution is enabled by default
+  m_FlagMultiThreadedExecution = true;
+
+  this->SetNumberOfRequiredInputs( 1 );
+  this->SetNumberOfRequiredOutputs( 1 );
+}
+
+
+/* -----------------------------------------------------------------------
+   Destructor
+   ----------------------------------------------------------------------- */
+
+template<class TInputImage, class TOutputImage>
+MammogramMaskSegmentationImageFilter<TInputImage,TOutputImage>
+::~MammogramMaskSegmentationImageFilter()
+{
+}
+
+
+/* -----------------------------------------------------------------------
+   EnlargeOutputRequestedRegion()
+   ----------------------------------------------------------------------- */
+
+template <typename TInputImage, typename TOutputImage>
+void 
+MammogramMaskSegmentationImageFilter<TInputImage,TOutputImage>
+::EnlargeOutputRequestedRegion(DataObject *output)
+{
+  TOutputImage *out = dynamic_cast<TOutputImage*>(output);
+
+  if (out) 
+    out->SetRequestedRegion( out->GetLargestPossibleRegion() );
+}
+
+
+/* -----------------------------------------------------------------------
+   BeforeThreadedGenerateData()
+   ----------------------------------------------------------------------- */
+
+template <typename TInputImage, typename TOutputImage>
+void 
+MammogramMaskSegmentationImageFilter<TInputImage,TOutputImage>
+::BeforeThreadedGenerateData(void)
+{
+  typedef itk::ImageFileWriter< RealImageType > WriterType;
+  typename WriterType::Pointer writer = WriterType::New();
+
+  typedef itk::BasicImageFeaturesImageFilter< TInputImage, 
+                                              TOutputImage > BasicImageFeaturesFilterType;
+
+ 
+  // Create the basic image features filter
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  BasicImageFeaturesFilterType::Pointer BIFsFilter = BasicImageFeaturesFilterType::New();
+
+  BIFsFilter->SetEpsilon( 1e-3 );
+
+  BIFsFilter->SetInput( this->GetInput() );     
+  BIFsFilter->SetSigma( 1. );
+
+  std::cout << "Computing Basic Image Features" << std::endl;
+  BIFsFilter->Update();
+        
+  this->GraftOutput( BIFsFilter->GetOutput() );
+  
+}
+
+
+/* -----------------------------------------------------------------------
+   GenerateData()
+   ----------------------------------------------------------------------- */
+
+template <typename TInputImage, typename TOutputImage>
+void 
+MammogramMaskSegmentationImageFilter<TInputImage,TOutputImage>
+::GenerateData(void)
+{
+  // Perform multi-threaded execution by default
+
+  if (m_FlagMultiThreadedExecution) {
+    
+    niftkitkDebugMacro( "Multi-threaded mammogram mask segmentation");
+
+    Superclass::GenerateData();
+  }
+
+  // Single-threaded execution
+
+  else {
+  
+    niftkitkDebugMacro( "Single-threaded mammogram mask segmentation");
+
+    this->AllocateOutputs();
+    this->BeforeThreadedGenerateData();
+  
+    // Set up the multithreaded processing
+    typename ImageSource<TOutputImage>::ThreadStruct str;
+    str.Filter = this;
+    
+    this->GetMultiThreader()->SetNumberOfThreads( 1 );
+    this->GetMultiThreader()->SetSingleMethod(this->ThreaderCallback, &str);
+    
+    // multithread the execution
+    this->GetMultiThreader()->SingleMethodExecute();
+    
+    // Call a method that can be overridden by a subclass to perform
+    // some calculations after all the threads have completed
+    this->AfterThreadedGenerateData();
+  }
+}
+
+
+/* -----------------------------------------------------------------------
+   ThreadedGenerateData(const OutputImageRegionType&, int)
+   ----------------------------------------------------------------------- */
+
+template <typename TInputImage, typename TOutputImage>
+void 
+MammogramMaskSegmentationImageFilter<TInputImage,TOutputImage>
+::ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
+                       ThreadIdType threadId)
+{
+
+}
+
+
+/* -----------------------------------------------------------------------
+   AfterThreadedGenerateData()
+   ----------------------------------------------------------------------- */
+
+template<class TInputImage, class TOutputImage>
+void
+MammogramMaskSegmentationImageFilter<TInputImage,TOutputImage>
+::AfterThreadedGenerateData(void)
+{
+
+}
+
+
+/* -----------------------------------------------------------------------
+   PrintSelf()
+   ----------------------------------------------------------------------- */
+
+template<class TInputImage, class TOutputImage>
+void
+MammogramMaskSegmentationImageFilter<TInputImage,TOutputImage>
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+
+  if (m_FlagMultiThreadedExecution)
+    os << indent << "MultiThreadedExecution: ON" << std::endl;
+  else
+    os << indent << "MultiThreadedExecution: OFF" << std::endl;
+
+}
+
+} // end namespace itk
+
+#endif
