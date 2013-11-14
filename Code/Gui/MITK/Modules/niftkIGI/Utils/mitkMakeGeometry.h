f4bf8826d2f51b026c5bc6e96316a35678ee5546
Steve Thompson <s.thompson@ucl.ac.uk>
2013-11-14 12:17:25 +0000

    Bug #2773 corrected opencv export to IGI export

--- Code/Gui/MITK/Modules/niftkIGI/Utils/mitkMakeGeometry.h
+++ Code/Gui/MITK/Modules/niftkIGI/Utils/mitkMakeGeometry.h
@@ -15,7 +15,7 @@
 #ifndef mitkMakeGeometry_h
 #define mitkMakeGeometry_h
 
-#include "niftkOpenCVExports.h"
+#include "niftkIGIExports.h"
 #include <mitkSurface.h>
 #include <QString>
 #include <vtkMatrix4x4.h>
@@ -24,21 +24,21 @@
  * \param the rigid body filename to define the location of the tracking markers
  * \param the handeye calibration to define the tool origin
  */
-NIFTKOPENCV_EXPORT mitk::Surface::Pointer MakeLaparoscope(std::string rigidBodyFilename, std::string handeyeFilename );
+NIFTKIGI_EXPORT mitk::Surface::Pointer MakeLaparoscope(std::string rigidBodyFilename, std::string handeyeFilename );
 
 /**
  * \brief For visualisation purposes, creates a representation of the pointer.
  * \param the rigid body filename to define the location of the tracking markers
  * \param the handeye calibration to define the tool origin
  */
-NIFTKOPENCV_EXPORT mitk::Surface::Pointer MakePointer(std::string rigidBodyFilename, std::string handeyeFilename );
+NIFTKIGI_EXPORT mitk::Surface::Pointer MakePointer(std::string rigidBodyFilename, std::string handeyeFilename );
 
 /**
  * \brief For visualisation purposes, creates a representation of the reference.
  * \param the rigid body filename to define the location of the tracking markers
  * \param the handeye calibration to define the tool origin
  */
-NIFTKOPENCV_EXPORT mitk::Surface::Pointer MakeReference(std::string rigidBodyFilename, std::string handeyeFilename );
+NIFTKIGI_EXPORT mitk::Surface::Pointer MakeReference(std::string rigidBodyFilename, std::string handeyeFilename );
 
 /**
  * \brief For visualisation purposes, make a wall of a cube
@@ -46,7 +46,7 @@ NIFTKOPENCV_EXPORT mitk::Surface::Pointer MakeReference(std::string rigidBodyFil
  * \param which wall to make 
  * \param the xoffset, room will be centred at x= size * xOffset 
  * */
-NIFTKOPENCV_EXPORT mitk::Surface::Pointer MakeAWall( const int& whichwall, const float& size = 4000,
+NIFTKIGI_EXPORT mitk::Surface::Pointer MakeAWall( const int& whichwall, const float& size = 4000,
     const float& xOffset = 0.0 , const float& yOffset = 0.0, const float& zOffset = -0.3, 
     const float& thickness = 10.0);
 
