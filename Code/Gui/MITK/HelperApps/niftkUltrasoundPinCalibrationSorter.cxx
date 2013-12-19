2f001baaf49850b3e1b8b4ac13e5a137aeb5a2dd
Matt Clarkson <m.clarkson@ucl.ac.uk>
2013-12-19 08:39:00 +0000

    Bug #3067: Added empty helper app

---
+++ Code/Gui/MITK/HelperApps/niftkUltrasoundPinCalibrationSorter.cxx
@@ -0,0 +1,50 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include <cstdlib>
+#include <limits>
+#include <niftkUltrasoundPinCalibrationSorterCLP.h>
+#include <mitkVector.h>
+
+int main(int argc, char** argv)
+{
+  PARSE_ARGS;
+  int returnStatus = EXIT_FAILURE;
+
+  if ( inputMatrixDirectory.length() == 0 
+  || inputImageDirectory.length() == 0
+  || outputMatrixDirectory.length() == 0
+  || outputPointDirectory.length() == 0
+  )
+  {
+    commandLine.getOutput()->usage(commandLine);
+    return returnStatus;
+  }
+
+  try
+  {
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Caught std::exception:" << e.what();
+    returnStatus = -1;
+  }
+  catch (...)
+  {
+    std::cerr << "Caught unknown exception:";
+    returnStatus = -2;
+  }
+
+  return returnStatus;
+}
