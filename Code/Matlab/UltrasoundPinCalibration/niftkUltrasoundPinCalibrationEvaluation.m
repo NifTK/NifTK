9900de1806ff311e9f90bbdc55574f3c059c9b30
Matt Clarkson <m.clarkson@ucl.ac.uk>
2014-01-23 10:11:19 +0000

    Bug #3121: Measure precision w.r.t. median point

--- Code/Matlab/UltrasoundPinCalibration/niftkUltrasoundPinCalibrationEvaluation.m
+++ Code/Matlab/UltrasoundPinCalibration/niftkUltrasoundPinCalibrationEvaluation.m
@@ -40,18 +40,18 @@ disp(goldStandardPoint);
 %   4. Use remaining points to assess how close we are to centre of reconstructed points = precision.
 %   5. Repeat `numberOfRepetitions` times to get statistics for mean and standard deviation.
 % ----------------------------------------------------------------------------------------------------------------------
-results = zeros(numberOfRepetitions, 2);
+results = zeros(numberOfRepetitions, 3);
 counterForSuccessfulCalibrations = 0;
 
-% --------------------------------------------------------------------
-% Run one calibration with all data, to get complete list of outliers.
-% --------------------------------------------------------------------
+% ----------------------------------------------------------------------------------------------
+% Run one calibration with all data, to get complete list of outliers, relative to median point.
+% ----------------------------------------------------------------------------------------------
 [finalParams, sumsqs, residuals, outliers] = niftkUltrasoundPinCalibration(initialGuess, allTrackingMatrices, allUltrasoundPoints);
 
 % -------------------------------------------------------------------------
 % Now we do evaluation, taking a subset of points, and measuring:
 %   accuracy  = RMS error from gold standard
-%   precision = RMS error from mean reconstructed point
+%   precision = RMS error from median reconstructed point
 % -------------------------------------------------------------------------
 while(true)
 
@@ -63,34 +63,37 @@ while(true)
     rMi = Comp_RigidBody_Matrix(finalParams(1:6));
     S = diag([finalParams(10) finalParams(10) 1 1]);
 
-    % -------------------------------------------------------------------------
-    % Work out mean reconstructed point using all data, that is not an outlier.
-    % -------------------------------------------------------------------------
-    counterForMean = 0;
-    meanReconstructedPoint = zeros(4,1);
+    % -----------------------------------------------------------------------------------
+    % Work out median reconstructed point using all data, that is not an obvious outlier.
+    % -----------------------------------------------------------------------------------
+    counterForMedian = 0;
+    reconstructedPoints = [];
+    medianReconstructedPoint = zeros(4,1);
     for j = 1:numberOfTrackingMatrices
       if ismember(allIndexes(j), selectedIndexes)
         if ~ismember(allIndexes(j), outliersFromSelectedData)
-          reconstructedPoint = allTrackingMatrices{allIndexes(j)}*rMi*S*allUltrasoundPoints{allIndexes(j)};
-          meanReconstructedPoint = plus(meanReconstructedPoint, reconstructedPoint);
-          counterForMean = counterForMean + 1;
+          if ~ismember(allIndexes(j), outliers)
+            reconstructedPoint = allTrackingMatrices{allIndexes(j)}*rMi*S*allUltrasoundPoints{allIndexes(j)};
+            reconstructedPoints = [reconstructedPoints reconstructedPoint];
+            counterForMedian = counterForMedian + 1;
+          end
         end
       end
     end
-    if(counterForMean == 0)
+
+    if(counterForMedian == 0)
       continue
     end
+    medianReconstructedPoint = median(reconstructedPoints, 2);
 
-    meanReconstructedPoint = times(meanReconstructedPoint,1.0/counterForMean);
-
-    % ---------------------------------------------------------------
-    % Work out stats relative to mean point, and gold standard point.
-    % ---------------------------------------------------------------
+    % -----------------------------------------------------------------
+    % Work out stats relative to median point, and gold standard point.
+    % -----------------------------------------------------------------
     counterForStats = 0;
     squaredDistanceFromGold = 0;
-    squaredDistanceFromMean = 0;
+    squaredDistanceFromMedian = 0;
     rmsErrorFromGold = 0;
-    rmsErrorFromMean = 0;
+    rmsErrorFromMedian = 0;
 
     % Calculate RMS error.
     for j = 1:numberOfTrackingMatrices
@@ -99,9 +102,9 @@ while(true)
           if ~ismember(allIndexes(j), outliers)
             reconstructedPoint = allTrackingMatrices{allIndexes(j)}*rMi*S*allUltrasoundPoints{allIndexes(j)};
             squaredDistanceFromGold = ((reconstructedPoint(1,1) -  gsp(1,1))*(reconstructedPoint(1,1) -  gsp(1,1)) + (reconstructedPoint(2,1) -  gsp(2,1))*(reconstructedPoint(2,1) -  gsp(2,1))+ (reconstructedPoint(3,1) -  gsp(3,1))*(reconstructedPoint(3,1) -  gsp(3,1)));
-            squaredDistanceFromMean = ((reconstructedPoint(1,1) -  meanReconstructedPoint(1,1))*(reconstructedPoint(1,1) -  meanReconstructedPoint(1,1)) + (reconstructedPoint(2,1) -  meanReconstructedPoint(2,1))*(reconstructedPoint(2,1) -  meanReconstructedPoint(2,1))+ (reconstructedPoint(3,1) -  meanReconstructedPoint(3,1))*(reconstructedPoint(3,1) -  meanReconstructedPoint(3,1)));
+            squaredDistanceFromMedian = ((reconstructedPoint(1,1) -  medianReconstructedPoint(1,1))*(reconstructedPoint(1,1) -  medianReconstructedPoint(1,1)) + (reconstructedPoint(2,1) -  medianReconstructedPoint(2,1))*(reconstructedPoint(2,1) -  medianReconstructedPoint(2,1))+ (reconstructedPoint(3,1) -  medianReconstructedPoint(3,1))*(reconstructedPoint(3,1) -  medianReconstructedPoint(3,1)));
             rmsErrorFromGold = rmsErrorFromGold + squaredDistanceFromGold;
-            rmsErrorFromMean = rmsErrorFromMean + squaredDistanceFromMean;
+            rmsErrorFromMedian = rmsErrorFromMedian + squaredDistanceFromMedian;
             counterForStats = counterForStats + 1;
           end
         end
@@ -112,12 +115,13 @@ while(true)
     end
 
     rmsErrorFromGold = rmsErrorFromGold / counterForStats;
-    rmsErrorFromMean = rmsErrorFromMean / counterForStats;
+    rmsErrorFromMedian = rmsErrorFromMedian / counterForStats;
     rmsErrorFromGold = sqrt(rmsErrorFromGold);
-    rmsErrorFromMean = sqrt(rmsErrorFromMean);
+    rmsErrorFromMedian = sqrt(rmsErrorFromMedian);
     counterForSuccessfulCalibrations = counterForSuccessfulCalibrations + 1;
     results(counterForSuccessfulCalibrations, 1) = rmsErrorFromGold;
-    results(counterForSuccessfulCalibrations, 2) = rmsErrorFromMean;
+    results(counterForSuccessfulCalibrations, 2) = rmsErrorFromMedian;
+    results(counterForSuccessfulCalibrations, 3) = counterForStats;
 
   end
 
@@ -135,12 +139,10 @@ disp('Number of samples in data');
 disp(numberOfTrackingMatrices);
 disp('Number of samples in calibration');
 disp(numberOfPoints);
-disp('Number of samples in validation');
-disp(numberOfTrackingMatrices - numberOfPoints);
 disp('Gold standard point');
 disp(goldStandardPoint);
-disp('Mean reconstructed point');
-disp(transpose(meanReconstructedPoint));
+disp('Median reconstructed point');
+disp(transpose(medianReconstructedPoint));
 disp('Mean accuracy');
 disp(means(1,1));
 disp('Std Dev accuracy');
@@ -149,5 +151,9 @@ disp('Mean precision');
 disp(means(1,2));
 disp('Std Dev precision');
 disp(stdDevs(1,2));
+disp('Mean points in validation');
+disp(means(1,3));
+disp('Std Dev points in validation');
+disp(stdDevs(1,3));
 
    
