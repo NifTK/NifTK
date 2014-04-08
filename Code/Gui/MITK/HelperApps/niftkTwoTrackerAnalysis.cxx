0ff6e809c421c598b58fd6a72293660e32d1a0f3
Steve Thompson <s.thompson@ucl.ac.uk>
2014-04-08 16:17:06 +0100

    Feature #3326 added flag to set number of matrices to use in handeye

--- Code/Gui/MITK/HelperApps/niftkTwoTrackerAnalysis.cxx
+++ Code/Gui/MITK/HelperApps/niftkTwoTrackerAnalysis.cxx
@@ -39,7 +39,7 @@ int main(int argc, char** argv)
     }
     if ( HEfileout.length() != 0 ) 
     {
-      trackerMatcherObject->HandeyeCalibration( false, HEfileout);
+      trackerMatcherObject->HandeyeCalibration( false, HEfileout, MatricesToUse);
     }
  
     returnStatus = EXIT_SUCCESS;
