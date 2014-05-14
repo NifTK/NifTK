90fce087b686ef6f18cb959ef8c3ecf1fccfe483
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-05-14 13:54:40 +0100

    Feature #3421: diffusion-workflow: Document a bit the interfaces and the nodes connections

--- Code/nipype-workflows/diffusion/diffusion_mri_processing.py
+++ Code/nipype-workflows/diffusion/diffusion_mri_processing.py
@@ -54,8 +54,11 @@ def create_diffusion_mri_processing_workflow(name='diffusion_mri_processing'):
     #do the node for resampling
     resampling = pe.Node(niftyreg.RegResample(), name = 'resampling', iterfield=['trans_file', 'flo_file'])
 
-    #do the node for merging all the files into one
-    merge_dwis = pe.Node(interface = niu.Select(), name = 'merge_dwis')
+    #do the node for merging all the DWIs and B0s files into one list
+    merge_dwis_list = pe.Node(interface = niu.Merge(), name = 'merge_dwis_list')
+    
+    #do the node for merging all the files into one 4D
+    merge_dwis = pe.Node(interface = fsl.Merge(), name = 'merge_dwis')
     
     workflow.connect([input_node, split_dwis,                  ['in_dwi_4d_file', 'in_file']))
     workflow.connect([split_dwis, select_B0s,                  ['out_files',      'inlist']))
@@ -72,11 +75,61 @@ def create_diffusion_mri_processing_workflow(name='diffusion_mri_processing'):
     workflow.connect([susceptibility_correction,   transformation_composition, ['out_deformation_field','in_comp_transformation_file2']))
     
     workflow.connect([groupwise_B0_coregistration, resampling, ['mean_image',              'ref_file']))
-    workflow.connect([split_dwis, resampling,                  ['out_files',               'flo_file']))
-    workflow.connect([transformation_composition, resampling,  ['out_comp_transformation', 'trans_file']))
-
-    workflow.connect([resampling, merge_dwis,                  ['res_file', 'in_file']))
-
+    workflow.connect([split_dwis,                  resampling, ['out_files',               'flo_file']))
+    workflow.connect([transformation_composition,  resampling, ['out_comp_transformation', 'trans_file']))
+    
+    workflow.connect([resampling, merge_dwis_list,             ['res_file', 'in']))
+    
+    workflow.connect([merge_dwis_list, merge_dwis,             ['out', 'in_files']))
+    
     workflow.connect([merge_dwis, tensor_fitting,              ['out_file',     'in_dwi_file']))
     workflow.connect([inputnode,  tensor_fitting,              ['in_bvec_file', 'in_bvec_file']))
     workflow.connect([inputnode,  tensor_fitting,              ['in_bval_file', 'in_bvec_file']))
+
+
+    # node explanation:
+    
+    # input_node
+    # contains the entry point for the following inputs: 
+    # - in_dwi_4d_file
+    # - in_bvec_file
+    # - in_bval_file
+    # - in_fm_magnitude_file
+    # - in_fm_phase_file
+    # - in_T1_file
+
+    # split_dwis
+    # Node using fsl.Split() to split the 4D image (in_dwi_4d_file) into 3D volume files
+
+    # select_B0s
+    # Node using niu.Select() to select only B0s from the list
+    
+    # select_DWIs 
+    # Node using niu.Select() to select only DWIs from the list
+    
+    # groupwise_B0_coregistration
+    # the groupwise registration workflow (IVOR), iterated over the 'flo_file'
+    
+    # susceptibility_correction
+    # the susceptibility correction workflow, TODO
+    
+    # dwi_to_B0_registration
+    # the groupwise registration node, it's iterated on 'flo_file'
+
+    # gradient_reorientation
+    # gradient reorientation node, TODO
+
+    # transformation_composition
+    # node using RegTransform() to compose between a linear transformation 
+    # and a deformation field, it's iterated over 'file1' and 'file2 and 'file3'
+
+    # resampling
+    # node to resample every DWI and B0 with composed transformation, 
+    # iterated on 'ref_file' 'flo_file' and 'res_file'
+    
+    # merge_dwis_list
+    #do the node for merging all the DWIs and B0s files into one list
+    
+    # merge_dwis
+    #do the node for merging all the files into one 4D
+    
