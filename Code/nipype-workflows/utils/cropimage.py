7122cfbc67b86da9e761b488fafe972f6fcb1f18
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-06-02 18:53:41 +0100

    Feature #3421: Added the interface for the CropImage.sh script which is used to crop an image with a binary mask bounding box.

---
+++ Code/nipype-workflows/utils/cropimage.py
@@ -0,0 +1,69 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+    Simple interface for the CropImage.sh script
+"""
+
+import os
+import numpy as np
+from nibabel import load
+import os.path as op
+import warnings
+
+from nipype.interfaces.niftyseg.base import NIFTYSEGCommand, NIFTYSEGCommandInputSpec
+from nipype.interfaces.base import (TraitedSpec, File, Directory, traits, InputMultiPath,
+                                    isdefined)
+
+
+warn = warnings.warn
+warnings.filterwarnings('always', category=UserWarning)
+
+class CropImageInputSpec(NIFTYSEGCommandInputSpec):
+    
+    in_file = File(argstr="%s", exists=True, mandatory=True, position = 2,
+                desc="Input target image filename")
+    
+    mask_file = File(exists=True, mandatory=True, argstr="%s", position = -3,
+                     desc="Mask over the input image")
+
+    out_file = File(exists=False, genfile = True, mandatory=False, argstr="%s", position = -2, 
+                     desc="Mask over the input image [default: none]")
+
+
+class CropImageOutputSpec(TraitedSpec):
+    out_file   = File(exists=False, genfile = True, desc="Output cropped image file")
+
+class CropImage(NIFTYSEGCommand):
+
+    """
+
+    Examples
+    --------
+    from cropimage import CropImage
+    cropper = CropImage()
+    cropper.inputs.in_file = "T1.nii.gz"
+    cropper.inputs.mask_file = "mask.nii.gz"
+    cropper.inputs.out_file = "T1_cropped.nii.gz"
+    cropper.run()
+    """
+
+    _cmd = "CropImage.sh"
+    _suffix = "_crop_image"
+    input_spec = CropImageInputSpec  
+    output_spec = CropImageOutputSpec
+
+    def _gen_filename(self, name):
+        if name == 'out_file':
+            return self._gen_fname(self.inputs.in_file, suffix=self._suffix, ext='.nii.gz')
+        return None
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        
+        if isdefined(self.inputs.out_file) and self.inputs.out_file:
+            outputs['out_file'] = os.path.abspath(self.inputs.out_file)
+        else:
+            outputs['out_file'] = self._gen_filename('out_file')
+        
+        return outputs
+        
