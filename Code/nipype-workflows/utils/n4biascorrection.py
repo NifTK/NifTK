a4aef5ff40a5cd554ce5707bc2c8ea74b028299e
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-06-12 16:02:49 +0100

    Feature #3421: nipype-workflows: adding a wrapper for the N4 bias correction interface

---
+++ Code/nipype-workflows/utils/n4biascorrection.py
@@ -0,0 +1,83 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+    Simple interface for the CropImage.sh script
+"""
+
+import os
+import numpy as np
+from nibabel import load
+import os.path as op
+import warnings
+
+from nipype.interfaces.niftyseg.base import NIFTYSEGCommand, NIFTYSEGCommandInputSpec
+from nipype.interfaces.base import (TraitedSpec, File, Directory, traits, InputMultiPath,
+                                    isdefined)
+
+
+warn = warnings.warn
+warnings.filterwarnings('always', category=UserWarning)
+
+class N4BiasCorrectionInputSpec(NIFTYSEGCommandInputSpec):
+    
+    in_file = File(argstr="-i %s", exists=True, mandatory=True,
+                desc="Input target image filename")
+    
+    mask_file = File(exists=True, argstr="-m %s",
+                     desc="Mask over the input image")
+
+    out_file = File(argstr="-o %s", 
+                    desc="output bias corrected image",
+                    name_source = ['in_file'],
+                    name_template = '%s_corrected')
+
+    in_levels = traits.BaseInt(desc='Number of Multi-Scale Levels - optional - default = 3', 
+                               argstr='-n %d')
+
+    in_downsampling = traits.BaseInt(desc='Level of Downsampling - optional - default = 1 (no downsampling), downsampling to level 2 is recommended', 
+                                     argstr='-d %d')
+
+    in_maxiter = traits.BaseInt(desc='Maximum number of Iterations - optional - default = 50', 
+                                argstr='-t %d')
+
+    out_biasfield_file = File(argstr="-b %s", 
+                              desc="output bias field file",
+                              name_source = ['in_file'],
+                              name_template = '%s_biasfield')
+
+    in_convergence = traits.Float(desc='Convergence Threshold - optional - default = 0.001', argstr='-c %f')
+
+
+class N4BiasCorrectionOutputSpec(TraitedSpec):
+
+    out_file = File(exists=True, desc="output bias corrected image")
+    out_biasfield_file = File(desc="output bias field")
+
+class N4BiasCorrection(NIFTYSEGCommand):
+
+    """
+    
+    -i	 Input Image - mandatory
+    -o	 Output Image - mandatory
+    -m	 Binary Mask Image - optional
+    -n	 Number of Multi-Scale Levels - optional - default = 3
+    -d	 Level of Downsampling - optional - default = 1 (no downsampling), downsampling to level 2 is recommended
+    -t	 Maximum number of Iterations - optional - default = 50
+    -b	 Output Bias Field image - optional
+    -c	 Convergence Threshold - optional - default = 0.001
+    
+    Examples
+    --------
+    from n4biascorrection import N4BiasCorrection
+    biascorrect = N4BiasCorrection()
+    biascorrect.inputs.in_file = "T1.nii.gz"
+    biascorrect.inputs.mask_file = "mask.nii.gz"
+    biascorrect.inputs.out_file = "T1_cropped.nii.gz"
+    biascorrect.run()
+    """
+
+    _cmd = "n4biascorrection"
+
+    input_spec = N4BiasCorrectionInputSpec  
+    output_spec = N4BiasCorrectionOutputSpec
+
