d72dcb61893698a85e8e5e14878778b441dfc8d2
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-06-23 19:39:23 +0100

    Feature #3421: nipype workflow: dti reproducibility study: Adding a pipeline to test the different options for diffusion preprocessing from a template tensor field

---
+++ Code/nipype-workflows/dti_reproducibility_study/dti_reproducibility_study.py
@@ -0,0 +1,132 @@
+#! /usr/bin/env python
+
+import nipype.interfaces.utility        as niu     # utility
+import nipype.pipeline.engine           as pe          # pypeline engine
+import nipype.interfaces.niftyseg       as niftyseg
+import nipype.interfaces.niftyreg       as niftyreg
+import nipype.interfaces.niftyfit       as niftyfit
+import nipype.interfaces.fsl            as fsl
+import nipype.interfaces.susceptibility as susceptibility
+
+import diffusion_mri_processing         as dmri
+import nipype.interfaces.ttk            as ttk
+import epi_distortion_generation        as epi
+import math
+
+'''
+This file provides the creation of the whole workflow necessary for 
+processing diffusion MRI images.
+'''
+
+def create_dti_reproducibility_study_workflow(name='create_dti_reproducibility_study'):
+
+    """
+    Example
+    -------
+
+    >>> dti_repr_st = create_dti_reproducibility_study_workflow(name='dti_repr_st')
+    >>> dti_repr_st.inputs.
+
+
+    Inputs::
+
+        input_node.
+    Outputs::
+
+        output_node.
+
+
+    Optional arguments::
+        
+    """
+
+    workflow = pe.Workflow(name=name)
+    workflow.base_output_dir=name
+
+    # We need to create an input node for the workflow
+    input_node = pe.Node(
+        niu.IdentityInterface(
+            fields=['in_tensors_file',
+                    'in_T1_file',
+                    'in_B0_file',
+                    'in_bvec_file',
+                    'in_bval_file']),
+        name='input_node')
+    
+    distortion_generator = pe.Node(interface = epi.DistortionGenerator(), 
+                                   name = 'distortion_generator')
+    distortion_generator.inputs.in_var_translation = 0.2
+    distortion_generator.inputs.in_var_rotation = 0.01*math.pi/180
+    distortion_generator.inputs.in_var_shear = 0.0003
+    
+    tensor_resampling = pe.MapNode(interface = niftyreg.RegResample(), 
+                                   name = 'tensor_resampling', 
+                                   iterfield = ['trans_file'])
+
+    tensor_2_dwi = pe.MapNode(interface = ttk.Tensor2Dwi(), 
+                              name = 'tensor_2_dwi', 
+                              iterfield = ['in_file'])
+
+    merge_dwis = pe.Node(interface = fsl.Merge(), 
+                         direction = 't',
+                         name = 'merge_dwis')
+    
+    r = dmri.create_diffusion_mri_processing_workflow(name = 'dmri_workflow', correct_susceptibility = False)
+
+    inv_estimated_distortions = pe.MapNode(interface = niftyreg.RegTransform(), 
+                                           name = 'inv_estimated_distortions', 
+                                           iterfield = ['inv_aff_input'])
+
+    tensor_resampling_2 = pe.MapNode(interface = niftyreg.RegResample(), 
+                                     name = 'tensor_resampling_2', 
+                                     iterfield = ['trans_file'])
+
+    tensor_2_dwi_2 = pe.MapNode(interface = ttk.Tensor2Dwi(), 
+                                name = 'tensor_2_dwi_2', 
+                                iterfield = ['in_file'])
+
+    merge_dwis_2 = pe.Node(interface = fsl.Merge(), 
+                          direction = 't',
+                          name = 'merge_dwis_2')
+
+    # Output node
+    output_node = pe.Node( interface=niu.IdentityInterface(
+        fields=['simulated_dwis',
+                'estimated_dwis',
+                'simulated_tensors',
+                'estimated_tensors']),
+                           name="output_node" )
+    
+    workflow.connect(input_node, 'in_bval_file', distortion_generator, 'in_bval_file')
+    workflow.connect(input_node, 'in_bvec_file', distortion_generator, 'in_bvec_file')
+    workflow.connect(input_node, 'in_tensors_file', tensor_resampling, 'ref_file')
+    workflow.connect(input_node, 'in_tensors_file', tensor_resampling, 'flo_file')
+    workflow.connect(distortion_generator, 'aff_files', tensor_resampling, 'trans_file')
+    workflow.connect(tensor_resampling, 'res_file', tensor_2_dwi, 'in_file')
+    workflow.connect(input_node, 'in_bvec_file',    tensor_2_dwi, 'in_bvec_file')
+    workflow.connect(input_node, 'in_bval_file',    tensor_2_dwi, 'in_bval_file')
+    workflow.connect(input_node, 'in_B0_file',      tensor_2_dwi, 'in_B0_file')
+    workflow.connect(tensor_2_dwi, 'out_files', merge_dwis, 'in_files')
+    workflow.connect(merge_dwis, 'merged_file', r, 'input_node.in_dwi_4d_file')
+    workflow.connect(input_node, 'in_bvec_file', r, 'input_node.in_bvec_file')
+    workflow.connect(input_node, 'in_bval_file', r, 'input_node.in_bval_file')
+    workflow.connect(input_node, 'in_T1_file', r, 'input_node.in_T1_file')
+    workflow.connect(r, 'output_node.transformations', inv_estimated_distortions, 'inv_aff_input')
+    workflow.connect(r, 'output_node.tensor', tensor_resampling_2, 'ref_file')
+    workflow.connect(r, 'output_node.tensor', tensor_resampling_2, 'flo_file')
+    workflow.connect(inv_estimated_distortions, 'out_file', tensor_resampling_2, 'trans_file')
+    workflow.connect(tensor_resampling_2, 'res_file', tensor_2_dwi_2, 'in_file')
+    workflow.connect(input_node, 'in_bvec_file', tensor_2_dwi_2, 'in_bvec_file')
+    workflow.connect(input_node, 'in_bval_file', tensor_2_dwi_2, 'in_bval_file')
+    workflow.connect(input_node, 'in_B0_file', tensor_2_dwi_2, 'in_B0_file')
+    workflow.connect(tensor_2_dwi_2, 'out_files', merge_dwis_2, 'in_files')
+    workflow.connect(merge_dwis,'merged_file',output_node, 'simulated_dwis')
+    workflow.connect(merge_dwis_2, 'merged_file', output_node, 'estimated_dwis')
+    workflow.connect(input_node, 'in_tensors_file', output_node, 'simulated_tensors')
+    workflow.connect(r, 'output_node.tensor', output_node, 'estimated_tensors')
+    
+    return workflow
+    
+
+    # node explanation:
+    
---
+++ Code/nipype-workflows/dti_reproducibility_study/usage_example.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import dti_reproducibility_study as dmri
+import argparse
+parser = argparse.ArgumentParser(description='Diffusion usage example')
+parser.add_argument('-b','--basedir', help='Base directory that contains the diffusion data in the required format',
+                    required=True)
+args = parser.parse_args()
+
+basedir = args.basedir
+
+bvals = basedir + 'chicago-template.bval'
+bvecs = basedir + 'chicago-template.bvec'
+T1    = basedir + 'chicago-template-T1.nii.gz'
+tensors = basedir + 'chicago-template-tensors.nii.gz'
+
+r = dmri.create_dti_reproducibility_study_workflow('dti_reproducibility_study')
+r.base_dir = basedir
+
+r.inputs.input_node.in_tensors_file = tensors
+r.inputs.input_node.in_bvec_file = bvecs
+r.inputs.input_node.in_bval_file = bvals
+r.inputs.input_node.in_T1_file = T1
+
+r.write_graph(graph2use = 'hierarchical')
+
+qsubargs='-l h_rt=00:05:00 -l tmem=1.8G -l h_vmem=1.8G -l vf=2.8G -l s_stack=10240 -j y -b y -S /bin/csh -V'
+
+#r.run()
+#r.run(plugin='SGE',       plugin_args={'qsub_args': qsubargs})
+#r.run(plugin='SGEGraph',  plugin_args={'qsub_args': qsubargs})
+r.run(plugin='Linear')
+
