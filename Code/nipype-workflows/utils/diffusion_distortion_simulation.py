3b4c1bf6a5d2243168ef1560454064dfdfe3a565
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-06-30 17:21:01 +0100

    Feature #3421: diffusion-distortion interface: the node now also writes the bvec and bval corresponding to each distortion generated

--- Code/nipype-workflows/utils/diffusion_distortion_simulation.py
+++ Code/nipype-workflows/utils/diffusion_distortion_simulation.py
@@ -76,6 +76,8 @@ class DistortionGeneratorInputSpec(BaseInterfaceInputSpec):
     
 class DistortionGeneratorOutputSpec(TraitedSpec):
     aff_files   = OutputMultiPath(desc="Output distortion matrices")
+    bval_files  = OutputMultiPath(desc="Output bval files")
+    bvec_files  = OutputMultiPath(desc="Output bvec files")
 
 class DistortionGenerator(BaseInterface):
 
@@ -94,11 +96,19 @@ class DistortionGenerator(BaseInterface):
         _, base, _ = split_filename(bval_file)
         gtab = gradients.gradient_table(bval_file, bvec_file)
         b0_list = list(gtab.b0s_mask)
-        outfilenames = []
+        out_aff_filenames = []
+        out_bval_filenames = []
+        out_bvec_filenames = []
         for i in range(len(b0_list)):
-            outfile = base + self._suffix + str(i) + '.txt'
-            outfilenames.append(outfile)
-        return outfilenames
+            outbase = base + self._suffix + str(i)
+            outaff  = outbase + '.txt'
+            outbval = outbase + '.bval'
+            outbvec = outbase + '.bvec'
+            out_aff_filenames.append(outaff)
+            out_bval_filenames.append(outbval)
+            out_bvec_filenames.append(outbvec)
+
+        return out_aff_filenames, out_bval_filenames, out_bvec_filenames
 
     def _run_interface(self, runtime):
         std_trans = self.inputs.stddev_translation_val
@@ -109,9 +119,9 @@ class DistortionGenerator(BaseInterface):
         
         gtab = gradients.gradient_table(bval_file, bvec_file)
         b0_list = list(gtab.b0s_mask)
-
-        out_filenames = self._gen_output_filenames(bval_file, bvec_file)
-
+        
+        out_aff_filenames, out_bval_filenames, out_bvec_filenames = self._gen_output_filenames(bval_file, bvec_file)
+        
         for i in range(len(b0_list)):
             is_b0 = b0_list[i]
             if is_b0 == True:
@@ -119,24 +129,41 @@ class DistortionGenerator(BaseInterface):
             else:
                 shear = std_shear
             distortion = generate_distortion(std_trans,std_rot,shear)
-            outfile = out_filenames[i]
-            f=open(outfile, 'w+')
-            for i in range(len(distortion)-1):
-                l = str(distortion[i])
+            outaff = out_aff_filenames[i]
+            f=open(outaff, 'w+')
+            for j in range(len(distortion)):
+                l = str(distortion[j])
                 l = l.replace ('[', '')
                 l = l.replace (']', '')
                 l = l.replace (',', ' ')
                 f.write("%s\n" % l)
+            f.close
+
+            outbval = out_bval_filenames[i]
+            f=open(outbval, 'w+')
+            f.write("%f" % gtab.bvals[i])
+            f.close
+
+            outbvec = out_bvec_filenames[i]
+            f=open(outbvec, 'w+')
+            for j in range(3):
+               f.write("%f\n" % gtab.bvecs[i][j])
             f.close
             
         return runtime
     
     def _list_outputs(self):
         outputs = self.output_spec().get()
-        out_filenames = self._gen_output_filenames(self.inputs.bval_file, self.inputs.bvec_file)
+        out_aff_filenames, out_bval_filenames, out_bvec_filenames = self._gen_output_filenames(self.inputs.bval_file, self.inputs.bvec_file)
         aff_files = []
-        for item in out_filenames:
-            aff_files.append(os.path.abspath(item))
-        outputs['aff_files'] = aff_files
+        bval_files = []
+        bvec_files = []
+        for i in range(len(out_aff_filenames)):
+            aff_files.append(os.path.abspath(out_aff_filenames[i]))
+            bval_files.append(os.path.abspath(out_bval_filenames[i]))
+            bvec_files.append(os.path.abspath(out_bvec_filenames[i]))
+        outputs['aff_files']  = aff_files
+        outputs['bval_files'] = bval_files
+        outputs['bvec_files'] = bvec_files
         return outputs
         
