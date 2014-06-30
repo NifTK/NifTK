8639b717abd688199bf079f49e0a9103a20ddbcc
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-06-30 17:24:15 +0100

    Feature #3421: dti-likelihood workflow: the standard deviations for the distortions are now set outside of the workflow

--- Code/nipype-workflows/dti_likelihood_study/usage_example.py
+++ Code/nipype-workflows/dti_likelihood_study/usage_example.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python
 
-import dti_reproducibility_study as dmri
+import dti_likelihood_study as dmri
 import argparse
+import math
+
 parser = argparse.ArgumentParser(description='Diffusion usage example')
 parser.add_argument('-b','--basedir', help='Base directory that contains the diffusion data in the required format',
                     required=True)
@@ -15,7 +17,7 @@ T1    = basedir + 'chicago-template-T1.nii.gz'
 tensors = basedir + 'chicago-template-tensors.nii.gz'
 B0 = basedir + 'chicago-template-b0.nii.gz'
 
-r = dmri.create_dti_reproducibility_study_workflow('dti_reproducibility_study')
+r = dmri.create_dti_reproducibility_study_workflow(name='dti_likelihood_study')
 r.base_dir = basedir
 
 r.inputs.input_node.in_tensors_file = tensors
@@ -23,6 +25,9 @@ r.inputs.input_node.in_bvec_file = bvecs
 r.inputs.input_node.in_bval_file = bvals
 r.inputs.input_node.in_B0_file = B0
 r.inputs.input_node.in_T1_file = T1
+r.inputs.input_node.in_stddev_translation = 0.2
+r.inputs.input_node.in_stddev_rotation = 0.01*math.pi/180
+r.inputs.input_node.in_stddev_shear = 0.0003
 
 r.write_graph(graph2use = 'hierarchical')
 
@@ -31,5 +36,5 @@ qsubargs='-l h_rt=00:05:00 -l tmem=1.8G -l h_vmem=1.8G -l vf=2.8G -l s_stack=102
 #r.run()
 #r.run(plugin='SGE',       plugin_args={'qsub_args': qsubargs})
 #r.run(plugin='SGEGraph',  plugin_args={'qsub_args': qsubargs})
-r.run(plugin='Linear')
+r.run(plugin='MultiProc')
 
