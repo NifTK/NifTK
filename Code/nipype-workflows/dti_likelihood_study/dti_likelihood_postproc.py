c3c7f5039388ae6581609380146d53be39576116
Ivor Simpson <ivor.simpson@ucl.ac.uk>
2014-07-03 15:01:18 +0100

    Feature #3421: dti-likelihood workflow: Added nodes and connections to make the mean squared difference of the tensor images and calculate the mean and standard deviations of these differences within a set of ROIs

--- Code/nipype-workflows/dti_likelihood_study/dti_likelihood_postproc.py
+++ Code/nipype-workflows/dti_likelihood_study/dti_likelihood_postproc.py
@@ -7,6 +7,7 @@ import nipype.interfaces.niftyreg       as niftyreg
 import nipype.interfaces.niftyfit       as niftyfit
 import nipype.interfaces.fsl            as fsl
 import nipype.interfaces.ttk            as ttk
+from extract_roi_statistics import ExtractRoiStatistics
 
 def create_dti_likelihood_post_proc_workflow(name='dti_likelihood_post_proc'):
 
@@ -36,9 +37,23 @@ def create_dti_likelihood_post_proc_workflow(name='dti_likelihood_post_proc'):
                       name = 'logger2')
     logger2.inputs.use_fsl_style = True
     
-    substracter = pe.Node(interface = fsl.maths.BinaryMaths(), 
-                          name = 'substracter')
-    substracter.inputs.operation = 'sub'
+    tensor_subtracter = pe.Node(interface = niftyseg.maths.BinaryMaths(), 
+                          name = 'tensor_subtracter')
+    tensor_subtracter.inputs.operation = 'sub'
+    
+    tensor_sqr = pe.Node(interface=niftyseg.maths.BinaryMaths(),
+                         name = 'tensor_squarer')
+    tensor_sqr.inputs.operation = 'mul'
+    
+    tensor_tmean = pe.Node(interface= niftyseg.maths.UnaryMaths(), 
+                           name = 'tensor_meaner')
+    tensor_tmean.inputs.operation = 'tmean'
+    
+    #dwi_subtracter = pe.Node(interface = fsl.maths.BinaryMaths(), 
+    #                      name = 'dwi_substracter')
+    #dwi_subtracter.inputs.operation = 'sub'
+    
+    roi_stats = pe.Node(interface=ExtractRoiStatistics(), name='roi_stats')    
     
     output_node = pe.Node(
         niu.IdentityInterface(
@@ -49,10 +64,23 @@ def create_dti_likelihood_post_proc_workflow(name='dti_likelihood_post_proc'):
     workflow.connect(input_node, 'simulated_tensors', logger1, 'in_file')
     workflow.connect(input_node, 'estimated_tensors', logger2, 'in_file')
     
-    workflow.connect(logger1, 'out_file', substracter, 'in_file')
-    workflow.connect(logger2, 'out_file', substracter, 'operand_file')
+    workflow.connect(logger1, 'out_file',  tensor_subtracter, 'in_file')
+    workflow.connect(logger2, 'out_file',  tensor_subtracter, 'operand_file')
     
-    workflow.connect(substracter, 'out_file', output_node, 'tensor_metric_map')
+    workflow.connect(tensor_subtracter, 'out_file', tensor_sqr, 'in_file')
+    workflow.connect(tensor_subtracter, 'out_file', tensor_sqr, 'operand_file')
+    
+    workflow.connect(tensor_sqr, 'out_file', tensor_tmean,'in_file')
+    
+    #workflow.connect(input_node,'simulated_dwis',  dwi_subtracter, 'in_file')
+    #workflow.connect(input_node, 'estimated_dwis',  dwi_subtracter, 'operand_file')
+    
+    workflow.connect(tensor_tmean, 'out_file', roi_stats, 'in_file')
+    workflow.connect(input_node, 'labels_file', roi_stats, 'roi_file')
+    
+    
+    #workflow.connect( tensor_subtracter, 'out_file', output_node, 'tensor_metric_map')
+    workflow.connect(roi_stats, 'out_array', output_node, 'tensor_metric_ROI_statistics')
     #   workflow.connect(??, output_node, 'tensor_metric_ROI_statistics')
     
     return workflow
