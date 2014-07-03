4c54b7fb2fe25b8979f939c01b7909f1347b4776
Ivor Simpson <ivor.simpson@ucl.ac.uk>
2014-07-03 17:53:05 +0100

    Feature #3421: nipype workflow: Added a wrapped function in utils to write an array to a csv file. Modified the dti-likelihood workflow to write the output to file

--- Code/nipype-workflows/dti_likelihood_study/dti_likelihood_postproc.py
+++ Code/nipype-workflows/dti_likelihood_study/dti_likelihood_postproc.py
@@ -8,6 +8,7 @@ import nipype.interfaces.niftyfit       as niftyfit
 import nipype.interfaces.fsl            as fsl
 import nipype.interfaces.ttk            as ttk
 from extract_roi_statistics import ExtractRoiStatistics
+from write_array_to_csv import WriteArrayToCsv
 
 def create_dti_likelihood_post_proc_workflow(name='dti_likelihood_post_proc'):
 
@@ -49,16 +50,35 @@ def create_dti_likelihood_post_proc_workflow(name='dti_likelihood_post_proc'):
                            name = 'tensor_meaner')
     tensor_tmean.inputs.operation = 'tmean'
     
-    #dwi_subtracter = pe.Node(interface = fsl.maths.BinaryMaths(), 
-    #                      name = 'dwi_substracter')
-    #dwi_subtracter.inputs.operation = 'sub'
+    dwi_subtracter = pe.Node(interface = fsl.maths.BinaryMaths(), 
+                          name = 'dwi_substracter')
+    dwi_subtracter.inputs.operation = 'sub'
     
-    roi_stats = pe.Node(interface=ExtractRoiStatistics(), name='roi_stats')    
+    dwi_sqr = pe.Node(interface=niftyseg.maths.BinaryMaths(),
+                         name = 'dwi_squarer')
+    dwi_sqr.inputs.operation = 'mul'
+    
+    dwi_tmean = pe.Node(interface= niftyseg.maths.UnaryMaths(), 
+                           name = 'dwi_meaner')
+    dwi_tmean.inputs.operation = 'tmean'
+    
+    array_tensor_writer = pe.Node(interface=WriteArrayToCsv(),name='tensor_stat_writer')
+    array_tensor_writer.inputs.in_name = 'tensor_stats'
+    
+    array_dwi_writer = pe.Node(interface=WriteArrayToCsv(),name='dwi_stat_writer')
+    array_dwi_writer.inputs.in_name = 'dwi_stats'
+    
+    
+    roi_tensor_stats = pe.Node(interface=ExtractRoiStatistics(), name='roi_tensor_stats')
+    roi_dwi_stats = pe.Node(interface=ExtractRoiStatistics(), name='roi_dwi_stats')  
     
     output_node = pe.Node(
         niu.IdentityInterface(
             fields=['tensor_metric_map',
-                    'tensor_metric_ROI_statistics']),
+                    'tensor_metric_ROI_statistics',
+                    'dwi_metric_map',
+                    'dwi_metric_ROI_statistics',
+                    'dwi_likelihood']),
         name='output_node')
     
     workflow.connect(input_node, 'simulated_tensors', logger1, 'in_file')
@@ -69,18 +89,28 @@ def create_dti_likelihood_post_proc_workflow(name='dti_likelihood_post_proc'):
     
     workflow.connect(tensor_subtracter, 'out_file', tensor_sqr, 'in_file')
     workflow.connect(tensor_subtracter, 'out_file', tensor_sqr, 'operand_file')
-    
     workflow.connect(tensor_sqr, 'out_file', tensor_tmean,'in_file')
     
-    #workflow.connect(input_node,'simulated_dwis',  dwi_subtracter, 'in_file')
-    #workflow.connect(input_node, 'estimated_dwis',  dwi_subtracter, 'operand_file')
+    workflow.connect(input_node,'simulated_dwis',  dwi_subtracter, 'in_file')
+    workflow.connect(input_node, 'estimated_dwis',  dwi_subtracter, 'operand_file')
+    workflow.connect(dwi_subtracter, 'out_file', dwi_sqr, 'in_file')
+    workflow.connect(dwi_subtracter, 'out_file', dwi_sqr, 'operand_file')
+    workflow.connect(dwi_sqr, 'out_file', dwi_tmean,'in_file')
     
-    workflow.connect(tensor_tmean, 'out_file', roi_stats, 'in_file')
-    workflow.connect(input_node, 'labels_file', roi_stats, 'roi_file')
+    workflow.connect(tensor_tmean, 'out_file', roi_tensor_stats, 'in_file')
+    workflow.connect(input_node, 'labels_file', roi_tensor_stats, 'roi_file')
     
+    workflow.connect(dwi_tmean, 'out_file', roi_dwi_stats, 'in_file')
+    workflow.connect(input_node, 'labels_file', roi_dwi_stats, 'roi_file')
     
-    #workflow.connect( tensor_subtracter, 'out_file', output_node, 'tensor_metric_map')
-    workflow.connect(roi_stats, 'out_array', output_node, 'tensor_metric_ROI_statistics')
+    workflow.connect(roi_tensor_stats, 'out_array', array_tensor_writer, 'in_array')
+    workflow.connect(roi_dwi_stats, 'out_array', array_dwi_writer, 'in_array')
+    
+    workflow.connect( tensor_subtracter, 'out_file', output_node, 'tensor_metric_map')
+    workflow.connect(array_tensor_writer, 'out_file', output_node, 'tensor_metric_ROI_statistics')
+    
+    workflow.connect(dwi_subtracter, 'out_file', output_node, 'dwi_metric_map')
+    workflow.connect(array_dwi_writer, 'out_file', output_node, 'dwi_metric_ROI_statistics')
     #   workflow.connect(??, output_node, 'tensor_metric_ROI_statistics')
     
     return workflow
