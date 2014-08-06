27eb30835a82ac220cde0ab7cf5bfc9999d57322
Steve Thompson <s.thompson@ucl.ac.uk>
2014-08-06 13:06:00 +0100

    Feature #3701 added check that cvQuery frame has worked

--- Code/Gui/MITK/Modules/niftkOpenCV/VideoTrackerMatching/mitkFindAndTriangulateCrossHair.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCV/VideoTrackerMatching/mitkFindAndTriangulateCrossHair.cxx
@@ -115,7 +115,7 @@ void FindAndTriangulateCrossHair::Initialise(std::string directory,
     m_VideoIn = videoFiles[0];
    
     m_Capture = cvCreateFileCapture(m_VideoIn.c_str()); 
-    m_VideoWidth = (double)cvGetCaptureProperty (m_Capture, CV_CAP_PROP_FRAME_WIDTH);
+    m_VideoWidth = (int)cvGetCaptureProperty (m_Capture, CV_CAP_PROP_FRAME_WIDTH);
     m_VideoHeight = (double)cvGetCaptureProperty (m_Capture, CV_CAP_PROP_FRAME_HEIGHT);
     
     MITK_INFO << "Opened " << m_VideoIn << " ( " << m_VideoWidth << " x " << m_VideoHeight << " )";
@@ -180,64 +180,76 @@ void FindAndTriangulateCrossHair::Triangulate()
   m_ScreenPoints.clear();
   while ( framenumber < m_TrackerMatcher->GetNumberOfFrames() && key != 'q')
   {
-
     cv::Mat videoImage = cvQueryFrame ( m_Capture ) ;
     leftFrame = videoImage.clone();
     videoImage = cvQueryFrame ( m_Capture ) ;
     rightFrame = videoImage.clone();
-    
-    cv::cvtColor( leftFrame, leftHough, CV_BGR2GRAY );
-    cv::cvtColor( rightFrame, rightHough, CV_BGR2GRAY );
-    int lowThreshold=20;
-    int highThreshold = 70;
-    int kernel = 3;
-    cv::Canny(leftHough,leftCanny, lowThreshold,highThreshold,kernel);
-    cv::Canny(rightHough,rightCanny, lowThreshold,highThreshold,kernel);
-    cv::vector<cv::Vec4i> linesleft;
-    cv::vector<cv::Vec4i> linesright;
-    cv::HoughLinesP (leftCanny, linesleft,m_HoughRho,m_HoughTheta, m_HoughThreshold, m_HoughLineLength , m_HoughLineGap);  
-    cv::HoughLinesP (rightCanny, linesright,m_HoughRho,m_HoughTheta, m_HoughThreshold, m_HoughLineLength , m_HoughLineGap);  
+
     std::pair <cv::Point2d, cv::Point2d> screenPoints;
     screenPoints.first = cv::Point2d(-100.0, -100.0);
     screenPoints.second = cv::Point2d(-100.0, -100.0);
-    for ( unsigned int i = 0 ; i < linesleft.size() ; i ++ )
+    //check data 
+    //
+    if ( ( leftFrame.cols != m_VideoWidth ) || ( leftFrame.rows != m_VideoHeight )
+        || ( rightFrame.cols != m_VideoWidth ) || (rightFrame.rows != m_VideoHeight ) )
+    if ( ( leftFrame.cols == 0  ) || ( leftFrame.rows == 0 )
+        || ( rightFrame.cols == 0 ) || (rightFrame.rows == 0  ) )
     {
-      cv::line(leftFrame,cvPoint(linesleft[i][0],linesleft[i][1]),
-          cvPoint(linesleft[i][2],linesleft[i][3]),cvScalar(255,0,0));
+      m_ScreenPoints.push_back(screenPoints);
+      MITK_WARN << "Failed to read video at frame " << m_VideoWidth << m_VideoHeight << leftFrame.cols << leftFrame.rows << framenumber;
     }
-    for ( unsigned int i = 0 ; i < linesright.size() ; i ++ )
+    else
     {
-      cv::line(rightFrame,cvPoint(linesright[i][0],linesright[i][1]),
-          cvPoint(linesright[i][2],linesright[i][3]),cvScalar(255,0,0));
-    }
-    std::vector <cv::Point2d> leftIntersectionPoints = mitk::FindIntersects (linesleft, true, true);
-    std::vector <cv::Point2d> rightIntersectionPoints = mitk::FindIntersects (linesright, true, true);
-    screenPoints.first = mitk::GetCentroid(leftIntersectionPoints,true);
-    screenPoints.second = mitk::GetCentroid(rightIntersectionPoints,true);
-    cv::circle(leftFrame , screenPoints.first,10, cvScalar(0,0,255),2,8,0);
-    cv::circle(rightFrame , screenPoints.second,10, cvScalar(0,255,0),2,8,0);
-    m_ScreenPoints.push_back(screenPoints);
-    if ( m_Visualise ) 
-    {
-      IplImage leftIpl(leftFrame);
-      IplImage rightIpl(rightFrame);
-      IplImage processedIpl(leftCanny);
-      IplImage *smallleft = cvCreateImage (cvSize(960, 270), 8,3);
-      cvResize (&leftIpl, smallleft,CV_INTER_LINEAR);
-      IplImage *smallright = cvCreateImage (cvSize(960, 270), 8,3);
-      cvResize (&rightIpl, smallright,CV_INTER_LINEAR);
-      IplImage *smallprocessed = cvCreateImage (cvSize(960, 270), 8,1);
-      cvResize (&processedIpl, smallprocessed , CV_INTER_LINEAR);
-      cvShowImage("Left Channel" , smallleft);
-      cvShowImage("Right Channel" , smallright);
-      cvShowImage("Processed Left", smallprocessed);
-      key = cvWaitKey (20);
-      if ( key == 's' )
+    
+      cv::cvtColor( leftFrame, leftHough, CV_BGR2GRAY );
+      cv::cvtColor( rightFrame, rightHough, CV_BGR2GRAY );
+      int lowThreshold=20;
+      int highThreshold = 70;
+      int kernel = 3;
+      cv::Canny(leftHough,leftCanny, lowThreshold,highThreshold,kernel);
+      cv::Canny(rightHough,rightCanny, lowThreshold,highThreshold,kernel);
+      cv::vector<cv::Vec4i> linesleft;
+      cv::vector<cv::Vec4i> linesright;
+      cv::HoughLinesP (leftCanny, linesleft,m_HoughRho,m_HoughTheta, m_HoughThreshold, m_HoughLineLength , m_HoughLineGap);  
+      cv::HoughLinesP (rightCanny, linesright,m_HoughRho,m_HoughTheta, m_HoughThreshold, m_HoughLineLength , m_HoughLineGap);  
+      for ( unsigned int i = 0 ; i < linesleft.size() ; i ++ )
       {
-        m_Visualise = false;
+        cv::line(leftFrame,cvPoint(linesleft[i][0],linesleft[i][1]),
+            cvPoint(linesleft[i][2],linesleft[i][3]),cvScalar(255,0,0));
+      }
+      for ( unsigned int i = 0 ; i < linesright.size() ; i ++ )
+      {
+        cv::line(rightFrame,cvPoint(linesright[i][0],linesright[i][1]),
+            cvPoint(linesright[i][2],linesright[i][3]),cvScalar(255,0,0));
+      }
+      std::vector <cv::Point2d> leftIntersectionPoints = mitk::FindIntersects (linesleft, true, true);
+      std::vector <cv::Point2d> rightIntersectionPoints = mitk::FindIntersects (linesright, true, true);
+      screenPoints.first = mitk::GetCentroid(leftIntersectionPoints,true);
+      screenPoints.second = mitk::GetCentroid(rightIntersectionPoints,true);
+      cv::circle(leftFrame , screenPoints.first,10, cvScalar(0,0,255),2,8,0);
+      cv::circle(rightFrame , screenPoints.second,10, cvScalar(0,255,0),2,8,0);
+      m_ScreenPoints.push_back(screenPoints);
+      if ( m_Visualise ) 
+      {
+        IplImage leftIpl(leftFrame);
+        IplImage rightIpl(rightFrame);
+        IplImage processedIpl(leftCanny);
+        IplImage *smallleft = cvCreateImage (cvSize(960, 270), 8,3);
+        cvResize (&leftIpl, smallleft,CV_INTER_LINEAR);
+        IplImage *smallright = cvCreateImage (cvSize(960, 270), 8,3);
+        cvResize (&rightIpl, smallright,CV_INTER_LINEAR);
+        IplImage *smallprocessed = cvCreateImage (cvSize(960, 270), 8,1);
+        cvResize (&processedIpl, smallprocessed , CV_INTER_LINEAR);
+        cvShowImage("Left Channel" , smallleft);
+        cvShowImage("Right Channel" , smallright);
+        cvShowImage("Processed Left", smallprocessed);
+        key = cvWaitKey (20);
+        if ( key == 's' )
+        {
+          m_Visualise = false;
+        }
       }
     }
-
     framenumber ++;
     framenumber ++;
   }
