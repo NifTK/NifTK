0fee968ce58b53d971c1a530ecdc6c9103a2cc9a
Steve Thompson <s.thompson@ucl.ac.uk>
2014-08-12 14:17:20 +0100

    Feature '3701 added unit tests to check point type creation and NaN handling

--- Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkOpenCVPointTypesTest.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCV/Testing/mitkOpenCVPointTypesTest.cxx
@@ -23,12 +23,35 @@
 #include <cmath>
 
 /**
- * \file Test harness for inversion routines.
+ * \file Test mitkOpenCVPointTypes
  */
 int mitkOpenCVPointTypesTest(int argc, char * argv[])
 {
   // always start with this!
   MITK_TEST_BEGIN("mitkPointTypesTest");
+
+  mitk::ProjectedPointPair point1 = mitk::ProjectedPointPair ( 
+      cv::Point2d ( std::numeric_limits<double>::quiet_NaN(), 0.0),
+      cv::Point2d ( 0.0, 0.0));
+  mitk::ProjectedPointPair point2 = mitk::ProjectedPointPair ();
+  mitk::ProjectedPointPair point3 = mitk::ProjectedPointPair (
+            cv::Point2d ( std::numeric_limits<double>::infinity(), 0.0),
+            cv::Point2d ( std::numeric_limits<double>::quiet_NaN(), 0.0));
+  mitk::ProjectedPointPair point4 = mitk::ProjectedPointPair (
+            cv::Point2d ( 1.0, 0.0),
+            cv::Point2d ( 0.0, 0.0));
+
+  MITK_TEST_CONDITION ( point1.LeftNaN() == true, "Testing leftNaN handling 1" ) ;
+  MITK_TEST_CONDITION ( point1.RightNaN() == false, "Testing rightNaN handling 1" ) ;
+
+  MITK_TEST_CONDITION ( point2.LeftNaN() == true, "Testing leftNaN handling 2" ) ;
+  MITK_TEST_CONDITION ( point2.RightNaN() == true, "Testing rightNaN handling 2" ) ;
+
+  MITK_TEST_CONDITION ( point3.LeftNaN() == false, "Testing leftNaN handling 3" ) ;
+  MITK_TEST_CONDITION ( point3.RightNaN() == true, "Testing rightNaN handling 3" ) ;
+
+  MITK_TEST_CONDITION ( point4.LeftNaN() == false, "Testing leftNaN handling 4" ) ;
+  MITK_TEST_CONDITION ( point4.RightNaN() == false, "Testing rightNaN handling 4" ) ;
 
   mitk::WorldPoint x;
   MITK_TEST_END();
