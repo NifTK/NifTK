0f2ea68b4100443afec3d2d5792149a410886ac4
Matt Clarkson <m.clarkson@ucl.ac.uk>
2014-08-14 19:40:38 +0100

    Bug #3725: Remove redundant call to RegisterNifTKCoreObjectFactory in tests.

--- Code/Gui/MITK/Modules/niftkCore/Testing/mitkMIDASImageUtilsTest.cxx
+++ Code/Gui/MITK/Modules/niftkCore/Testing/mitkMIDASImageUtilsTest.cxx
@@ -44,8 +44,6 @@ public:
   {
     std::string fileName = file;
 
-    // Need to load images, specifically using MIDAS/DRC object factory.
-    RegisterNifTKCoreObjectFactory();
     mitk::DataStorage::Pointer dataStorage;
     dataStorage = mitk::StandaloneDataStorage::New();
 
