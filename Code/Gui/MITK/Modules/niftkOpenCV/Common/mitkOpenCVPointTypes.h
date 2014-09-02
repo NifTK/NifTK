210279c8a128de5ab4d1fe7915182c70dfaca246
Matt Clarkson <m.clarkson@ucl.ac.uk>
2014-09-02 06:47:11 +0100

    Bug #3775: Formatting

--- Code/Gui/MITK/Modules/niftkOpenCV/Common/mitkOpenCVPointTypes.h
+++ Code/Gui/MITK/Modules/niftkOpenCV/Common/mitkOpenCVPointTypes.h
@@ -27,115 +27,123 @@
  */
 namespace mitk {
 
-
+/**
+* \class contains the gold standard points
+* consisting of the frame number, the point and optionally the point index
+*/
 class NIFTKOPENCV_EXPORT GoldStandardPoint
 {
+public:
+
+  GoldStandardPoint();
+  GoldStandardPoint(unsigned int , int, cv::Point2d);
+  GoldStandardPoint(std::istream& is);
+
   /**
-  * \class contains the gold standard points
-  * consisting of the frame number, the point and optionally the point index
+  * \brief an input operator
   */
-  public:
-    GoldStandardPoint();
-    GoldStandardPoint(unsigned int , int, cv::Point2d);
-    GoldStandardPoint(std::istream& is);
-    unsigned int m_FrameNumber;
-    int m_Index;
-    cv::Point2d  m_Point;
+  friend std::istream& operator>> (std::istream& is, const GoldStandardPoint& gsp );
 
-    /** 
-    * \brief an input operator
-    */
-    friend std::istream& operator>> (std::istream& is, const GoldStandardPoint& gsp );
+  friend bool operator < ( const GoldStandardPoint &GSP1 , const GoldStandardPoint &GSP2);
 
-    friend bool operator < ( const GoldStandardPoint &GSP1 , const GoldStandardPoint &GSP2);
+  unsigned int m_FrameNumber;
+  int m_Index;
+  cv::Point2d  m_Point;
 };
 
+
+/**
+ * \class contains a point in 3D and a corresponding scalar value
+ */
 class NIFTKOPENCV_EXPORT WorldPoint
 {
-  /**
-   * \class contains a point in 3D and a corresponding scalar value
-   */
-   public:
-     WorldPoint();
-     WorldPoint(cv::Point3d, cv::Scalar);
-     WorldPoint(cv::Point3d);
+public:
 
-     cv::Point3d  m_Point;
-     cv::Scalar   m_Scalar;
-     
-     bool IsNaN ();
+  WorldPoint();
+  WorldPoint(cv::Point3d, cv::Scalar);
+  WorldPoint(cv::Point3d);
+
+  bool IsNaN ();
+
+  cv::Point3d m_Point;
+  cv::Scalar m_Scalar;
 };
 
+
+/**
+ * \class contains a vector of world points and a corresponding long long timing error
+ */
 class NIFTKOPENCV_EXPORT WorldPointsWithTimingError
 {
-  /**
-   * \class contains a vector of world points and a corresponding long long timing error
-   */
-   public:
-     WorldPointsWithTimingError();
-     WorldPointsWithTimingError(std::vector <mitk::WorldPoint>, long long);
-     WorldPointsWithTimingError(std::vector <mitk::WorldPoint>);
+public:
 
-     std::vector <mitk::WorldPoint> m_Points;
-     long long                      m_TimingError;
+  WorldPointsWithTimingError();
+  WorldPointsWithTimingError(std::vector <mitk::WorldPoint>, long long);
+  WorldPointsWithTimingError(std::vector <mitk::WorldPoint>);
+
+  std::vector <mitk::WorldPoint> m_Points;
+  long long                      m_TimingError;
 };
 
 
+/**
+ * \class contains a left and right projected point
+ */
 class NIFTKOPENCV_EXPORT ProjectedPointPair
 {
-  /**
-   * \class contains a left and right projected point
-   */
-  public:
-    ProjectedPointPair();
-    ProjectedPointPair(cv::Point2d, cv::Point2d);
+public:
+  ProjectedPointPair();
+  ProjectedPointPair(cv::Point2d, cv::Point2d);
 
-    cv::Point2d m_Left;
-    cv::Point2d m_Right;
+  bool LeftNaNOrInf ();
+  bool RightNaNOrInf ();
 
-    bool LeftNaNOrInf ();
-    bool RightNaNOrInf ();
+  cv::Point2d m_Left;
+  cv::Point2d m_Right;
 };
 
+
+/**
+ * \class contains a vector of left and right projected points and a timing error
+ */
 class NIFTKOPENCV_EXPORT ProjectedPointPairsWithTimingError
 {
-  /**
-   * \class contains a vector of left and right projected points and a timing error
-   */
-  public:
-    ProjectedPointPairsWithTimingError();
-    ProjectedPointPairsWithTimingError(std::vector <mitk::ProjectedPointPair>, long long);
-    ProjectedPointPairsWithTimingError(std::vector <mitk::ProjectedPointPair>);
+public:
+  ProjectedPointPairsWithTimingError();
+  ProjectedPointPairsWithTimingError(std::vector <mitk::ProjectedPointPair>, long long);
+  ProjectedPointPairsWithTimingError(std::vector <mitk::ProjectedPointPair>);
 
-    std::vector <mitk::ProjectedPointPair> m_Points;
-    long long                              m_TimingError; 
+  std::vector <mitk::ProjectedPointPair> m_Points;
+  long long                              m_TimingError;
 };
 
+
+/**
+ * \class contains an opencv matrix of video data, a left or right flag and the
+ * timestamp
+ */
 class NIFTKOPENCV_EXPORT VideoFrame
 {
-  /** 
-   * \class contains an opencv matrix of video data, a left or right flag and the 
-   * timestamp
-   */
+public:
 
-  public:
-    VideoFrame();
-    VideoFrame(cv::VideoCapture* capture, std::ifstream* frameMapLogFile);
+  VideoFrame();
+  VideoFrame(cv::VideoCapture* capture, std::ifstream* frameMapLogFile);
 
-    itkGetMacro   (VideoData, cv::Mat);
-    itkGetMacro   (TimeStamp, unsigned long long);
-    
-    bool WriteToFile (std::string prefix);
-    void OutputVideoInformation (cv::VideoCapture* capture);
+  itkGetMacro   (VideoData, cv::Mat);
+  itkGetMacro   (TimeStamp, unsigned long long);
 
-  private:
-    cv::Mat             m_VideoData;
-    unsigned long long  m_TimeStamp;
-    bool                m_Left;
+  bool WriteToFile (std::string prefix);
+  void OutputVideoInformation (cv::VideoCapture* capture);
 
-    unsigned int        m_FrameNumber;
-    unsigned int        m_SequenceNumber;
-    unsigned int        m_Channel;
+private:
+
+  cv::Mat             m_VideoData;
+  unsigned long long  m_TimeStamp;
+  bool                m_Left;
+
+  unsigned int        m_FrameNumber;
+  unsigned int        m_SequenceNumber;
+  unsigned int        m_Channel;
 };
 
 } // end namespace
