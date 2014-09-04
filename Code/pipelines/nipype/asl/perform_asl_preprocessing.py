88268dfe8d44d3bfa2ce1284317ca164d0277cab
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-09-04 15:44:25 +0100

    Feature #3421: NiftyFit ASL processing pipeline: adding the necessary files allowing the processing of ASL data as a nipype workflow, ready to use, requres NiftyFit binaries (in particular fit_asl) to be available from the environment path

---
+++ Code/pipelines/nipype/asl/perform_asl_preprocessing.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+
+import nipype.interfaces.utility        as niu            
+import nipype.interfaces.io             as nio     
+import nipype.pipeline.engine           as pe
+import nipype.interfaces.dcm2nii        as mricron
+import nipype.interfaces.niftyreg       as niftyreg
+import nipype.interfaces.niftyseg       as niftyseg
+import asl_processing                as asl
+import argparse
+import os
+help_message = \
+'Perform Arterial Spin Labelling Fitting with pre-processing steps. \n\n' + \
+'Mandatory Input is the 4D nifti image ASL sequence. \n' + \
+'as well as a T1 image for reference space. \n\n' 
+
+parser = argparse.ArgumentParser(description=help_message)
+
+parser.add_argument('-s', '--source',
+                    dest='source',
+                    required=True,
+                    help='ASL sequence 4D source file')
+parser.add_argument('-t', '--t1',
+                    dest='t1',
+                    required=False,
+                    help='T1 structural image file')
+parser.add_argument('-o', '--output',
+                    dest='output',
+                    metavar='output',
+                    help='Result directory where the output data is to be stored',
+                    required=False,
+                    default='results')
+
+args = parser.parse_args()
+
+result_dir = os.path.abspath(args.output)
+
+if not os.path.exists(result_dir):
+    os.mkdir(result_dir)
+
+r = asl.create_asl_processing_workflow(name = 'asl_workflow')
+r.base_dir = os.getcwd()
+
+r.get_node('input_node').inputs.in_source = os.path.abspath(args.source)
+
+ds = pe.Node(nio.DataSink(), name='ds')
+ds.inputs.base_directory = result_dir
+ds.inputs.parameterization = False
+
+r.connect(r.get_node('output_node'), 'cbf_file', ds, '@cbf_file')
+r.connect(r.get_node('output_node'), 'syn_file', ds, '@syn_file')
+r.connect(r.get_node('output_node'), 'error_file', ds, '@error_file')
+
+r.write_graph(graph2use = 'colored')
+
+qsubargs='-l h_rt=00:05:00 -l tmem=1.8G -l h_vmem=1.8G -l vf=2.8G -l s_stack=10240 -j y -b y -S /bin/csh -V'
+#r.run(plugin='SGE',       plugin_args={'qsub_args': qsubargs})
+r.run(plugin='MultiProc')
+
