124b116c7615501068b7ecd33934f5dd02cf5a01
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-09-22 12:55:14 +0100

    Feature #3421: the XNAT source and sink test has been re-initiated, the Source works and the sink requires the assessment to be created with a report prior to the upload

--- Code/pipelines/nipype/xnat/download_xnat_data_and_upload_mask.py
+++ Code/pipelines/nipype/xnat/download_xnat_data_and_upload_mask.py
@@ -12,17 +12,22 @@ import argparse
 import os, sys
 import pyxnat
 
-parser = argparse.ArgumentParser(description='Diffusion usage example')
+mni_template = os.path.join(os.environ['FSLDIR'], 'data', 'standard', 'MNI152_T1_2mm.nii.gz')
+mni_template_mask = os.path.join(os.environ['FSLDIR'], 'data', 'standard', 'MNI152_T1_2mm_brain_mask_dil.nii.gz')
+
+parser = argparse.ArgumentParser(description='XNAT usage example')
 parser.add_argument('-i', '--server',
                     dest='server',
                     metavar='server',
                     help='XNAT server from where the data is taken',
-                    required=True)
+                    required=False,
+                    default='https://cmic-xnat.cs.ucl.ac.uk')
 parser.add_argument('-u', '--username',
                     dest='username',
                     metavar='username',
                     help='xnat server username',
-                    required=True)
+                    required=False,
+                    default = 'ntoussaint')
 parser.add_argument('-q', '--password',
                     dest='password',
                     metavar='password',
@@ -32,13 +37,15 @@ parser.add_argument('-p', '--project',
                     dest='project',
                     metavar='project',
                     help='xnat server project',
-                    required=True)
+                    required=False,
+                    default='ADNI')
 parser.add_argument('-s', '--subjects',
                     dest='subjects',
                     metavar='subjects',
                     help='xnat server subjects',
-                    required=True,
-                    nargs='+')
+                    required=False,
+                    nargs='+',
+                    default = '0003')
 parser.add_argument('-o', '--output',
                     dest='output',
                     metavar='output',
@@ -81,8 +88,8 @@ for subject in args.subjects:
         continue
 
     i = i+1
-    r = pe.Workflow(name='xnat_grabber_'+str(i))
-    r.base_output_dir='xnat_grabber'
+    r = pe.Workflow(name='xnat_download_upload_workflow_'+str(i))
+    r.base_output_dir=current_dir
     r.base_dir = current_dir
     
     dg = pe.Node(interface = nio.XNATSource(infields=['project','subject'],
@@ -105,7 +112,12 @@ for subject in args.subjects:
     dcm2nii.inputs.reorient_and_crop = False
 #'/project/ADNI/subjects/0002/experiments/*/assessors/BET_MASK/resources/NIFTI
 
-    bet = pe.MapNode(interface = fsl.BET(mask=True), name = 'bet', iterfield = ['in_file'])
+    mni_to_input = pe.Node(interface=niftyreg.RegAladin(), name='mni_to_input')
+    mni_to_input.inputs.flo_file = mni_template
+    
+    mask_resample  = pe.Node(interface = niftyreg.RegResample(), name = 'mask_resample')
+    mask_resample.inputs.inter_val = 'NN'
+    mask_resample.inputs.flo_file = mni_template_mask
 
     dsx = pe.Node(interface = nio.XNATSink(),
                  name = 'dsx')
@@ -115,15 +127,24 @@ for subject in args.subjects:
     dsx.inputs.project_id = args.project
     dsx.inputs.subject_id = subject
     dsx.inputs.experiment_id = first_mr_experiment.label()
-    dsx.inputs.assessor_id = 'BET_MASK_2'
-
-    assessor = first_mr_experiment.assessor('BET_MASK_2')
+    dsx.inputs.assessor_id = 'BRAIN_MASK'
+    
+    assessor = first_mr_experiment.assessor('BRAIN_MASK')
     if not assessor.exists():
         assessor.create()
 
     r.connect(dg, 'struct', dcm2nii, 'source_names')
-    r.connect(dcm2nii, 'converted_files', bet, 'in_file')
-    r.connect(bet, 'mask_file', dsx, '@xnat_data')
+    r.connect(dcm2nii, 'converted_files', mni_to_input, 'ref_file')
+    r.connect(dcm2nii, 'converted_files', mask_resample, 'ref_file')
+    r.connect(mni_to_input, 'aff_file', mask_resample, 'aff_file')    
+    r.connect(mask_resample, 'res_file', dsx, '@xnat_data')
+
+
+    ds = pe.Node(nio.DataSink(), name='ds')
+    ds.inputs.base_directory = result_dir
+    ds.inputs.parameterization = False
+    
+    r.connect(mask_resample, 'res_file', ds, '@mask')
 
     r.run(plugin='Linear')
 
