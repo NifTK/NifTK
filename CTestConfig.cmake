b3912e8018eb972a466a962adb950f0ed0276e9a
Miklos Espak <m.espak@ucl.ac.uk>
2014-10-24 17:32:52 +0100

    Bug #3953: NiftyMIDAS uses its own CDash dashboard

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -12,7 +12,7 @@
 #
 #============================================================================*/
 
-set(CTEST_PROJECT_NAME "NifTK")
+set(CTEST_PROJECT_NAME "NiftyMIDAS")
 set(CTEST_NIGHTLY_START_TIME "20:00:00 GMT")
 set(CTEST_TEST_TIMEOUT "3600")
 
@@ -22,7 +22,7 @@ endif(NOT DEFINED CTEST_DROP_METHOD)
 
 if(CTEST_DROP_METHOD STREQUAL "http")
   set(CTEST_DROP_SITE "cmicdev.cs.ucl.ac.uk")
-  set(CTEST_DROP_LOCATION "/cdash/submit.php?project=NifTK")
+  set(CTEST_DROP_LOCATION "/cdash/submit.php?project=NiftyMIDAS")
   set(CTEST_DROP_SITE_CDASH TRUE)
 endif(CTEST_DROP_METHOD STREQUAL "http")
 
