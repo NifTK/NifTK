dccfe5799b42ec01ea7bc19e9005267f8b5e6553
Johannes Totz <j.totz@ucl.ac.uk>
2014-11-06 16:47:08 +0000

    Bug #3978: adjust output directory pattern to match what we actually use for most of our data collections, so that directory names sort nicely by date.

--- Code/Gui/MITK/Modules/niftkIGIGuiManager/DataSources/QmitkIGIDataSourceManager.cxx
+++ Code/Gui/MITK/Modules/niftkIGIGuiManager/DataSources/QmitkIGIDataSourceManager.cxx
@@ -1132,7 +1132,7 @@ QString QmitkIGIDataSourceManager::GetDirectoryName()
   QDateTime dateTime;
   dateTime.setMSecsSinceEpoch(millis);
 
-  QString formattedTime = dateTime.toString("yyyy-MM-dd_hh-mm-ss-zzz");
+  QString formattedTime = dateTime.toString("yyyy.MM.dd_hh-mm-ss-zzz");
   QString directoryName = baseDirectory + QDir::separator() + formattedTime;
 
   return directoryName;
