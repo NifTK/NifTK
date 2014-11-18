d246f7acbb3cfe3d4acbd1021b97034275b6067d
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-11-18 11:28:14 +0000

    Feature #4001: fMRI nipype integration: Making the test script up to date

--- Code/pipelines/nipype/interfaces/test/test_restingstatefmri.py
+++ Code/pipelines/nipype/interfaces/test/test_restingstatefmri.py
@@ -1,10 +1,13 @@
-from nipype.interfaces.afni.proc import RestingStatefMRIPreprocess as RS
-rs = RS()
+import restingstatefmri as fmri
+
+rs = fmri.RestingStatefMRIPreprocess()
+
 rs.inputs.in_subjectid = "test"
-rs.inputs.in_fmri = "/Users/me/data/pipeline_test/func.nii.gz"
-rs.inputs.in_anat = "/Users/me/data/pipeline_test/anat.nii.gz"
-rs.inputs.in_group_avg = "/Users/me/data/pipeline_test/avg.nii.gz"
-rs.inputs.in_group_trans = "/Users/me/data/pipeline_test/group_trans.nii.gz"
-rs.inputs.in_seg = "/Users/me/data/pipeline_test/seg.nii.gz"
-rs.inputs.in_atlas = "/Users/me/data/pipeline_test/atlas.nii.gz"
+rs.inputs.in_fmri = "func.nii.gz"
+rs.inputs.in_anat = "anat.nii.gz"
+rs.inputs.in_group_avg = "avg.nii.gz"
+rs.inputs.in_group_trans = "group_trans.nii.gz"
+rs.inputs.in_seg = "seg.nii.gz"
+rs.inputs.in_atlas = "atlas.nii.gz"
+
 rs.run()
