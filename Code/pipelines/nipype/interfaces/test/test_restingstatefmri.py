0f82be50d7eddc038af21cd9cb34186b28416e97
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-11-18 11:59:42 +0000

    Feature #4001: fMRI nipype integration: test script: The inputs need to be given with absolute paths

--- Code/pipelines/nipype/interfaces/test/test_restingstatefmri.py
+++ Code/pipelines/nipype/interfaces/test/test_restingstatefmri.py
@@ -1,13 +1,16 @@
 import restingstatefmri as fmri
+import os
 
 rs = fmri.RestingStatefMRIPreprocess()
 
+data_path = '/Users/nicolastoussaint/data/nipype/fmri'
+
 rs.inputs.in_subjectid = "test"
-rs.inputs.in_fmri = "func.nii.gz"
-rs.inputs.in_anat = "anat.nii.gz"
-rs.inputs.in_group_avg = "avg.nii.gz"
-rs.inputs.in_group_trans = "group_trans.nii.gz"
-rs.inputs.in_seg = "seg.nii.gz"
-rs.inputs.in_atlas = "atlas.nii.gz"
+rs.inputs.in_fmri = os.path.join(data_path, "func.nii.gz")
+rs.inputs.in_anat = os.path.join(data_path, "anat.nii.gz")
+rs.inputs.in_group_avg = os.path.join(data_path, "avg.nii.gz")
+rs.inputs.in_group_trans = os.path.join(data_path, "group_trans.nii.gz")
+rs.inputs.in_seg = os.path.join(data_path, "seg.nii.gz")
+rs.inputs.in_atlas = os.path.join(data_path, "atlas.nii.gz")
 
 rs.run()
