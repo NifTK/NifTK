0d8ed339278ba31be13dd5ad9dd0030cde793f58
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-11-18 13:54:15 +0000

    Feature #4001: fMRI nipype integration: the test_fmri script is now free from any subject ID information and any group information

--- Code/pipelines/nipype/interfaces/test/test_restingstatefmri.py
+++ Code/pipelines/nipype/interfaces/test/test_restingstatefmri.py
@@ -5,12 +5,9 @@ rs = fmri.RestingStatefMRIPreprocess()
 
 data_path = '/Users/nicolastoussaint/data/nipype/fmri'
 
-rs.inputs.in_subjectid = "test"
+rs.inputs.in_t1 = os.path.join(data_path, "anat.nii.gz")
 rs.inputs.in_fmri = os.path.join(data_path, "func.nii.gz")
-rs.inputs.in_anat = os.path.join(data_path, "anat.nii.gz")
-rs.inputs.in_group_avg = os.path.join(data_path, "avg.nii.gz")
-rs.inputs.in_group_trans = os.path.join(data_path, "group_trans.nii.gz")
-rs.inputs.in_seg = os.path.join(data_path, "seg.nii.gz")
-rs.inputs.in_atlas = os.path.join(data_path, "atlas.nii.gz")
+rs.inputs.in_tissue_segmentation = os.path.join(data_path, "seg.nii.gz")
+rs.inputs.in_parcellation = os.path.join(data_path, "atlas.nii.gz")
 
 rs.run()
