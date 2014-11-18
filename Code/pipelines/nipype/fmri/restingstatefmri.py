6df381fb160f8b4dd29c378f3ce2ad5b649b1e78
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-11-18 14:59:52 +0000

    Feature #4001: fMRI nipype integration: typo in the resting state api fixed

--- Code/pipelines/nipype/fmri/restingstatefmri.py
+++ Code/pipelines/nipype/fmri/restingstatefmri.py
@@ -70,7 +70,7 @@ class RestingStatefMRIPreprocess(RestingStatefMRIPreprocessCommand):
     output_spec = RestingStatefMRIPreprocessOutputSpec
     
     def _list_outputs(self):
-        current_dir = os.get_cwd()
+        current_dir = os.getcwd()
         outputs = self.output_spec().get()
         outputs['out_corrected_fmri'] = os.path.abspath('fmri_pp.nii.gz')
         outputs['out_fmri_to_t1_transformation'] = os.path.abspath('fmri_to_t1_transformation.txt')
