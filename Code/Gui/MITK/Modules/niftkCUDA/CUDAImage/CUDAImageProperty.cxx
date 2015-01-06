4914f8fcc737b2f67564e7abbc81b472995e6d0f
Johannes Totz <j.totz@ucl.ac.uk>
2015-01-06 16:43:06 +0000

    Bug #4051: add a property that can hold a cuda-image, so we can attach it to other data items

---
+++ Code/Gui/MITK/Modules/niftkCUDA/CUDAImage/CUDAImageProperty.cxx
@@ -0,0 +1,52 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "CUDAImageProperty.h"
+
+
+//-----------------------------------------------------------------------------
+bool CUDAImageProperty::IsEqual(const mitk::BaseProperty& property) const
+{
+  const CUDAImageProperty*  other = dynamic_cast<const CUDAImageProperty*>(&property);
+  if (other == 0)
+    return false;
+
+  return this->Get().GetId() == other->Get().GetId();
+}
+
+
+//-----------------------------------------------------------------------------
+bool CUDAImageProperty::Assign(const mitk::BaseProperty& property)
+{
+  const CUDAImageProperty*  other = dynamic_cast<const CUDAImageProperty*>(&property);
+  if (other == 0)
+    return false;
+
+  Set(other->Get());
+  return true;
+}
+
+
+//-----------------------------------------------------------------------------
+LightweightCUDAImage CUDAImageProperty::Get() const
+{
+  return m_LWCI;
+}
+
+
+//-----------------------------------------------------------------------------
+void CUDAImageProperty::Set(LightweightCUDAImage lwci)
+{
+  m_LWCI = lwci;
+}
