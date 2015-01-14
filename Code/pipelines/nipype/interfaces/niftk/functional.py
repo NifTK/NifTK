6d6eed21c314842c1d95f3d698eae4f14ca8d32f
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2015-01-14 16:49:00 +0000

    Feature #4065: nipype harmonisation: the remaining perform scripts are moved to <pipelines> and the interface directory is clean. The bash and matlab scripts have been moved to <pipelines> too, leaving <niftk> the only python package in interfaces

--- Code/pipelines/nipype/interfaces/niftk/functional.py
+++ Code/pipelines/nipype/interfaces/niftk/functional.py
@@ -1,14 +1,230 @@
-#! /usr/bin/env python
+"""
+   Interface for niftk filter tools
+"""
 
+import os
+import numpy as np
+import nibabel as nib
+
+from niftk.base import NIFTKCommand, NIFTKCommandInputSpec, getNiftkPath
+from nipype.interfaces.base import (TraitedSpec, File, traits)
 import nipype.interfaces.utility        as niu          # utility
 import nipype.pipeline.engine           as pe           # pypeline engine
 import nipype.interfaces.niftyseg       as niftyseg     # NiftySeg
 import nipype.interfaces.niftyreg       as niftyreg     # NiftyReg
-from extract_roi_statistics             import ExtractRoiStatistics
-from normalise_roi_average_values       import NormaliseRoiAverageValues
-from write_array_to_csv                 import WriteArrayToCsv
 
-import os
+from .stats import ExtractRoiStatistics
+from .utils import WriteArrayToCsv
+
+
+class ExtractSideInputSpec(NIFTKCommandInputSpec):
+    
+    in_file = File(argstr="%s", exists=True, mandatory=True, \
+                desc="Input target image filename")
+                
+    in_dict = traits.Dict(argstr="%s", mandatory=True, \
+                desc='Dictionary containing the axis orientation information. Output of the GetAxisOrientation interface')
+                
+    in_side = traits.Enum('left', 'right', argstr='%s', mandatory=True, \
+                          desc='side to extract')
+
+class ExtractSideOutputSpec(TraitedSpec):
+
+    out_file = traits.File(desc='Image that contains the requested half.')
+
+class ExtractSide(NIFTKCommand):
+
+    input_spec = ExtractSideInputSpec  
+    output_spec = ExtractSideOutputSpec
+    
+    def _gen_output_filename(self, in_file):
+        _, base, _ = split_filename(in_file)
+        outfile = base + '_halved.nii.gz'
+        return os.path.abspath(outfile)
+        
+    def _run_interface(self, runtime):
+        input_file = self.inputs.in_file
+        input_dict = self.inputs.in_dict
+        input_side = self.inputs.in_side
+        self.out_file=self.extract_side(input_file, input_dict, input_side)
+        return runtime
+    
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['out_file'] = self.out_file
+        return outputs
+        
+    def extract_side(self,
+                     image_file,
+                     dictionary,
+                     side_info):
+        # Get the input image dimension
+        ori_image = nib.load(image_file)
+        ori_data = ori_image.get_data()
+        ori_shape = np.array(ori_data.shape)
+        
+        # Check which axis has to be split
+        axis_to_split=int()
+        for axis in ['i', 'j', 'k']:
+            if 'Left-to-Right' in dictionary[axis] or 'Right-to-Left' in dictionary[axis]:
+                if axis=='i':
+                    axis_to_split=0
+                elif axis=='j':
+                    axis_to_split=1
+                elif axis=='k':
+                    axis_to_split=2
+        
+        # Define the new shape and data
+        new_shape=np.array(ori_shape)
+        new_shape[axis_to_split]=round(new_shape[axis_to_split]*0.6)
+        new_data=np.zeros(new_shape)
+        if dictionary['i']=='Left-to-Right' or dictionary['j']=='Left-to-Right' or dictionary['k']=='Left-to-Right':
+            if side_info == 'left':
+                new_data=ori_data[0:new_shape[0],0:new_shape[1],0:new_shape[2]]
+            elif side_info == 'right':
+                new_data=ori_data[ori_shape[0]-new_shape[0]:-1,ori_shape[1]-new_shape[1]:-1,ori_shape[2]-new_shape[2]:-1]
+        else: # Right-to-Left
+            if side_info == 'left':
+                new_data=ori_data[ori_shape[0]-new_shape[0]:-1,ori_shape[1]-new_shape[1]:-1,ori_shape[2]-new_shape[2]:-1]
+            elif side_info == 'right':
+                new_data=ori_data[0:new_shape[0],0:new_shape[1],0:new_shape[2]]
+        # Create the new image
+        out_file = self._gen_output_filename(image_file)
+        new_image = nib.Nifti1Image(new_data, ori_image.get_affine())
+        nib.save(new_image, out_file)
+                     
+        return out_file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class NormaliseRoiAverageValuesInputSpec(NIFTKCommandInputSpec):
+    
+    in_file = File(argstr="%s", exists=True, mandatory=True,
+                   desc="Input image to extract the uptake values")
+    in_array = traits.Array(argstr="%s", exists=True, mandatory=True,
+                            desc="Array containing the uptake statistics. "+ \
+                            "Array order=[Label, mean, std, vol]")
+    roi = traits.String(argstr="%s", exists=True, mandatory=True,
+                        desc="Name of the roi needed for normalisation")
+    cereb_array = traits.Array(argstr="%s", exists=True, mandatory=False,
+                               desc="Array containing the grey matter cerebellum "+ \
+                               "statistics. Array order=[Label, mean, std, vol]")
+
+    
+class NormaliseRoiAverageValuesOutputSpec(TraitedSpec):
+    out_csv_file = File(desc="Output array organised as follow: "+ \
+        "label index, mean value, std value, roi volume in mm")
+    out_file = File(desc="Output array organised as follow: "+ \
+        "label index, mean value, std value, roi volume in mm")
+
+
+class NormaliseRoiAverageValues(NIFTKCommand):
+    
+    input_spec = NormaliseRoiAverageValuesInputSpec  
+    output_spec = NormaliseRoiAverageValuesOutputSpec
+    
+    roi1_list=[24,31,76,77,101,102,103,104,105,106,107,108,\
+            113,114,119,120,121,122,125,126,133,134,139,140,141,142,143,144,\
+            147,148,153,154,155,156,163,164,165,166,167,168,169,170,173,174,\
+            175,176,179,180,181,182,185,186,187,188,191,192,195,196,199,200,\
+            201,202,203,204,205,206,207,208]
+    roi2_list=[24,31,32,33,48,49,76,77,101,102,103,104,105,106,107,108,113,114,\
+            117,118,119,120,121,122,123,124,125,126,133,134,139,140,141,142,\
+            143,144,147,148,153,154,155,156,163,164,165,166,167,168,169,170,\
+            171,172,173,174,175,176,179,180,181,182,185,186,187,188,191,192,\
+            195,196,199,200,201,202,203,204,205,206,207,208]
+    cereb_list=[39,40,41,42,72,73,74]
+
+    def _run_interface(self, runtime):
+        in_file = self.inputs.in_file
+        in_array = self.inputs.in_array
+        roi = self.inputs.roi
+        cereb_array = self.inputs.cereb_array
+        self.normalise_uptake_values(in_file, in_array, roi, cereb_array)
+        return runtime
+    
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['out_csv_file'] = self.suvr_file
+        outputs['out_file'] = self.norm_file
+        return outputs
+    
+    def normalise_uptake_values(self,
+                                in_file,
+                                in_array,
+                                norm_roi,
+                                cereb_array=None):
+        _, base, _ = split_filename(in_file)
+        self.norm_file=os.path.abspath('norm_'+norm_roi+'_'+base+'.nii.gz')
+        self.suvr_file=os.path.abspath('suvr_'+norm_roi+'_'+base+'.csv')
+        
+        # Create csv file to save the data
+        out=open(self.suvr_file,'w')
+        out.write('Input Functional image,'+str(in_file)+'\n')
+        # Extract the normalisation value
+        normalisation_value=0.0
+        if norm_roi=='pons':
+            i=np.where(in_array[:,0]==35)[0]
+            normalisation_value=in_array[i,1]
+        elif norm_roi=='cereb':
+            total_volume=0.0
+            for label in self.cereb_list:
+                i=np.where(in_array[:,0]==label)[0]
+                normalisation_value=normalisation_value+in_array[i,1]*in_array[i,3]
+                total_volume=total_volume+in_array[i,3]
+            normalisation_value=normalisation_value/total_volume
+        elif norm_roi=='gm_cereb' and not cereb_array==None:
+            normalisation_value=cereb_array[1,1]
+        elif norm_roi=='none':
+            normalisation_value=1.0
+        # Write down the normalisation value
+        out.write('Normalisation,'+str(norm_roi)+',value,'+str(normalisation_value)+'\n')
+        out.write('Label index,Initial mean, normalised mean, volume\n')
+        # Normalise the input image
+        norm_file=niftyseg.BinaryMaths()
+        norm_file.inputs.in_file=in_file
+        norm_file.inputs.operand_value=np.float(normalisation_value)
+        norm_file.inputs.operation='div'   
+        norm_file.inputs.out_file=self.norm_file
+        norm_file.run()       
+        # Normalise all the SUVR
+        norm_array=in_array[:,1]/np.float(normalisation_value)
+        for i in range(0,len(norm_array)):
+            out.write(str(in_array[i,0])+','+str(in_array[i,1])+','+ \
+            str(norm_array[i])+','+str(in_array[i,3])+'\n')
+        # Extract the large ROI uptake values
+        roi1_uptake=0
+        roi1_volume=0
+        for label in self.roi1_list:
+            i=np.where(in_array[:,0]==label)[0]
+            roi1_uptake=roi1_uptake+norm_array[i]*in_array[i,3]
+            roi1_volume=roi1_volume+in_array[i,3]
+        roi1_uptake=np.float(roi1_uptake)/np.float(roi1_volume)
+        out.write('region1,,'+str(roi1_uptake)+','+str(roi1_volume)+'\n')
+        roi2_uptake=0
+        roi2_volume=0
+        for label in self.roi2_list:
+            i=np.where(in_array[:,0]==label)[0]
+            roi2_uptake=roi2_uptake+norm_array[i]*in_array[i,3]
+            roi2_volume=roi2_volume+in_array[i,3]
+        roi2_uptake=np.float(roi2_uptake)/np.float(roi2_volume)
+        out.write('region2,,'+str(roi2_uptake)+','+str(roi2_volume)+'\n')
+        out.close()
+
+
+
+
 
 def create_mask_from_functional():
     workflow = pe.Workflow(name='mask_func')
--- Code/pipelines/nipype/interfaces/niftk/stats.py
+++ Code/pipelines/nipype/interfaces/niftk/stats.py
@@ -5,6 +5,10 @@
 from niftk.base import NIFTKCommand, NIFTKCommandInputSpec, getNiftkPath
 from nipype.interfaces.base import (TraitedSpec, File, traits)
 
+import os
+import numpy as np
+import nibabel as nib
+
 
 
 class ComputeDiceScoreInputSpec(NIFTKCommandInputSpec):
@@ -134,3 +138,102 @@ class CalculateAffineDistances(NIFTKCommand):
             
         return distances
         
+
+
+
+
+class ExtractRoiStatisticsInputSpec(NIFTKCommandInputSpec):
+    
+    in_file = File(argstr="%s", exists=True, mandatory=True,
+                        desc="Input image to extract the statistics from")
+    roi_file = File(argstr="%s", exists=True, mandatory=True,
+                        desc="Input image that contains the different roi")
+    in_label = traits.List(traits.BaseInt,
+                           desc = "Label(s) to extract")
+
+class ExtractRoiStatisticsOutputSpec(TraitedSpec):
+    out_array   = traits.Array(desc="Output array organised as follow: "+ \
+        "label index, mean value, std value, roi volume in mm")
+
+
+class ExtractRoiStatistics(NIFTKCommand):
+
+    """
+
+    Examples
+    --------
+
+    """
+
+    input_spec = ExtractRoiStatisticsInputSpec  
+    output_spec = ExtractRoiStatisticsOutputSpec
+
+    def _run_interface(self, runtime):
+        in_file = self.inputs.in_file
+        roi_file = self.inputs.roi_file
+        labels = self.inputs.in_label
+        self.stats=self.extract_roi_statistics(in_file, roi_file, labels)
+        return runtime
+    
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['out_array'] = self.stats
+        return outputs
+        
+
+    def extract_roi_statistics(self,
+                               in_file,
+                               roi_file,
+                               labels):
+        # Read the input images
+        in_img=nib.load(in_file)
+        img_data=in_img.get_data()
+        roi_data=nib.load(roi_file).get_data()
+        # Get the voxel dimention
+        vox_dim=np.product(in_img.get_header().get_zooms())
+        
+        im_shape = img_data.shape
+        roi_shape = roi_data.shape
+                
+        self.number_of_ims = 1
+        self.number_of_roi_ims = 1
+        if len(im_shape) > 3:
+            self.number_of_ims = im_shape[3]
+        if len(roi_shape) > 3:
+            self.number_of_roi_ims = roi_shape[3]
+        
+        # Create an array to store mean uptakes and volumes
+        unique_values = 0
+        unique_values_number = 0
+
+        if len(labels) > 0:
+            unique_values = labels
+        else:
+            unique_values = np.unique(roi_data)
+
+        unique_values_number = len(unique_values)
+
+        stats_array= np.zeros((unique_values_number*self.number_of_ims,4))
+        
+        for im_index in range(0, self.number_of_ims):
+            if self.number_of_ims > 1:
+                self.image = img_data[:,:,:,im_index]
+            else:
+                self.image = img_data[:,:,:]
+            
+            if self.number_of_roi_ims > 1:
+                self.roi = roi_data[:,:,:,im_index]
+            else:
+                self.roi = roi_data[:,:,:]
+            
+            index=0
+            for i in unique_values:
+                mask = (self.roi==i)
+                values = self.image[mask]
+                stats_array[index+im_index*unique_values_number,0]=i
+                stats_array[index+im_index*unique_values_number,1]=np.mean(values)
+                stats_array[index+im_index*unique_values_number,2]=np.std(values)
+                stats_array[index+im_index*unique_values_number,3]=np.multiply(np.sum(mask),vox_dim)
+                index=index+1
+        return stats_array
+
