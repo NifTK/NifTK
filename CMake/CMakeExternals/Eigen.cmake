6f1095973b62177b3d44274b0b84df9459aec602
Miklos Espak <m.espak@ucl.ac.uk>
2015-01-17 17:13:19 +0000

    Issue #4072: Eigen built under EP_BASE
    Conflicts:
    	Config/NifTKConfigure.h.in

--- CMake/CMakeExternals/Eigen.cmake
+++ CMake/CMakeExternals/Eigen.cmake
@@ -23,6 +23,11 @@ if(DEFINED Eigen_DIR AND NOT EXISTS ${Eigen_DIR})
 endif()
 
 set(proj Eigen)
+set(proj_VERSION ${NIFTK_VERSION_EIGEN})
+set(proj_SOURCE ${EP_BASE}/${proj}-${proj_VERSION}-src)
+set(proj_CONFIG ${EP_BASE}/${proj}-${proj_VERSION}-cmake)
+set(proj_BUILD ${EP_BASE}/${proj}-${proj_VERSION}-build)
+set(proj_INSTALL ${EP_BASE}/${proj}-${proj_VERSION}-install)
 set(proj_DEPENDENCIES )
 set(Eigen_DEPENDS ${proj})
 
@@ -31,10 +36,10 @@ if(NOT DEFINED Eigen_DIR)
   niftkMacroGetChecksum(NIFTK_CHECKSUM_EIGEN ${NIFTK_LOCATION_EIGEN})
 
   ExternalProject_Add(${proj}
-    SOURCE_DIR ${proj}-src
-    BINARY_DIR ${proj}-build
-    PREFIX ${proj}-cmake
-    INSTALL_DIR ${proj}-install
+    SOURCE_DIR ${proj_SOURCE}
+    PREFIX ${proj_CONFIG}
+    BINARY_DIR ${proj_BUILD}
+    INSTALL_DIR ${proj_INSTALL}
     URL ${NIFTK_LOCATION_EIGEN}
     URL_MD5 ${NIFTK_CHECKSUM_EIGEN}
     #CONFIGURE_COMMAND ""
@@ -43,15 +48,15 @@ if(NOT DEFINED Eigen_DIR)
     INSTALL_COMMAND ""
     CMAKE_ARGS
       ${EP_COMMON_ARGS}
-      -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/${proj}-install
+      -DCMAKE_INSTALL_PREFIX:PATH=${proj_INSTALL}
       -DBUILD_TESTING:BOOL=OFF
       -DBUILD_EXAMPLES:BOOL=OFF
       -DEIGEN_LEAVE_TEST_IN_ALL_TARGET=ON
       -DBUILD_SHARED_LIBS:BOOL=${EP_BUILD_SHARED_LIBS}
     DEPENDS ${proj_DEPENDENCIES}
-    )
+  )
 
-  set(Eigen_DIR ${CMAKE_BINARY_DIR}/${proj}-src)
+  set(Eigen_DIR ${proj_SOURCE})
   set(Eigen_ROOT ${Eigen_DIR})
   set(Eigen_INCLUDE_DIR ${Eigen_DIR})
     
