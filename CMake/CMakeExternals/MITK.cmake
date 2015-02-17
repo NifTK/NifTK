8e07bf34c6ba4a57f0b29695c59f98d0bdc7db37
Johannes Totz <j.totz@ucl.ac.uk>
2015-02-17 12:15:11 +0000

    Bug #4158: pick up new mitk version that exports LocalFile

--- CMake/CMakeExternals/MITK.cmake
+++ CMake/CMakeExternals/MITK.cmake
@@ -22,7 +22,7 @@ if(DEFINED MITK_DIR AND NOT EXISTS ${MITK_DIR})
   message(FATAL_ERROR "MITK_DIR variable is defined but corresponds to non-existing directory \"${MITK_DIR}\".")
 endif()
 
-set(version "619aecd45a")
+set(version "97c899f2a7")
 set(location "${NIFTK_EP_TARBALL_LOCATION}/NifTK-MITK-${version}.tar.gz")
 
 niftkMacroDefineExternalProjectVariables(MITK ${version} ${location})
