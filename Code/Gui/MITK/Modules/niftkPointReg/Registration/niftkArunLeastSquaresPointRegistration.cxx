def62d68d59df9d18b862464e6e8b13005228b42
Matt Clarkson <m.clarkson@ucl.ac.uk>
2015-04-15 08:09:53 +0100

    Issue #4263: Create niftkPointReg module

---
+++ Code/Gui/MITK/Modules/niftkPointReg/Registration/niftkArunLeastSquaresPointRegistration.cxx
@@ -0,0 +1,88 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "niftkArunLeastSquaresPointRegistration.h"
+#include <mitkOpenCVMaths.h>
+#include <mitkExceptionMacro.h>
+
+namespace niftk {
+
+//-----------------------------------------------------------------------------
+double PointBasedRegistration(const std::vector<cv::Point3d>& fixedPoints,
+                              const std::vector<cv::Point3d>& movingPoints,
+                              cv::Matx44d& outputMatrix
+                             )
+{
+  if (fixedPoints.size() < 3)
+  {
+    mitkThrow() << "The number of 'fixed' points is < 3";
+  }
+
+  if (movingPoints.size() < 3)
+  {
+    mitkThrow() << "The number of 'moving' points is < 3";
+  }
+
+  if (fixedPoints.size() != movingPoints.size())
+  {
+    mitkThrow() << "The number of 'fixed' points is " << fixedPoints.size()
+                << " whereas the number of 'moving' points is " << movingPoints.size()
+                << " and they should correspond.";
+  }
+
+  // Arun Equation 4.
+  cv::Point3d pPrime = mitk::GetCentroid(fixedPoints);
+
+  // Arun Equation 6.
+  cv::Point3d p = mitk::GetCentroid(movingPoints);
+
+  // Arun Equation 7.
+  std::vector<cv::Point3d> q = mitk::SubtractPointFromPoints(movingPoints, p);
+
+  // Arun Equation 8.
+  std::vector<cv::Point3d> qPrime = mitk::SubtractPointFromPoints(fixedPoints, pPrime);
+
+  // Arun Equation 11.
+  cv::Matx33d H = mitk::CalculateCrossCovarianceH(q, qPrime);
+
+  // Delegate to helper method for the rest of the implementation.
+  double fre = std::numeric_limits<double>::max();
+  mitk::DoSVDPointBasedRegistration(fixedPoints, movingPoints, H, p, pPrime, outputMatrix, fre);
+
+  return fre;
+}
+
+
+//-----------------------------------------------------------------------------
+double PointBasedRegistration(const mitk::PointSet::Pointer& fixedPoints,
+                              const mitk::PointSet::Pointer& movingPoints,
+                              vtkMatrix4x4& matrix)
+{
+  std::vector<cv::Point3d> fP = mitk::PointSetToVector(fixedPoints);
+  std::vector<cv::Point3d> mP = mitk::PointSetToVector(movingPoints);
+  cv::Matx44d mat;
+
+  double fre = niftk::PointBasedRegistration(fP, mP, mat);
+  mitk::CopyToVTK4x4Matrix(mat, matrix);
+
+  return fre;
+}
+
+//-----------------------------------------------------------------------------
+} // end namespace
+
+
+
+
+
---
+++ Code/Gui/MITK/Modules/niftkPointReg/Registration/niftkArunLeastSquaresPointRegistration.h
@@ -0,0 +1,57 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef niftkArunLeastSquaresPointRegistration_h
+#define niftkArunLeastSquaresPointRegistration_h
+
+#include "niftkPointRegExports.h"
+
+#include <cv.h>
+#include <mitkPointSet.h>
+#include <vtkMatrix4x4.h>
+
+namespace niftk {
+
+/**
+ * \file niftkArunLeastSquaresPointRegistration.h
+ * \brief Performs SVD based registration of two point sets, as in
+ * <a href="http://ieeexplore.ieee.org/xpl/articleDetails.jsp?arnumber=4767965">Least-Squares Fitting of two, 3-D Point Sets, Arun, 1987, 10.1109/TPAMI.1987.4767965</a>.
+ *
+ * IMPORTANT: Must throw mitk::Exception or subclasses for all errors.
+ */
+
+/**
+ * @brief Does Point Based Registration of two same sized, corresponding point sets.
+ * @param fixedPoints fixed point set.
+ * @param movingPoints moving point set.
+ * @param outputMatrix output 4 x 4 homogeneous rigid body transformation.
+ * @return fiducial registration error
+ */
+extern "C++" NIFTKPOINTREG_EXPORT
+double PointBasedRegistration(const std::vector<cv::Point3d>& fixedPoints,
+                              const std::vector<cv::Point3d>& movingPoints,
+                              cv::Matx44d& outputMatrix);
+
+/**
+ * @brief Overloaded method for MITK and VTK data types.
+ *
+ * Calls the above method. Converts (copies) the point sets.
+ */
+extern "C++" NIFTKPOINTREG_EXPORT
+double PointBasedRegistration(const mitk::PointSet::Pointer& fixedPoints,
+                              const mitk::PointSet::Pointer& movingPoints,
+                              vtkMatrix4x4& matrix);
+} // end namespace
+
+#endif
