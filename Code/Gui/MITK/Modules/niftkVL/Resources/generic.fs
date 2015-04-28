19351bba4560e57ca324808434efe5f3dbed47c1
Johannes Totz <j.totz@ucl.ac.uk>
2015-04-28 16:59:29 +0100

    Bug #4280: make texture and geometry blend correctly

--- Code/Gui/MITK/Modules/niftkVL/Resources/generic.fs
+++ Code/Gui/MITK/Modules/niftkVL/Resources/generic.fs
@@ -11,5 +11,7 @@ void main()
 {
   vec4    texcolor = texture2D(u_TextureMap, gl_TexCoord[0].st);
 
-  gl_FragData[0] = mix(gl_Color, texcolor, texcolor.a);
+  // where alpha in the texture is translucent, the shaded mesh/geom/etc shines through.
+  // but overall translucency is still determined by mesh itself.
+  gl_FragData[0] = vec4(mix(gl_Color.rgb, texcolor.rgb, texcolor.a), gl_Color.a);
 }
