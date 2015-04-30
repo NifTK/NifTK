b156c60b0ec5401f095553c92ee476147d5aba89
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2015-04-30 10:35:46 +0000

    Feature #4285 changed int to char

--- Code/Gui/MITK/Modules/niftkOpenCV/Common/mitkOpenCVImageProcessing.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCV/Common/mitkOpenCVImageProcessing.cxx
@@ -94,7 +94,7 @@ public:
         (point.y >= 0) &&
         (point.y < m_Mask.rows) )
     {
-      maskValue = m_Mask.at<unsigned int> ( point.y, point.x );
+      maskValue = m_Mask.at<unsigned char> ( point.y, point.x );
     }
     if ( maskValue == m_BlankValue )
     {
