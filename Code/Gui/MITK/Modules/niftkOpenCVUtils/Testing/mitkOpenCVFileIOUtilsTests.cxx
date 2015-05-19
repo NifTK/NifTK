7c74e8083c8c0c6f678ba86c982bb05837aaa4d8
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2015-05-19 10:54:24 +0000

    Feature #4276 fixed fileIO unit test

--- Code/Gui/MITK/Modules/niftkOpenCVUtils/Testing/mitkOpenCVFileIOUtilsTests.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCVUtils/Testing/mitkOpenCVFileIOUtilsTests.cxx
@@ -29,17 +29,17 @@
 
 void LoadTimeStampedPointsTest(std::string dir)
 {
-  std::string pointdir = dir + "points";
-  std::string matrixdir = dir + "matrices";
+  std::string pointdir = dir + "pins";
+  std::string matrixdir = dir + "Aurora_2/1";
 
   std::vector < std::pair<unsigned long long, cv::Point3d> > timeStampedPoints = mitk::LoadTimeStampedPoints(pointdir);
   
-  MITK_TEST_CONDITION ( timeStampedPoints.size() == 88 , "Testing 88 points were loaded. " << timeStampedPoints.size() );
-  MITK_TEST_CONDITION ( timeStampedPoints[0].first == 1389084822590698200 , "Testing first time stamp " <<  timeStampedPoints[0].first);
-  MITK_TEST_CONDITION ( timeStampedPoints[87].first == 1389085072647000600 , "Testing last time stamp " <<  timeStampedPoints[87].first);
+  MITK_TEST_CONDITION ( timeStampedPoints.size() == 102 , "Testing 102 points were loaded. " << timeStampedPoints.size() );
+  MITK_TEST_CONDITION ( timeStampedPoints[0].first == 1429793163701888600 , "Testing first time stamp " <<  timeStampedPoints[0].first);
+  MITK_TEST_CONDITION ( timeStampedPoints[101].first == 1429793858654637600 , "Testing last time stamp " <<  timeStampedPoints[101].first);
   
-  MITK_TEST_CONDITION ( mitk::NearlyEqual (timeStampedPoints[0].second,cv::Point3d (315.0,397.0,0.0),1e-6), "Testing first point value " <<  timeStampedPoints[0].second);
-  MITK_TEST_CONDITION ( mitk::NearlyEqual (timeStampedPoints[87].second, cv::Point3d (338.0, 359.0, 0.0),1e-6), "Testing last time stamp " <<  timeStampedPoints[87].second);
+  MITK_TEST_CONDITION ( mitk::NearlyEqual (timeStampedPoints[0].second,cv::Point3d (317,191.0,0.0),1e-6), "Testing first point value " <<  timeStampedPoints[0].second);
+  MITK_TEST_CONDITION ( mitk::NearlyEqual (timeStampedPoints[101].second, cv::Point3d (345.0, 162.0, 0.0),1e-6), "Testing last time stamp " <<  timeStampedPoints[87].second);
 
 }
 
