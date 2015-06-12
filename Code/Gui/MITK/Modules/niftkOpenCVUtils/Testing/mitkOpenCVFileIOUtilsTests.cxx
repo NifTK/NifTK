894d7159b5c5db35503ce2b78bd7528ca5d9241c
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2015-06-12 14:15:45 +0000

    Feature #4305 unit test OK

--- Code/Gui/MITK/Modules/niftkOpenCVUtils/Testing/mitkOpenCVFileIOUtilsTests.cxx
+++ Code/Gui/MITK/Modules/niftkOpenCVUtils/Testing/mitkOpenCVFileIOUtilsTests.cxx
@@ -57,6 +57,16 @@ void TestLoadPickedObject ( char * filename )
   {
     MITK_ERROR << "Failed to open " << filename;
   }
+  MITK_TEST_CONDITION ( p1.size() == 13 , "Testing that 13 picked objects were read : " << p1.size());
+  MITK_TEST_CONDITION ( p1[3].m_TimeStamp == 1421406496123439600, "Testing point 4 time stamp is 1421406496123439600 : " << p1[4].m_TimeStamp );
+  MITK_TEST_CONDITION ( p1[1].m_Id == 2, "Testing point 1 ID is 2 : " << p1[2].m_Id );
+  MITK_TEST_CONDITION ( p1[8].m_FrameNumber == 0, "Testing point 8 frame number  is 0 : " << p1[9].m_FrameNumber );
+  MITK_TEST_CONDITION ( p1[9].m_Channel ==  "left", "Testing point 9 channel  is left : " << p1[10].m_Channel );
+  MITK_TEST_CONDITION ( p1[0].m_IsLine , "Testing point 0 is line " << p1[0].m_IsLine );
+  MITK_TEST_CONDITION ( ! p1[4].m_IsLine, "Testing point 4 is not a line " << p1[4].m_IsLine );
+  MITK_TEST_CONDITION ( p1[11].m_Points.size() == 3 , "Testing there are 3 points in object 11" << p1[11].m_Points.size());
+  MITK_TEST_CONDITION ( mitk::NearlyEqual(p1[12].m_Points[1], cv::Point3d ( 262, 98, 0), 1e-8 ) , "Testing value of point 2 in object 13" << p1[12].m_Points[1]);
+
 }
 
 int mitkOpenCVFileIOUtilsTests(int argc, char * argv[])
