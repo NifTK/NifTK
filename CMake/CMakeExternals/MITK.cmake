c31eff2d65e5d035951ed267b47eab3022546ddd
Miklos Espak <m.espak@ucl.ac.uk>
2015-09-24 15:14:32 +0100

    Issue #4338: Fix for opening images as CL arguments
    Opening images from the command line stopped working after the MITK upgrade.
    This has been reported as bug #19289 in MITK. The fix required a change in CTK
    as well. These changes have not been integrated to the CTK and MITK master,
    but they reside in our forks for now.

--- CMake/CMakeExternals/MITK.cmake
+++ CMake/CMakeExternals/MITK.cmake
@@ -22,7 +22,7 @@ if(DEFINED MITK_DIR AND NOT EXISTS ${MITK_DIR})
   message(FATAL_ERROR "MITK_DIR variable is defined but corresponds to non-existing directory \"${MITK_DIR}\".")
 endif()
 
-set(version "2643e84344")
+set(version "f33196377a")
 set(location "${NIFTK_EP_TARBALL_LOCATION}/NifTK-MITK-${version}.tar.gz")
 
 niftkMacroDefineExternalProjectVariables(MITK ${version} ${location})
