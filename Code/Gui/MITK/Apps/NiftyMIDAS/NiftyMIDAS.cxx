9318ee64e8e29479f63a1625a0481fdf3fd64676
Miklos Espak <m.espak@ucl.ac.uk>
2015-09-24 22:54:22 +0100

    Issue #4354: Catch exception if unknown option is specified

--- Code/Gui/MITK/Apps/NiftyMIDAS/NiftyMIDAS.cxx
+++ Code/Gui/MITK/Apps/NiftyMIDAS/NiftyMIDAS.cxx
@@ -14,7 +14,39 @@
 
 #include <mitkBaseApplication.h>
 
+#include <Poco/Util/OptionException.h>
+
+#include <mitkLogMacros.h>
+
 #include <QVariant>
+
+
+class NiftyMIDASApplication : public mitk::BaseApplication
+{
+public:
+
+  NiftyMIDASApplication(int argc, char** argv)
+  : mitk::BaseApplication(argc, argv)
+  {
+    this->setApplicationName("NiftyMIDAS");
+    this->setOrganizationName("CMIC");
+    this->setProperty(mitk::BaseApplication::PROP_APPLICATION, "uk.ac.ucl.cmic.gui.qt.niftymidas");
+  }
+
+  int run() override
+  {
+    try
+    {
+      this->init(this->getArgc(), this->getArgv());
+    }
+    catch (Poco::Util::UnknownOptionException& e)
+    {
+      MITK_WARN << e.displayText();
+    }
+
+    return Poco::Util::Application::run();
+  }
+};
 
 /**
  * \file NiftyMIDAS.cxx
@@ -23,12 +55,7 @@
 
 int main(int argc, char** argv)
 {
-  // Create a QApplication instance first
-  mitk::BaseApplication myApp(argc, argv);
-  myApp.setApplicationName("NiftyMIDAS");
-  myApp.setOrganizationName("CMIC");
+  NiftyMIDASApplication application(argc, argv);
 
-  myApp.setProperty(mitk::BaseApplication::PROP_APPLICATION, "uk.ac.ucl.cmic.gui.qt.niftymidas");
-
-  return myApp.run();
+  return application.run();
 }
