f1951ea95a21a532dd15b35d6eed3e1c47e4af5a
Matt Clarkson <m.clarkson@ucl.ac.uk>
2015-10-16 14:30:07 +0100

    Issue #1368 update MITK to 203529962e

--- CMake/CMakeExternals/MITK.cmake
+++ CMake/CMakeExternals/MITK.cmake
@@ -21,7 +21,7 @@ if(DEFINED MITK_DIR AND NOT EXISTS ${MITK_DIR})
   message(FATAL_ERROR "MITK_DIR variable is defined but corresponds to non-existing directory \"${MITK_DIR}\".")
 endif()
 
-set(version "55992069fb")
+set(version "203529962e")
 set(location "${NIFTK_EP_TARBALL_LOCATION}/NifTK-MITK-${version}.tar.gz")
 
 niftkMacroDefineExternalProjectVariables(MITK ${version} ${location})
