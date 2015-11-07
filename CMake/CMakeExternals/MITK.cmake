e8a587a27665191611747a802aa415200628df0f
Miklos Espak <m.espak@ucl.ac.uk>
2015-11-07 11:15:39 +0000

    Issue #4467: Make the MITK whitelists cumulative
    Another improvement to the previous way is that now any change made in the whitelists
    means a change in MITK.cmake that will trigger reconfiguring MITK and rebuilding the
    required modules or plugins if necessary.

--- CMake/CMakeExternals/MITK.cmake
+++ CMake/CMakeExternals/MITK.cmake
@@ -43,20 +43,170 @@ if(WIN32)
 endif(WIN32)
 
 
-if(NIFTK_Apps/NiftyView)
-  set(_mitk_whitelist "MITK-NiftyView")
-elseif(NIFTK_Apps/NiftyIGI)
-  set(_mitk_whitelist "MITK-NiftyIGI")
-else()
-  set(_mitk_whitelist "MITK-dummy")
-endif()
-
-
 if(NOT DEFINED MITK_DIR)
 
-    ######################################################################
-    # Configure the MITK Superbuild, to decide which plugins we want.
-    ######################################################################
+    # Configure the MITK Superbuild, to decide which modules and plugins we want.
+    #
+    # Listing a module or plugin here does not mean that it will actually be built. That you can
+    # control through CMake flags when configuring MITK. However, if a required module or plugin
+    # is not listed here, it will not be built and it might prevent other modules or plugins from
+    # being built that you would need. (If the required module depends on a module that is not
+    # listed, eventually transitively.) The dependency lists are not complete, only the first
+    # dependency is marked in the comments.
+
+    set(_enabled_modules "")
+    set(_enabled_plugins "")
+
+    if(NIFTK_Apps/NiftyView)
+
+      list(APPEND _enabled_modules
+      # Modules with linking dependency from our code:
+        Core                    # needed by niftkCore
+        SceneSerializationBase  # needed by niftkCoreIO
+        LegacyGL                # needed by PlanarFigure and AlgorithmsExt
+        PlanarFigure            # needed by QtWidgets
+        Overlays                # needed by QtWidgets
+        QtWidgets               # needed by niftkCoreGui
+        DataTypesExt            # needed by AlgorithmsExt
+        AlgorithmsExt           # needed by ImageExtraction
+        ImageExtraction         # needed by ImageStatistics
+        ImageStatistics         # needed by QtWidgetsExt
+        QtWidgetsExt            # needed by niftkCoreGui
+        SceneSerialization      # needed by org.mitk.gui.qt.ext
+        AppUtil                 # needed by org.mitk.gui.qt.ext
+        LegacyIO                # needed by uk.ac.ucl.cmic.xnat
+        LegacyAdaptors          # needed by Segmentation
+        SurfaceInterpolation    # needed by Segmentation
+        GraphAlgorithms         # needed by Segmentation
+        ContourModel            # needed by Segmentation
+        Multilabel              # needed by Segmentation
+        Segmentation            # needed by uk.ac.ucl.cmic.surfaceextractor
+        MapperExt               # needed by org.mitk.gui.qt.basicimageprocessing
+        ImageDenoising          # needed by org.mitk.gui.qt.basicimageprocessing
+        SegmentationUI          # needed by org.mitk.gui.qt.segmentation
+        DicomUI                 # needed by org.mitk.gui.qt.dicom
+        Python                  # needed by org.mitk.gui.qt.python
+      # Auto-load modules. No linking dependency, but they provide IO classes and mappers that we need.
+        IOExt
+      #  IpPicSupportIO
+      #  VtkShaders
+      )
+
+      list(APPEND _enabled_plugins
+      # Plugins with compile-time or run-time dependency from our code:
+        org.blueberry.core.runtime      # needed by org.blueberry.core.commands
+        org.blueberry.core.commands     # needed by org.blueberry.ui.qt
+        org.blueberry.core.expressions  # needed by org.blueberry.ui.qt
+        org.blueberry.ui.qt             # needed by NiftyView
+        org.mitk.core.ext               # needed by org.mitk.gui.qt.ext
+        org.mitk.core.services          # needed by org.mitk.gui.common
+        org.mitk.gui.common             # needed by org.mitk.gui.qt.application
+        org.mitk.gui.qt.application     # needed by org.mitk.gui.qt.datamanager and org.mitk.gui.qt.ext
+        org.mitk.gui.qt.common          # needed by org.mitk.gui.qt.datamanager
+        org.mitk.gui.qt.ext             # needed by uk.ac.ucl.cmic.gui.qt.commonapps
+        org.mitk.gui.qt.datamanager     # needed by NiftyView
+      # Plugins that we do not physically depend one but that we want to use:
+        org.blueberry.ui.qt.help
+        org.blueberry.ui.qt.log
+        org.mitk.gui.qt.common.legacy           # needed by org.mitk.gui.qt.basicimageprocessing
+        org.mitk.gui.qt.basicimageprocessing
+        org.mitk.gui.qt.volumevisualization
+        org.mitk.gui.qt.pointsetinteraction
+        org.mitk.gui.qt.stdmultiwidgeteditor
+        org.mitk.gui.qt.segmentation
+        org.mitk.gui.qt.cmdlinemodules
+        org.mitk.gui.qt.dicom
+        org.mitk.gui.qt.measurementtoolbox
+        org.mitk.gui.qt.moviemaker
+        org.mitk.gui.qt.properties
+        org.mitk.gui.qt.python
+      )
+
+    endif()
+
+    if(NIFTK_Apps/NiftyIGI)
+
+      list(APPEND _enabled_modules
+      # Modules with linking dependency from our code:
+        Core                    # needed by niftkCore
+        SceneSerializationBase  # needed by niftkCoreIO
+        LegacyGL                # needed by PlanarFigure and AlgorithmsExt
+        PlanarFigure            # needed by QtWidgets
+        Overlays                # needed by QtWidgets
+        QtWidgets               # needed by niftkCoreGui
+        DataTypesExt            # needed by AlgorithmsExt
+        AlgorithmsExt           # needed by ImageExtraction
+        ImageExtraction         # needed by ImageStatistics
+        ImageStatistics         # needed by QtWidgetsExt
+        QtWidgetsExt            # needed by niftkCoreGui
+        SceneSerialization      # needed by org.mitk.gui.qt.ext
+        AppUtil                 # needed by org.mitk.gui.qt.ext
+        LegacyIO                # needed by uk.ac.ucl.cmic.xnat
+        LegacyAdaptors          # needed by Segmentation
+        SurfaceInterpolation    # needed by Segmentation
+        GraphAlgorithms         # needed by Segmentation
+        ContourModel            # needed by Segmentation
+        Multilabel              # needed by Segmentation
+        Segmentation            # needed by uk.ac.ucl.cmic.surfaceextractor
+        IGTBase                 # needed by IGT
+        OpenIGTLink             # needed by IGT
+        IGT                     # needed by CameraCalibration
+        OpenCVVideoSupport      # needed by niftkOpenCV
+        CameraCalibration       # needed by niftkOpenCV
+        Persistence             # needed by IGTUI
+        IGTUI                   # needed by niftkIGIGui
+      # Auto-load modules. No linking dependency, but they provide IO classes and mappers that we need.
+        IOExt
+      #  IpPicSupportIO
+      #  VtkShaders
+      )
+
+      list(APPEND _enabled_plugins
+      # Plugins with compile-time or run-time dependency from our code:
+        org.blueberry.core.runtime      # needed by org.blueberry.core.commands
+        org.blueberry.core.commands     # needed by org.blueberry.ui.qt
+        org.blueberry.core.expressions  # needed by org.blueberry.ui.qt
+        org.blueberry.ui.qt             # needed by NiftyIGI
+        org.mitk.core.ext               # needed by org.mitk.gui.qt.ext
+        org.mitk.core.services          # needed by org.mitk.gui.common
+        org.mitk.gui.common             # needed by org.mitk.gui.qt.application
+        org.mitk.gui.qt.application     # needed by org.mitk.gui.qt.datamanager and org.mitk.gui.qt.ext
+        org.mitk.gui.qt.common          # needed by org.mitk.gui.qt.datamanager
+        org.mitk.gui.qt.ext             # needed by NiftyIGI
+        org.mitk.gui.qt.datamanager     # needed by NiftyIGI
+      # Plugins that we do not physically depend one but that we want to use:
+        org.blueberry.ui.qt.help
+        org.blueberry.ui.qt.log
+        org.mitk.gui.qt.properties
+      )
+
+    endif()
+
+    if(MITK_USE_Python)
+
+      list(APPEND _enabled_modules
+        Python                  # needed by org.mitk.gui.qt.python
+      )
+
+      list(APPEND _enabled_plugins
+        org.mitk.gui.qt.python
+      )
+
+    endif()
+
+    list(REMOVE_DUPLICATES _enabled_modules)
+    list(REMOVE_DUPLICATES _enabled_plugins)
+
+    set(_whitelists_dir "${CMAKE_CURRENT_BINARY_DIR}")
+    set(_whitelist_name "MITK-whitelist")
+    file(WRITE "${_whitelists_dir}/${_whitelist_name}.cmake" "
+      set(enabled_modules
+        ${_enabled_modules}
+      )
+      set(enabled_plugins
+        ${_enabled_plugins}
+      )
+    ")
 
     # Note:
     # The DCMTK_DIR variable should not really be set here. This is a workaround because
@@ -151,8 +301,8 @@ if(NOT DEFINED MITK_DIR)
         -DEigen_DIR:PATH=${Eigen_DIR}
         ${mitk_optional_cache_args}
         -DMITK_INITIAL_CACHE_FILE:FILEPATH=${MITK_INITIAL_CACHE_FILE}
-        -DMITK_WHITELIST:STRING=${_mitk_whitelist}\ \(external\)
-        -DMITK_WHITELISTS_EXTERNAL_PATH:STRING=${CMAKE_SOURCE_DIR}/CMake/Whitelists
+        -DMITK_WHITELIST:STRING=${_whitelist_name}\ \(external\)
+        -DMITK_WHITELISTS_EXTERNAL_PATH:STRING=${_whitelists_dir}
       CMAKE_CACHE_ARGS
         ${EP_COMMON_CACHE_ARGS}
       CMAKE_CACHE_DEFAULT_ARGS
