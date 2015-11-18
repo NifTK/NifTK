5afc29d027f0b536556142141e5846dce4ef84f1
Miklos Espak <m.espak@ucl.ac.uk>
2015-11-18 19:56:09 +0000

    Issue #4490: Store DCMTK install root in DCMTK to avoid being overwritten
    See also MITK bug 19442.
    
    http://bugs.mitk.org/show_bug.cgi?id=19442

--- CMake/CMakeExternals/MITK.cmake
+++ CMake/CMakeExternals/MITK.cmake
@@ -21,7 +21,7 @@ if(DEFINED MITK_DIR AND NOT EXISTS ${MITK_DIR})
   message(FATAL_ERROR "MITK_DIR variable is defined but corresponds to non-existing directory \"${MITK_DIR}\".")
 endif()
 
-set(version "7a1865eb1b")
+set(version "4262369f27")
 set(location "${NIFTK_EP_TARBALL_LOCATION}/NifTK-MITK-${version}.tar.gz")
 
 niftkMacroDefineExternalProjectVariables(MITK ${version} ${location})
@@ -257,6 +257,7 @@ if(NOT DEFINED MITK_DIR)
         -DITK_DIR:PATH=${ITK_DIR}                              # FindITK expects ITK_DIR
         -DCTK_DIR:PATH=${CTK_DIR}                              # FindCTK expects CTK_DIR
         -DDCMTK_DIR:PATH=${DCMTK_DIR}                          # FindDCMTK expects DCMTK_DIR
+        -DDCMTK_ROOT:PATH=${DCMTK_ROOT}                        # DCMTK_ROOT is used in MITKConfig.cmake
         -DOpenCV_DIR:PATH=${OpenCV_DIR}
         -DOpenIGTLink_DIR:PATH=${OpenIGTLink_DIR}
         -DEigen_INCLUDE_DIR:PATH=${Eigen_INCLUDE_DIR}
