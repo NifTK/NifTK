c80d6b5d3497d2235aaaf029b7001c446441a7c6
Matt Clarkson <m.clarkson@ucl.ac.uk>
2015-11-27 14:57:25 +0000

    Issue #2586: Added missing DataSourceManagerWidget files.

---
+++ Code/Gui/MITK/Modules/IGIDataSourcesManager/niftkIGIDataSourceManagerWidget.cxx
@@ -0,0 +1,293 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "niftkIGIDataSourceManagerWidget.h"
+
+#include <QMessageBox>
+#include <QTableWidgetItem>
+#include <QVector>
+#include <QDateTime>
+#include <QTextStream>
+#include <QList>
+
+namespace niftk
+{
+
+//-----------------------------------------------------------------------------
+IGIDataSourceManagerWidget::IGIDataSourceManagerWidget(mitk::DataStorage::Pointer dataStorage, QWidget *parent)
+: m_Manager(niftk::IGIDataSourceManager::New(dataStorage))
+{
+  Ui_IGIDataSourceManager::setupUi(parent);
+  QList<QString> namesOfSources = m_Manager->GetAllSources();
+  foreach (QString source, namesOfSources)
+  {
+    m_SourceSelectComboBox->addItem(source);
+  }
+  m_SourceSelectComboBox->setCurrentIndex(0);
+
+  m_PlayPushButton->setIcon(QIcon(":/niftkIGIDataSourcesManagerResources/play.png"));
+  m_RecordPushButton->setIcon(QIcon(":/niftkIGIDataSourcesManagerResources/record.png"));
+  m_StopPushButton->setIcon(QIcon(":/niftkIGIDataSourcesManagerResources/stop.png"));
+
+  m_PlayPushButton->setEnabled(true);
+  m_RecordPushButton->setEnabled(true);
+  m_StopPushButton->setEnabled(false);
+
+  m_Frame->setContentsMargins(0, 0, 0, 0);
+
+  m_DirectoryChooser->setFilters(ctkPathLineEdit::Dirs);
+  m_DirectoryChooser->setOptions(ctkPathLineEdit::ShowDirsOnly);
+
+  m_ToolManagerPlaybackGroupBox->setCollapsed(true);
+  m_ToolManagerConsoleGroupBox->setCollapsed(true);
+  m_ToolManagerConsole->setMaximumHeight(100);
+  m_TableWidget->setMaximumHeight(200);
+  m_TableWidget->setSelectionMode(QAbstractItemView::SingleSelection);
+
+  // the active column has a fixed, minimal size. note that this line relies
+  // on the table having columns already! the ui file has them added.
+  m_TableWidget->horizontalHeader()->setResizeMode(0, QHeaderView::ResizeToContents);
+
+  bool ok = false;
+  ok = QObject::connect(m_AddSourcePushButton, SIGNAL(clicked()), this, SLOT(OnAddSource()) );
+  assert(ok);
+  ok = QObject::connect(m_RemoveSourcePushButton, SIGNAL(clicked()), this, SLOT(OnRemoveSource()) );
+  assert(ok);
+  ok = QObject::connect(m_RecordPushButton, SIGNAL(clicked()), this, SLOT(OnRecordStart()) );
+  assert(ok);
+  ok = QObject::connect(m_StopPushButton, SIGNAL(clicked()), this, SLOT(OnStop()) );
+  assert(ok);
+  ok = QObject::connect(m_PlayPushButton, SIGNAL(clicked()), this, SLOT(OnPlayStart()));
+  assert(ok);
+  ok = QObject::connect(m_TableWidget, SIGNAL(cellDoubleClicked(int, int)), this, SLOT(OnCellDoubleClicked(int, int)) );
+  assert(ok);
+  ok = QObject::connect(m_TableWidget->horizontalHeader(), SIGNAL(sectionClicked(int)), this, SLOT(OnFreezeTableHeaderClicked(int)));
+  assert(ok);
+}
+
+
+//-----------------------------------------------------------------------------
+IGIDataSourceManagerWidget::~IGIDataSourceManagerWidget()
+{
+  bool ok = false;
+  ok = QObject::disconnect(m_AddSourcePushButton, SIGNAL(clicked()), this, SLOT(OnAddSource()) );
+  assert(ok);
+  ok = QObject::disconnect(m_RemoveSourcePushButton, SIGNAL(clicked()), this, SLOT(OnRemoveSource()) );
+  assert(ok);
+  ok = QObject::disconnect(m_RecordPushButton, SIGNAL(clicked()), this, SLOT(OnRecordStart()) );
+  assert(ok);
+  ok = QObject::disconnect(m_StopPushButton, SIGNAL(clicked()), this, SLOT(OnStop()) );
+  assert(ok);
+  ok = QObject::disconnect(m_PlayPushButton, SIGNAL(clicked()), this, SLOT(OnPlayStart()));
+  assert(ok);
+  ok = QObject::disconnect(m_TableWidget, SIGNAL(cellDoubleClicked(int, int)), this, SLOT(OnCellDoubleClicked(int, int)) );
+  assert(ok);
+  ok = QObject::disconnect(m_TableWidget->horizontalHeader(), SIGNAL(sectionClicked(int)), this, SLOT(OnFreezeTableHeaderClicked(int)));
+  assert(ok);
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::SetDirectoryPrefix(const QString& directoryPrefix)
+{
+  m_Manager->SetDirectoryPrefix(directoryPrefix);
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::SetFramesPerSecond(const int& framesPerSecond)
+{
+  m_Manager->SetFramesPerSecond(framesPerSecond);
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::StartRecording()
+{
+  this->OnRecordStart();
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::StopRecording()
+{
+  this->OnStop();
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::OnPlayStart()
+{
+  MITK_INFO << "OnPlayStart";
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::OnRecordStart()
+{
+  if (!m_RecordPushButton->isEnabled())
+  {
+    // shortcut in case we are already recording.
+    return;
+  }
+
+  m_RecordPushButton->setEnabled(false);
+  m_StopPushButton->setEnabled(true);
+  assert(!m_PlayPushButton->isChecked());
+  m_PlayPushButton->setEnabled(false);
+
+  QString directoryName = m_Manager->GetDirectoryName();
+  QDir directory(directoryName);
+
+  m_DirectoryChooser->setCurrentPath(directory.absolutePath());
+  m_Manager->StartRecording(directory.absolutePath());
+
+  // Tell interested parties (e.g. other plugins) that recording has started.
+  // We do this before dumping the descriptor because that might pop up a message box,
+  // which would stall delivering this signal.
+  emit RecordingStarted(directory.absolutePath());
+
+  try
+  {
+    m_Manager->WriteDescriptorFile(directory.absolutePath());
+
+  } catch (mitk::Exception& e)
+  {
+    QMessageBox msgbox;
+    msgbox.setText("Error creating descriptor file.");
+    msgbox.setInformativeText("Cannot create descriptor file due to: " + QString::fromStdString(e.GetDescription()));
+    msgbox.setIcon(QMessageBox::Warning);
+    msgbox.exec();
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::OnStop()
+{
+  if (m_PlayPushButton->isChecked())
+  {
+    // we are playing back, so simulate a user click to stop.
+    m_PlayPushButton->click();
+  }
+  else
+  {
+    m_Manager->StopRecording();
+
+    m_RecordPushButton->setEnabled(true);
+    m_StopPushButton->setEnabled(false);
+    m_PlayPushButton->setEnabled(true);
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::OnAddSource()
+{
+  QString name = m_SourceSelectComboBox->currentText();
+
+  try
+  {
+    QList<QMap<QString, QVariant> > properties;
+
+    bool needsGui = m_Manager->NeedsStartupGui(name);
+    if (needsGui)
+    {
+      // Launch startup GUI.
+      // Populate parameters when GUI is OK'd.
+    }
+
+    m_Manager->AddSource(name, properties);
+
+    bool shouldUpdate = true;
+    QVector<QString> fields;
+    fields.push_back("status");
+    fields.push_back("0");    // rate
+    fields.push_back("0");    // lag
+    fields.push_back("type");
+    fields.push_back("device");
+    fields.push_back("description");
+
+    // Create new row in table.
+    // Previously: You got 1 row per tracker tool.
+    //             So 1 spectra could have 3 rows in total.
+    // Now:        Its strictly 1 row per data source.
+    int newRowNumber = m_TableWidget->rowCount();
+    m_TableWidget->insertRow(newRowNumber);
+    for (unsigned int i = 0; i < fields.size(); i++)
+    {
+      QTableWidgetItem *item = new QTableWidgetItem(fields[i]);
+      item->setTextAlignment(Qt::AlignCenter);
+      item->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+      m_TableWidget->setItem(newRowNumber, i + 1, item);
+    }
+    QTableWidgetItem* freezeItem = new QTableWidgetItem(" ");
+    freezeItem->setTextAlignment(Qt::AlignCenter);
+    freezeItem->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsEnabled);
+    freezeItem->setCheckState(shouldUpdate ? Qt::Checked : Qt::Unchecked);
+    m_TableWidget->setItem(newRowNumber, 0, freezeItem);
+
+  } catch (mitk::Exception& e)
+  {
+    QMessageBox::critical(this, QString("Creating ") + name,
+      QString("ERROR:") + QString(e.GetDescription()),
+      QMessageBox::Ok);
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::OnRemoveSource()
+{
+  if (m_TableWidget->rowCount() == 0)
+  {
+    return;
+  }
+
+  // Get a valid row number, or delete the last item in the table.
+  int rowIndex = m_TableWidget->currentRow();
+  if (rowIndex < 0)
+  {
+    rowIndex = m_TableWidget->rowCount() - 1;
+  }
+
+  bool updateTimerWasOn = m_Manager->IsUpdateTimerOn();
+
+  m_Manager->StopUpdateTimer();
+  m_Manager->RemoveSource(rowIndex);
+
+  m_TableWidget->removeRow(rowIndex);
+  m_TableWidget->update();
+
+  if (m_TableWidget->rowCount() > 0 && updateTimerWasOn)
+  {
+    m_Manager->StartUpdateTimer();
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::OnCellDoubleClicked(int row, int column)
+{
+  MITK_INFO << "OnCellDoubleClicked";
+}
+
+
+//-----------------------------------------------------------------------------
+void IGIDataSourceManagerWidget::OnFreezeTableHeaderClicked(int section)
+{
+  MITK_INFO << "OnFreezeTableHeaderClicked";
+}
+
+} // end namespace
---
+++ Code/Gui/MITK/Modules/IGIDataSourcesManager/niftkIGIDataSourceManagerWidget.h
@@ -0,0 +1,130 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef niftkIGIDataSourceManagerWidget_h
+#define niftkIGIDataSourceManagerWidget_h
+
+#include "niftkIGIDataSourcesManagerExports.h"
+#include "ui_niftkIGIDataSourceManager.h"
+#include "niftkIGIDataSourceManager.h"
+
+#include <mitkDataStorage.h>
+#include <QWidget>
+
+namespace niftk
+{
+
+/**
+ * \class IGIDataSourceManagerWidget
+ * \brief Widget class to manage a list of IGIDataSources (trackers, ultra-sound machines, video etc).
+ *
+ * This class must delegate all functionality to IGIDataSourceManager, and should
+ * only contain Widget related stuff. Conversely, IGIDataSourceManager should
+ * only contain non-Widget related stuff.
+ */
+class NIFTKIGIDATASOURCESMANAGER_EXPORT IGIDataSourceManagerWidget :
+    public QWidget,
+    public Ui_IGIDataSourceManager
+{
+
+  Q_OBJECT
+
+public:
+
+  IGIDataSourceManagerWidget(mitk::DataStorage::Pointer dataStorage, QWidget *parent = 0);
+  virtual ~IGIDataSourceManagerWidget();
+
+  /**
+  * \brief Called from the Plugin when the surgical guidance plugin preferences are modified.
+  */
+  void SetDirectoryPrefix(const QString& directoryPrefix);
+
+  /**
+  * \brief Called from the Plugin when the surgical guidance plugin preferences are modified.
+  */
+  void SetFramesPerSecond(const int& framesPerSecond);
+
+  /**
+  * \brief Called from the Plugin (e.g. event bus events) to start the recording process.
+  */
+  void StartRecording();
+
+  /**
+  * \brief Called from the Plugin (e.g. event bus events) to stop the recording process.
+  */
+  void StopRecording();
+
+signals:
+
+  /**
+  * \brief Emmitted when recording has successfully started.
+  */
+  void RecordingStarted(QString basedirectory);
+
+  /**
+  * \brief Emmitted when the manager has asked each data source to update, and they have all updated.
+  */
+  void UpdateGuiFinishedDataSources(niftk::IGIDataType::IGITimeType);
+
+protected:
+
+  IGIDataSourceManagerWidget(const IGIDataSourceManagerWidget&); // Purposefully not implemented.
+  IGIDataSourceManagerWidget& operator=(const IGIDataSourceManagerWidget&); // Purposefully not implemented.
+
+private slots:
+
+  /**
+   * \brief Callback to start playing back data.
+   */
+  void OnPlayStart();
+
+  /**
+   * \brief Callback to start recording data.
+   */
+  void OnRecordStart();
+
+  /**
+   * \brief Callback to stop recording/playback data.
+   */
+  void OnStop();
+
+  /**
+  * \brief Adds a data source to the GUI table, getting the name from the combo box.
+  */
+  void OnAddSource();
+
+  /**
+  * \brief Removes a data source from the GUI table, and completely destroys it.
+  */
+  void OnRemoveSource();
+
+  /**
+  * \brief Callback to indicate that a cell has been double clicked, to launch that sources' GUI.
+  */
+  void OnCellDoubleClicked(int row, int column);
+
+  /**
+  * \brief Used to freeze data source updates.
+  */
+  void OnFreezeTableHeaderClicked(int section);
+
+private:
+
+  IGIDataSourceManager::Pointer m_Manager;
+
+}; // end class;
+
+} // end namespace
+
+#endif
