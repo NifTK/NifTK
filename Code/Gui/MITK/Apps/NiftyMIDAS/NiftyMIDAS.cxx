7926db5b51ee39d6deef11928e3b913929e601b6
Miklos Espak <m.espak@ucl.ac.uk>
2015-12-04 13:35:32 +0000

    Issue #4515: NiftyMIDAS main app sources updated as NiftyView

--- Code/Gui/MITK/Apps/NiftyMIDAS/NiftyMIDAS.cxx
+++ Code/Gui/MITK/Apps/NiftyMIDAS/NiftyMIDAS.cxx
@@ -14,39 +14,7 @@
 
 #include <mitkBaseApplication.h>
 
-#include <Poco/Util/OptionException.h>
-
-#include <mitkLogMacros.h>
-
 #include <QVariant>
-
-
-class NiftyMIDASApplication : public mitk::BaseApplication
-{
-public:
-
-  NiftyMIDASApplication(int argc, char** argv)
-  : mitk::BaseApplication(argc, argv)
-  {
-    this->setApplicationName("NiftyMIDAS");
-    this->setOrganizationName("CMIC");
-    this->setProperty(mitk::BaseApplication::PROP_APPLICATION, "uk.ac.ucl.cmic.gui.qt.niftymidas");
-  }
-
-  int run() override
-  {
-    try
-    {
-      this->init(this->getArgc(), this->getArgv());
-    }
-    catch (Poco::Util::UnknownOptionException& e)
-    {
-      MITK_WARN << e.displayText();
-    }
-
-    return Poco::Util::Application::run();
-  }
-};
 
 /**
  * \file NiftyMIDAS.cxx
@@ -55,7 +23,12 @@ public:
 
 int main(int argc, char** argv)
 {
-  NiftyMIDASApplication application(argc, argv);
+  // Create a QApplication instance first
+  mitk::BaseApplication myApp(argc, argv);
+  myApp.setApplicationName("NiftyMIDAS");
+  myApp.setOrganizationName("CMIC");
 
-  return application.run();
+  myApp.setProperty(mitk::BaseApplication::PROP_APPLICATION, "uk.ac.ucl.cmic.gui.qt.niftymidas");
+
+  return myApp.run();
 }
