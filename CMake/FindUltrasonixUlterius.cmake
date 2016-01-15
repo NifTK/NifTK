7ab2c531c1a90f90c4e6f9e1b01d500547dd633b
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-01-15 08:26:50 +0000

    Issue #2586: Only build Ultrasonix Data Source on 32 bit windows builds

---
+++ CMake/FindUltrasonixUlterius.cmake
@@ -0,0 +1,48 @@
+#/*================================================================================
+#
+#  NiftyGuide: A suite of utilities for Image Guided Interventions.
+#
+#  Copyright (c) 2013 University College London (UCL).
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#================================================================================*/
+
+set(UltrasonixUlterius_FOUND 0)
+
+if(WIN32)
+
+  if(${CMAKE_GENERATOR} MATCHES "Win64")
+    message("Cannot build niftkUltrasonixDataSourceService in 64 bit mode")
+  else()
+
+    find_path(Ultrasonix_ULTERIUS_INCLUDE_DIR
+      NAMES ulterius.h
+
+      # It's a 32 bit library
+      PATHS "C:/Program Files (x86)/sdk_6.0.4_(00.036.203)/ulterius/inc"
+      )
+
+    find_library(Ultrasonix_ULTERIUS_LIBRARY
+      NAMES ulterius_old
+
+      # It's a 32 bit library
+      PATHS "C:/Program Files (x86)/sdk_6.0.4_(00.036.203)/ulterius/lib"
+    )
+
+    if(Ultrasonix_ULTERIUS_INCLUDE_DIR AND Ultrasonix_ULTERIUS_LIBRARY)
+      set(UltrasonixUlterius_FOUND 1)
+    endif()
+  endif()
+endif(WIN32)
+
