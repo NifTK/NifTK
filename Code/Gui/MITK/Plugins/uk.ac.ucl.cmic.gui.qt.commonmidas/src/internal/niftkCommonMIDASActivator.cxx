e610ab8154cac30189caee987a5299af332e9be6
Miklos Espak <m.espak@ucl.ac.uk>
2016-04-16 10:37:37 +0100

    Issue #4594: CommonMIDAS plugin activator put back
    The activator was removed because its functionality was moved to other plugins,
    but the empty activator is still needed so that the plugin gets registered.

---
+++ Code/Gui/MITK/Plugins/uk.ac.ucl.cmic.gui.qt.commonmidas/src/internal/niftkCommonMIDASActivator.cxx
@@ -0,0 +1,31 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "niftkCommonMIDASActivator.h"
+#include <QtPlugin>
+
+//-----------------------------------------------------------------------------
+void niftk::CommonMIDASActivator::start(ctkPluginContext* context)
+{
+  Q_UNUSED(context)
+}
+
+
+//-----------------------------------------------------------------------------
+void niftk::CommonMIDASActivator::stop(ctkPluginContext* context)
+{
+  Q_UNUSED(context)
+}
+
+Q_EXPORT_PLUGIN2(uk_ac_ucl_cmic_commonmidas, niftk::CommonMIDASActivator)
