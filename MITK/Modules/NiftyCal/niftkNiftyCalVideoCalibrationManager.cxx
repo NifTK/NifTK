034a5cc67b3593effc47e3ee11a2cdaa37d9a852
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-05-07 21:33:28 +0100

    Issue #4659: New MITK Module for NiftyCal integration.

---
+++ MITK/Modules/NiftyCal/niftkNiftyCalVideoCalibrationManager.cxx
@@ -0,0 +1,100 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "niftkNiftyCalVideoCalibrationManager.h"
+#include <mitkExceptionMacro.h>
+
+namespace niftk
+{
+
+//-----------------------------------------------------------------------------
+NiftyCalVideoCalibrationManager::NiftyCalVideoCalibrationManager()
+: m_DataStorage(nullptr)
+, m_LeftImageNode(nullptr)
+, m_RightImageNode(nullptr)
+, m_TrackingTransformNode(nullptr)
+, m_MinimumNumberOfSnapshotsForCalibrating(5)
+{
+}
+
+
+//-----------------------------------------------------------------------------
+NiftyCalVideoCalibrationManager::~NiftyCalVideoCalibrationManager()
+{
+
+}
+
+
+//-----------------------------------------------------------------------------
+void NiftyCalVideoCalibrationManager::SetDataStorage(
+    const mitk::DataStorage::Pointer& storage)
+{
+  if (storage.IsNull())
+  {
+    mitkThrow() << "Null DataStorage passed";
+  }
+  m_DataStorage = storage;
+}
+
+
+//-----------------------------------------------------------------------------
+unsigned int NiftyCalVideoCalibrationManager::GetNumberOfSnapshots() const
+{
+  return 1;
+}
+
+
+//-----------------------------------------------------------------------------
+void NiftyCalVideoCalibrationManager::Restart()
+{
+  MITK_INFO << "Restart";
+}
+
+
+//-----------------------------------------------------------------------------
+bool NiftyCalVideoCalibrationManager::Grab()
+{
+  MITK_INFO << "Grabbing data.";
+  return true;
+}
+
+
+//-----------------------------------------------------------------------------
+double NiftyCalVideoCalibrationManager::Calibrate()
+{
+  MITK_INFO << "Calibrating.";
+  int j=0;
+  for (int i = 0; i < 1000000000; i++)
+  {
+    j++;
+  }
+  MITK_INFO << "Calibrating - DONE";
+  return 0;
+}
+
+
+//-----------------------------------------------------------------------------
+void NiftyCalVideoCalibrationManager::Undo()
+{
+  MITK_INFO << "Removing last snapshot.";
+}
+
+
+//-----------------------------------------------------------------------------
+void NiftyCalVideoCalibrationManager::Save(const std::string dirName)
+{
+  MITK_INFO << "Saving calibration to " << dirName;
+}
+
+} // end namespace
---
+++ MITK/Modules/NiftyCal/niftkNiftyCalVideoCalibrationManager.h
@@ -0,0 +1,80 @@
+/*=============================================================================
+
+  NifTK: A software platform for medical image computing.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef niftkNiftyCalVideoCalibrateManager_h
+#define niftkNiftyCalVideoCalibrateManager_h
+
+#include "niftkNiftyCalExports.h"
+#include <itkObject.h>
+#include <itkObjectFactoryBase.h>
+#include <mitkDataStorage.h>
+#include <mitkDataNode.h>
+
+namespace niftk {
+
+/**
+ * \class NiftyCalVideoCalibrationManager
+ * \brief Manager class to perform video calibration as provided by NiftyCal.
+ *
+ * This one is not an MITK Service as it is stateful. So, it would
+ * be unwise to have a system-wide service, called from multiple threads.
+ */
+class NIFTKNIFTYCAL_EXPORT NiftyCalVideoCalibrationManager : public itk::Object
+{
+public:
+
+  mitkClassMacroItkParent(NiftyCalVideoCalibrationManager, itk::Object);
+  itkNewMacro(NiftyCalVideoCalibrationManager);
+
+  itkSetMacro(LeftImageNode, mitk::DataNode::Pointer);
+  itkGetMacro(LeftImageNode, mitk::DataNode::Pointer);
+
+  itkSetMacro(RightImageNode, mitk::DataNode::Pointer);
+  itkGetMacro(RightImageNode, mitk::DataNode::Pointer);
+
+  itkSetMacro(TrackingTransformNode, mitk::DataNode::Pointer);
+  itkGetMacro(TrackingTransformNode, mitk::DataNode::Pointer);
+
+  itkSetMacro(MinimumNumberOfSnapshotsForCalibrating, unsigned int);
+  itkGetMacro(MinimumNumberOfSnapshotsForCalibrating, unsigned int);
+
+  void SetDataStorage(const mitk::DataStorage::Pointer& storage);
+  void Restart();
+  unsigned int GetNumberOfSnapshots() const;
+  bool Grab();
+  double Calibrate();
+  void Undo();
+  void Save(const std::string dirName);
+
+protected:
+
+  NiftyCalVideoCalibrationManager(); // Purposefully hidden.
+  virtual ~NiftyCalVideoCalibrationManager(); // Purposefully hidden.
+
+  NiftyCalVideoCalibrationManager(const NiftyCalVideoCalibrationManager&); // Purposefully not implemented.
+  NiftyCalVideoCalibrationManager& operator=(const NiftyCalVideoCalibrationManager&); // Purposefully not implemented.
+
+private:
+
+  mitk::DataStorage::Pointer m_DataStorage;
+  mitk::DataNode::Pointer    m_LeftImageNode;
+  mitk::DataNode::Pointer    m_RightImageNode;
+  mitk::DataNode::Pointer    m_TrackingTransformNode;
+  unsigned int               m_MinimumNumberOfSnapshotsForCalibrating;
+
+}; // end class
+
+} // end namespace
+
+#endif
