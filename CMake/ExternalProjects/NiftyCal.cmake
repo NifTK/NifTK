44f8f504562ebe7cd99e6906ed99bb1008b698dc
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-06-24 13:19:27 +0200

    Issue #4659: Update NiftyCal to get stereo reconstruction optimisation.

--- CMake/ExternalProjects/NiftyCal.cmake
+++ CMake/ExternalProjects/NiftyCal.cmake
@@ -24,7 +24,7 @@ endif()
 if(BUILD_IGI)
 
   set(location "https://cmiclab.cs.ucl.ac.uk/CMIC/NiftyCal.git")
-  set(version "27342c4071")
+  set(version "b1dfc341e0")
 
   niftkMacroDefineExternalProjectVariables(NiftyCal ${version} ${location})
   set(proj_DEPENDENCIES OpenCV Eigen AprilTags ITK)
