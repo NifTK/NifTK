f4086ada745fefbf0a5b2c6f28f156b6bce5765c
John Hipwell <j.hipwell@ucl.ac.uk>
2016-07-19 16:58:45 +0100

    Issue #4682: New project ProtoBuf-CMake added to enable CMake build of ProtoBuf

--- CMake/ExternalProjects/Caffe.cmake
+++ CMake/ExternalProjects/Caffe.cmake
@@ -33,7 +33,7 @@ endif()
 
 # NifTK Caffe rc3 clone with NifTK relevant mods
 # https://cmiclab.cs.ucl.ac.uk/CMIC/CaffeNifTK
-set(version "50839f4")
+set(version "d937a15")
 
 set(location "${NIFTK_EP_TARBALL_LOCATION}/caffe-${version}.tar.gz")
 
