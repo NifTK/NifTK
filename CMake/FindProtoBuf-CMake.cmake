ed61761c6214a22904669fcd18ff095a7db4257e
John Hipwell <j.hipwell@ucl.ac.uk>
2016-07-20 16:24:15 +0100

    Issue #4682: CMake related mods

---
+++ CMake/FindProtoBuf-CMake.cmake
@@ -0,0 +1,33 @@
+#/*============================================================================
+#
+#  NifTK: A software platform for medical image computing.
+#
+#  Copyright (c) University College London (UCL). All rights reserved.
+#
+#  This software is distributed WITHOUT ANY WARRANTY; without even
+#  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.
+#
+#  See LICENSE.txt in the top level directory for details.
+#
+#============================================================================*/
+
+set(ProtoBuf-CMake_FOUND)
+
+set(ProtoBuf-CMake_DIR @ProtoBuf-CMake_DIR@ CACHE PATH "Directory containing ProtoBuf-CMake installation")
+
+find_file(ProtoBuf-CMake_CMakeLists
+  NAME CMakeLists.txt
+  PATHS ${ProtoBuf-CMake_SOURCE_DIR}
+  NO_DEFAULT_PATH
+)
+
+message( "FindProtoBuf-CMake.cmake ProtoBuf-CMake_CMakeLists: ${ProtoBuf-CMake_CMakeLists}" )
+
+if((ProtoBuf-CMake_CMakeLists)
+
+  set(ProtoBuf-CMake_FOUND 1)
+
+endif()
+
+
