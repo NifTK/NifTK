d0ff30e270e04f53a697b1eeeb3ba9e1a9aae30a
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2016-07-27 13:42:52 +0000

    Issue #4701 fixed unitialised cv::mat

--- MITK/Modules/OpenCVUtils/Testing/mitkOpenCVPointTypesTest.cxx
+++ MITK/Modules/OpenCVUtils/Testing/mitkOpenCVPointTypesTest.cxx
@@ -78,9 +78,9 @@ void TestPickedObjectMultipy()
   mitk::PickedObject p1;
   cv::Mat* mat = new cv::Mat(4,4,CV_64FC1);
 
-  for ( unsigned int i = 0 ; i < 3 ; ++i )
+  for ( unsigned int i = 0 ; i < 4 ; ++i )
   {
-    for ( unsigned int j = 0 ; j < 3 ; ++j )
+    for ( unsigned int j = 0 ; j < 4 ; ++j )
     {
       if ( i != j )
       {
