35a761295c534e7fff50e706459e94623a0e491d
John Hipwell <j.hipwell@ucl.ac.uk>
2016-08-10 09:27:20 +0100

    Issue #4682: Caffe-NifTK CropParameter added

--- CMake/ExternalProjects/Caffe.cmake
+++ CMake/ExternalProjects/Caffe.cmake
@@ -38,7 +38,7 @@ endif()
 # Microsoft Caffe Windows branch https://github.com/BVLC/caffe/tree/windows
 #set(version "5db3074")
 # Microsoft Caffe Windows branch with CaffeNifTK master commits copied over
-set(version "06a280b")
+set(version "8ee7001")
 
 set(location "${NIFTK_EP_TARBALL_LOCATION}/caffe-${version}.tar.gz")
 
