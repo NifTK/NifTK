e2b168b94afc41fbf8da1c52a59850b0f6ef8d19
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-08-28 17:04:07 +0100

    Issue #4744: Remove dllimport for CreateMitkImageInternal.

--- MITK/Modules/Core/Common/niftkImageConversion.h
+++ MITK/Modules/Core/Common/niftkImageConversion.h
@@ -12,7 +12,6 @@
 
 =============================================================================*/
 
-#include "niftkCoreExports.h"
 #include <mitkImageReadAccessor.h>
 #include <mitkImageCast.h>
 #include <itkImportImageFilter.h>
@@ -27,12 +26,12 @@ typedef itk::RGBAPixel<unsigned char> UCRGBAPixelType;
 //-----------------------------------------------------------------------------
 // Note: Does not do pixel conversions (e.g. BGR to RGB).
 template <typename ITKPixelType>
-NIFTKCORE_EXPORT mitk::Image::Pointer CreateMitkImageInternal(const char* imageData,
-                                                              unsigned int numberOfChannels,
-                                                              unsigned int width,
-                                                              unsigned int widthStep,
-                                                              unsigned int height
-                                                             )
+mitk::Image::Pointer CreateMitkImageInternal(const char* imageData,
+                                             unsigned int numberOfChannels,
+                                             unsigned int width,
+                                             unsigned int widthStep,
+                                             unsigned int height
+                                            )
 {
   typedef itk::Image<ITKPixelType, 3>             ItkImageType;
   typedef itk::ImportImageFilter<ITKPixelType, 3> ImportFilterType;
