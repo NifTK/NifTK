7e285eff43b9df5eb3d3fc3eadfda495d04bc4f4
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-08-31 06:15:07 +0100

    Issue #4744: OpenCV video source now RGB not RGBA.

--- MITK/Modules/OpenCVVideoDataSourceService/Internal/niftkOpenCVVideoDataSourceService.cxx
+++ MITK/Modules/OpenCVVideoDataSourceService/Internal/niftkOpenCVVideoDataSourceService.cxx
@@ -159,15 +159,15 @@ mitk::Image::Pointer OpenCVVideoDataSourceService::ConvertImage(niftk::IGIDataTy
   {
     // OpenCV's cannonical channel layout is bgr (instead of rgb),
     // while everything usually else expects rgb...
-    IplImage* rgbaOpenCVImage = cvCreateImage( cvSize( img->width, img->height ), img->depth, 4);
-    cvCvtColor( img, rgbaOpenCVImage,  CV_BGR2RGBA );
+    IplImage* rgbOpenCVImage = cvCreateImage( cvSize( img->width, img->height ), img->depth, 3);
+    cvCvtColor( img, rgbOpenCVImage,  CV_BGR2RGB );
 
     // ...so when we eventually extend/generalise CreateMitkImage() to handle different formats/etc
     // we should make sure we got the layout right. (opencv itself does not use this in any way.)
-    std::memcpy(&rgbaOpenCVImage->channelSeq[0], "RGBA", 4);
+    std::memcpy(&rgbOpenCVImage->channelSeq[0], "RGB", 3);
 
     // And then we stuff it into the DataNode, where the SmartPointer will delete for us if necessary.
-    mitk::Image::Pointer convertedImage = niftk::CreateMitkImage(rgbaOpenCVImage);
+    mitk::Image::Pointer convertedImage = niftk::CreateMitkImage(rgbOpenCVImage);
 
   #ifdef XXX_USE_CUDA
     // a compatibility stop-gap to interface with new renderer and cuda bits.
@@ -176,15 +176,15 @@ mitk::Image::Pointer OpenCVVideoDataSourceService::ConvertImage(niftk::IGIDataTy
       if (cm != 0)
       {
         cudaStream_t    mystream = cm->GetStream("OpenCVVideoDataSourceService::ConvertImage");
-        WriteAccessor   wa       = cm->RequestOutputImage(rgbaOpenCVImage->width, rgbaOpenCVImage->height, 4);
+        WriteAccessor   wa       = cm->RequestOutputImage(rgbaOpenCVImage->width, rgbaOpenCVImage->height, 3);
 
-        assert(rgbaOpenCVImage->widthStep >= (rgbaOpenCVImage->width * 4));
+        assert(rgbaOpenCVImage->widthStep >= (rgbOpenCVImage->width * 3));
         cudaMemcpy2DAsync(wa.m_DevicePointer,
                           wa.m_BytePitch,
-                          rgbaOpenCVImage->imageData,
-                          rgbaOpenCVImage->widthStep,
-                          rgbaOpenCVImage->width * 4,
-                          rgbaOpenCVImage->height,
+                          rgbOpenCVImage->imageData,
+                          rgbOpenCVImage->widthStep,
+                          rgbOpenCVImage->width * 3,
+                          rgbOpenCVImage->height,
                           cudaMemcpyHostToDevice,
                           mystream);
 
@@ -200,9 +200,9 @@ mitk::Image::Pointer OpenCVVideoDataSourceService::ConvertImage(niftk::IGIDataTy
     }
   #endif
 
-    outputNumberOfBytes = rgbaOpenCVImage->width * rgbaOpenCVImage->height * 4;
+    outputNumberOfBytes = rgbOpenCVImage->width * rgbOpenCVImage->height * 3;
 
-    cvReleaseImage(&rgbaOpenCVImage);
+    cvReleaseImage(&rgbOpenCVImage);
 
     return convertedImage;
   }
