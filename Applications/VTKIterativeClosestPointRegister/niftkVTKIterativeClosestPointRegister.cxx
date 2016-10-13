f8e68ef0f8c4ed5fce0f23cc72caba0501a18bae
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2016-10-13 17:09:37 +0000

    Issue #4789 updated calling functions to reflect new randomtransform interface

--- Applications/VTKIterativeClosestPointRegister/niftkVTKIterativeClosestPointRegister.cxx
+++ Applications/VTKIterativeClosestPointRegister/niftkVTKIterativeClosestPointRegister.cxx
@@ -206,7 +206,8 @@ int main(int argc, char** argv)
   {
     vtkSmartPointer<vtkMinimalStandardRandomSequence> Uni_Rand = vtkSmartPointer<vtkMinimalStandardRandomSequence>::New();
     Uni_Rand->SetSeed(time(NULL));
-    niftk::RandomTransform ( StartTrans, 10.0 , 10.0 , 10.0, 10.0 , 10.0, 10.0, Uni_Rand);
+    double scaleSD = -1.0;
+    StartTrans = niftk::RandomTransform ( 10.0 , 10.0 , 10.0, 10.0 , 10.0, 10.0, Uni_Rand, scaleSD);
     niftk::TranslatePolyData ( source , StartTrans);
   }
   if ( args.perturbTarget )
