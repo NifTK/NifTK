bb4b6cd2f1442d2c563c2f2b09ccfc57d9ea30d2
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2016-10-25 12:23:35 +0000

    Issue #4789 added normal rng and repeat option

--- Applications/VTKRandomTransform/niftkVTKRandomTransform.cxx
+++ Applications/VTKRandomTransform/niftkVTKRandomTransform.cxx
@@ -24,6 +24,7 @@
 
 #include <niftkConversionUtils.h>
 #include <vtkMinimalStandardRandomSequence.h>
+#include <vtkBoxMuellerRandomSequence.h>
 #include <vtkMatrix4x4.h>
 #include <vtkTransform.h>
 
@@ -32,14 +33,39 @@ int main(int argc, char** argv)
   // To parse command line args.
   PARSE_ARGS;
 
+  bool silent = false;
+
   vtkSmartPointer<vtkMatrix4x4> toCentreMat = vtkSmartPointer<vtkMatrix4x4>::New();
   vtkSmartPointer<vtkTransform> toCentre = vtkSmartPointer<vtkTransform>::New();
+  toCentreMat->Identity();
+  if ( modelToCentreTransform.length() != 0 )
+  {
+    toCentreMat = niftk::LoadMatrix4x4FromFile ( modelToCentreTransform, silent );
+  }
+
   toCentre->SetMatrix(toCentreMat);
 
-  vtkSmartPointer<vtkTransform> StartTrans = vtkSmartPointer<vtkTransform>::New();
-    
+  vtkSmartPointer<vtkTransform> randomTransform = vtkSmartPointer<vtkTransform>::New();
+  vtkSmartPointer<vtkMatrix4x4> randomMatrix = vtkSmartPointer<vtkMatrix4x4>::New();
+
   vtkSmartPointer<vtkMinimalStandardRandomSequence> Uni_Rand = vtkSmartPointer<vtkMinimalStandardRandomSequence>::New();
-  Uni_Rand->SetSeed(time(NULL));
-  
-  StartTrans = niftk::RandomTransform ( 10.0 , 10.0 , 10.0, 10.0 , 10.0, 10.0, *Uni_Rand, scaleSD);
+  vtkSmartPointer<vtkBoxMuellerRandomSequence> normal_Rand = vtkSmartPointer<vtkBoxMuellerRandomSequence>::New();
+
+  Uni_Rand->SetSeed(seed);
+
+  unsigned int widthOfNumber = static_cast<unsigned int>(std::floor(std::log10(repeats)) + 1);
+  for ( unsigned int i = 0 ; i < repeats ; ++i )
+  {
+
+    randomTransform = niftk::RandomTransformAboutRemoteCentre ( 10.0 , 10.0 , 10.0, 10.0 , 10.0, 10.0, *normal_Rand,
+        toCentre, scaleSD);
+
+    randomMatrix = randomTransform->GetMatrix();
+
+    std::ostringstream outputNumber;
+    outputNumber << std::setw(widthOfNumber) << std::setfill ('0') << i;
+    std::string output = outputPrefix + outputNumber.str() + ".4x4";
+
+    niftk::SaveMatrix4x4ToFile ( output,*randomMatrix, silent);
+  }
 }
