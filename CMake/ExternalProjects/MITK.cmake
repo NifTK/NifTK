aeabac579dc03d3482fc783ad3a8f3ba3f631f38
Miklos Espak <m.espak@ucl.ac.uk>
2016-11-02 11:44:56 +0000

    Issue #4808: MITK update to revert fixes for sliced geometry initialisation
    I had to give up on correcting the renderers' geometry. With a previous
    MITK patch (see MITK bug T20204) you can access the reference geometry
    of the renderers' geometry, i.e. the geometry that was used to initialise
    the renderers.
    
    The renderers' geometry should not be used for any calculation. You should
    always get their reference geometry and use that one for checking corners,
    converting between index and mm coordinates and so on.

--- CMake/ExternalProjects/MITK.cmake
+++ CMake/ExternalProjects/MITK.cmake
@@ -21,7 +21,7 @@ if(DEFINED MITK_DIR AND NOT EXISTS ${MITK_DIR})
   message(FATAL_ERROR "MITK_DIR variable is defined but corresponds to non-existing directory \"${MITK_DIR}\".")
 endif()
 
-set(version "f8486b5026")
+set(version "76c9404c59")
 set(location "${NIFTK_EP_TARBALL_LOCATION}/NifTK-MITK-${version}.tar.gz")
 
 niftkMacroDefineExternalProjectVariables(MITK ${version} ${location})
