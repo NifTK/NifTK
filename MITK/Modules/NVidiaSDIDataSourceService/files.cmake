551edb0a6fbfeee52d23597018f966623ac8c5b9
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-12-07 07:47:59 +0000

    Issue #4234: Fix CMake for CUDA < 7 [ci skip]

--- MITK/Modules/NVidiaSDIDataSourceService/files.cmake
+++ MITK/Modules/NVidiaSDIDataSourceService/files.cmake
@@ -21,29 +21,18 @@ set(CPP_FILES
   Internal/niftkNVidiaSDIDataType.cxx
 )
 
-if (CUDA_VERSION_MAJOR LESS 7)
-	
-else()
-	LIST(APPEND CPP_FILES Internal/niftkNV7SDIDataSourceImpl.cpp)
-endif()
-
-
 set(MOC_H_FILES
   Internal/niftkNVidiaSDIInitDialog.h
   Internal/niftkNVidiaSDIConfigDialog.h
-  #Internal/niftkNVidiaSDIDataSourceImpl.h
   Internal/niftkNVidiaSDIDataSourceService.h
 )
 
-
-
 if (CUDA_VERSION_MAJOR LESS 7)	
-	LIST (APPEND CPP_FILES Internal/niftkNVidiaSDIDataSourceImpl.cxx)
-	LIST (MOC_H_FILES Internal/niftkNVidiaSDIDataSourceImpl.h)
-
+  list(APPEND CPP_FILES   Internal/niftkNVidiaSDIDataSourceImpl.cxx)
+  list(APPEND MOC_H_FILES Internal/niftkNVidiaSDIDataSourceImpl.h)
 else()
-	LIST(APPEND MOC_H_FILES Internal/niftkNV7SDIDataSourceImpl.h)
-	LIST(APPEND CPP_FILES Internal/niftkNV7SDIDataSourceImpl.cpp)	
+  list(APPEND CPP_FILES   Internal/niftkNV7SDIDataSourceImpl.cxx)
+  list(APPEND MOC_H_FILES Internal/niftkNV7SDIDataSourceImpl.h)
 endif()
 
 set(UI_FILES
