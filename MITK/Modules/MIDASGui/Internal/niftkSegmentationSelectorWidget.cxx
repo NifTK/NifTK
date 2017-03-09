547f842e026229ca09c37bd0b413d9827f11de3a
Miklos Espak <m.espak@ucl.ac.uk>
2017-03-09 15:44:00 +0000

    Issue #4963: Set better tooltips for image selector widgets

--- MITK/Modules/MIDASGui/Internal/niftkSegmentationSelectorWidget.cxx
+++ MITK/Modules/MIDASGui/Internal/niftkSegmentationSelectorWidget.cxx
@@ -82,15 +82,32 @@ void SegmentationSelectorWidget::OnReferenceDataChanged()
   if (hasReferenceData)
   {
     QString referenceImageName = QString::fromStdString(m_ToolManager->GetReferenceData(0)->GetName());
-    m_ReferenceImageNameLabel->setText(QString("<font color='black'>%1</font>").arg(referenceImageName));
+    m_ReferenceImageNameLabel->setText(referenceImageName);
     m_ReferenceImageNameLabel->setToolTip(referenceImageName);
   }
   else
   {
     m_ReferenceImageNameLabel->setText("<font color='red'>&lt;not selected&gt;</font>");
-    m_ReferenceImageNameLabel->setToolTip("&lt;not selected&gt;");
+    m_ReferenceImageNameLabel->setToolTip("Choose a reference image from the Data Manager.");
   }
 
+  if (!hasReferenceData)
+  {
+    m_NewSegmentationButton->setToolTip(
+          "Choose a reference image from the Data Manager.");
+  }
+  else if (!hasWorkingData)
+  {
+    m_NewSegmentationButton->setToolTip(
+          "Hit the 'Start/restart segmentation' button after selecting\n"
+          "a reference image or a segmentation image in the Data Manager.");
+  }
+  else
+  {
+    m_NewSegmentationButton->setToolTip(
+          "Only one segmentation can be edited at a time. You need to finalise (OK)\n"
+          "or discard (Cancel) the current segmentation to start a new one.");
+  }
   m_NewSegmentationButton->setEnabled(hasReferenceData && !hasWorkingData);
 }
 
@@ -106,13 +123,37 @@ void SegmentationSelectorWidget::OnWorkingDataChanged()
     QString segmentationImageName = QString::fromStdString(m_ToolManager->GetWorkingData(0)->GetName());
     m_SegmentationImageNameLabel->setText(QString("<font color='black'>%1</font>").arg(segmentationImageName));
     m_SegmentationImageNameLabel->setToolTip(QString("<font color='black'>%1</font>").arg(segmentationImageName));
+    m_SegmentationImageNameLabel->setToolTip(
+          QString("The segmentation '%1' is being edited. Only one segmentation can be\n"
+                  "edited at a time. You need to finalise (OK) or discard (Cancel) the\n"
+                  "current segmentation to start a new one.").arg(segmentationImageName));
   }
   else
   {
     m_SegmentationImageNameLabel->setText("<font color='red'>&lt;not selected&gt;</font>");
-    m_SegmentationImageNameLabel->setToolTip("&lt;not selected&gt;");
+    m_SegmentationImageNameLabel->setToolTip(
+          "There is no segmentation in progress. Choose a reference image\n"
+          "or a segmentation from the Data Manager and hit the 'Start/restart\n"
+          "segmentation' button.");
   }
 
+  if (!hasReferenceData)
+  {
+    m_NewSegmentationButton->setToolTip(
+          "Choose a reference image from the Data Manager.");
+  }
+  else if (!hasWorkingData)
+  {
+    m_NewSegmentationButton->setToolTip(
+          "Hit the 'Start/restart segmentation' button after selecting\n"
+          "a reference image or a segmentation image in the Data Manager.");
+  }
+  else
+  {
+    m_NewSegmentationButton->setToolTip(
+          "Only one segmentation can be edited at a time. You need to finalise (OK)\n"
+          "or discard (Cancel) the current segmentation to start a new one.");
+  }
   m_NewSegmentationButton->setEnabled(hasReferenceData && !hasWorkingData);
 }
 
