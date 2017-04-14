013b92514bc3d37d5884ba2f0425b7b2dd9e4500
Matt Clarkson <m.clarkson@ucl.ac.uk>
2017-04-14 07:47:24 +0100

    Issue #5032: Pick up new NiftyCal to ensure timing calibration removes first and last 10 percent of data.

--- CMake/ExternalProjects/NiftyCal.cmake
+++ CMake/ExternalProjects/NiftyCal.cmake
@@ -24,7 +24,7 @@ endif()
 if(BUILD_NiftyIGI)
 
   set(location "https://cmiclab.cs.ucl.ac.uk/CMIC/NiftyCal.git")
-  set(version "65efd00e47")
+  set(version "4062bc0075")
 
   niftkMacroDefineExternalProjectVariables(NiftyCal ${version} ${location})
   set(proj_DEPENDENCIES OpenCV Eigen AprilTags ITK)
