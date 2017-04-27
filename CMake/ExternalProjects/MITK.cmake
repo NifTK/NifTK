51fa9a9441962c0f70051dc3a42a2a22de3834bd
Miklos Espak <m.espak@ucl.ac.uk>
2017-04-27 14:54:56 +0100

    Issue #5042: MITK upgrade for undo limit fix
    MITK task: 22774

--- CMake/ExternalProjects/MITK.cmake
+++ CMake/ExternalProjects/MITK.cmake
@@ -21,7 +21,7 @@ if(DEFINED MITK_DIR AND NOT EXISTS ${MITK_DIR})
   message(FATAL_ERROR "MITK_DIR variable is defined but corresponds to non-existing directory \"${MITK_DIR}\".")
 endif()
 
-set(version "510aa55f94")
+set(version "3256cf78ea")
 set(location "${NIFTK_EP_TARBALL_LOCATION}/NifTK-MITK-${version}.tar.gz")
 
 niftkMacroDefineExternalProjectVariables(MITK ${version} ${location})
