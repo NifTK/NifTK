bf3e4d4118f2fef5242bc78f7e7a89149f0ad6fc
Rachel Sparks <rachel.sparks@ucl.ac.uk>
2017-05-08 18:07:07 +0100

    Issue #5038: invert transformation functions now use command line parser.

--- Applications/InvertAffineTransform/niftkInvertAffineTransform.cxx
+++ Applications/InvertAffineTransform/niftkInvertAffineTransform.cxx
@@ -13,6 +13,7 @@
 =============================================================================*/
 
 #include <niftkLogHelper.h>
+#include <niftkCommandLineParser.h>
 #include <niftkConversionUtils.h>
 #include <itkEulerAffineTransform.h>
 
@@ -21,25 +22,24 @@
  * \page niftkInvertAffineTransform
  * \section niftkInvertAffineTransformSummary Inverts an affine transform.
  */
-void Usage(char *exec)
-{
-  niftk::LogHelper::PrintCommandLineHeader(std::cout);
-  std::cout << "  " << std::endl;
-  std::cout << "  Inverts an affine transform" << std::endl;
-  std::cout << "  " << std::endl;
-  std::cout << "  " << exec << " -i inputFileName -o outputFileName [options]" << std::endl;
-  std::cout << "  " << std::endl;
-  std::cout << "*** [mandatory] ***" << std::endl << std::endl;
-  std::cout << "    -i    <filename>        Input matrix, a plain text file, 4 rows, 4 columns." << std::endl;
-  std::cout << "    -o    <filename>        Output matrix, in same format as input." << std::endl << std::endl;      
-  std::cout << "*** [options]   ***" << std::endl << std::endl;   
-}
 
-struct arguments
+struct niftk::CommandLineArgumentDescription clArgList[] = 
 {
-  std::string inputImage;
-  std::string outputImage;  
+  {OPT_STRING|OPT_REQ, "i", "string", " Input matrix, a plain text file, 4 rows, 4 columns."},
+  {OPT_STRING|OPT_REQ, "o", "string", "Output matrix, in same format as input."},
+
+  {OPT_DONE, NULL, NULL, 
+   "Program to invert an affine transformation.\n"
+  }
 };
+
+enum
+{
+  O_INPUT_TRANSFORM, 
+
+  O_OUTPUT_TRANSFORM
+};
+
 
 /**
  * \brief Calculates inverse of an affine transform.
@@ -47,45 +47,24 @@ struct arguments
 int main(int argc, char** argv)
 {
   // To pass around command line args
-  struct arguments args;
-  
+  std::string inputTransform;
+  std::string outputTransform;    
 
-  // Parse command line args
-  for(int i=1; i < argc; i++){
-    if(strcmp(argv[i], "-help")==0 || strcmp(argv[i], "-Help")==0 || strcmp(argv[i], "-HELP")==0 || strcmp(argv[i], "-h")==0 || strcmp(argv[i], "--h")==0){
-      Usage(argv[0]);
-      return -1;
-    }
-    else if(strcmp(argv[i], "-i") == 0){
-      args.inputImage=argv[++i];
-      std::cout << "Set -i=" << args.inputImage << std::endl;
-    }
-    else if(strcmp(argv[i], "-o") == 0){
-      args.outputImage=argv[++i];
-      std::cout << "Set -o=" << args.outputImage << std::endl;
-    }
-    else {
-      std::cerr << argv[0] << ":\tParameter " << argv[i] << " unknown." << std::endl;
-      return -1;
-    }            
-  }
+  // Parse command line acd rgs
+  niftk::CommandLineParser CommandLineOptions(argc, argv, clArgList, false);
 
-  // Validate command line args
-  if (args.inputImage.length() == 0 || args.outputImage.length() == 0)
-    {
-      Usage(argv[0]);
-      return EXIT_FAILURE;
-    }
+  CommandLineOptions.GetArgument( O_INPUT_TRANSFORM, inputTransform );
+
+  CommandLineOptions.GetArgument( O_OUTPUT_TRANSFORM, outputTransform );
 
   // Short and sweet.... well... not as short and sweet as Matlab!
   
   typedef itk::EulerAffineTransform<double, 3, 3> MatrixType;
   
   MatrixType::Pointer inputMatrix = MatrixType::New();
-  inputMatrix->LoadFullAffineMatrix(args.inputImage);
+  inputMatrix->LoadFullAffineMatrix(inputTransform);
   
   MatrixType::Pointer outputMatrix = MatrixType::New();
   inputMatrix->GetInverse(outputMatrix);
-  outputMatrix->SaveFullAffineMatrix(args.outputImage);
-  
+  outputMatrix->SaveFullAffineMatrix(outputTransform);
 }
