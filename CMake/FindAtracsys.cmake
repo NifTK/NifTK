9c1af14c0c47953f3f79285199c3714294c63d7a
Matt Clarkson <m.clarkson@ucl.ac.uk>
2017-05-18 10:12:18 +0100

    Issue #5070: Find Atracsys on Windows.

--- CMake/FindAtracsys.cmake
+++ CMake/FindAtracsys.cmake
@@ -17,25 +17,30 @@ set(Atracsys_FOUND 0)
 if(WIN32)
 
   find_path(Atracsys_INCLUDE_DIR
-    NAMES blah.h
-    PATHS "C:/Program Files/Atracsys/inc"
+    NAMES ftkInterface.h
+    PATHS "C:\\Program Files\\Atracsys\\Passive Tracking SDK\\include"
     )
 
   set (_lib_name)
   if(${CMAKE_GENERATOR} MATCHES "Win64")
-    set(_lib_name blah)
+    set(_lib_name fusionTrack64)
   else()
-    set(_lib_name blah)
+    set(_lib_name fusionTrack32)
   endif()
 
   find_library(Atracsys_LIBRARY
     NAMES ${_lib_name}
-    PATHS "C:/Program Files/Atracsys/lib"
+    PATHS "C:\\Program Files\\Atracsys\\Passive Tracking SDK\\lib"
   )
 
   if(Atracsys_INCLUDE_DIR AND Atracsys_LIBRARY)
     set(Atracsys_FOUND 1)
+    message("Found Atracsys: inc=${Atracsys_INCLUDE_DIR}")
+    message("Found Atracsys: lib=${Atracsys_LIBRARY}")
+  else()
+    message("Didn't find Atracsys: inc=${Atracsys_INCLUDE_DIR}")
+    message("Didn't find Atracsys: lib=${Atracsys_LIBRARY}")
   endif()
 
 endif(WIN32)
-set(Atracsys_FOUND 1)
+
