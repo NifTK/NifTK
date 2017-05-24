e9d2d460e1a10cca43d6c358bc81dab156683514
Matt Clarkson <m.clarkson@ucl.ac.uk>
2017-05-24 11:59:33 +0100

    Issue #5070: Add comment.

--- MITK/Modules/Atracsys/niftkAtracsysTracker.h
+++ MITK/Modules/Atracsys/niftkAtracsysTracker.h
@@ -28,6 +28,11 @@ class AtracsysTrackerPrivate;
  * \class AtracsysTracker
  * \brief Interface to Atracsys tracker that can be used
  * from either command line or GUI clients within NifTK.
+ *
+ * The Atracsys API provides a blocking call, that will wait
+ * until the next frame is available. Therefore, this class
+ * contains no threading. You are expected to instantiate this
+ * class, and then poll it at your desired rate.
  */
 class NIFTKATRACSYS_EXPORT AtracsysTracker : public niftk::IGITracker
 {
