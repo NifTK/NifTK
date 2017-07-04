aada0ceeb01cd6724d02d6302a2129796ef65937
Miklos Espak <m.espak@ucl.ac.uk>
2017-07-04 14:29:12 +0100

    Issue #5170: Refactorings

--- MITK/Modules/MIDAS/Algorithms/niftkMorphologicalSegmentorPipelineManager.cxx
+++ MITK/Modules/MIDAS/Algorithms/niftkMorphologicalSegmentorPipelineManager.cxx
@@ -281,6 +281,7 @@ void MorphologicalSegmentorPipelineManager::FinalizeSegmentation()
     this->DestroyPipeline(segmentationImage);
 
     segmentationNode->SetBoolProperty("midas.morph.finished", true);
+    segmentationNode->SetIntProperty("midas.morph.stage", 0);
 
     UpdateVolumeProperty(segmentationImage, segmentationNode);
   }
