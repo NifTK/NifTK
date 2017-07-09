c14bbc39dc21361894dee54be283f5cf08ab8427
Miklos Espak <m.espak@ucl.ac.uk>
2017-07-09 10:33:21 +0100

    Issue #5170: API naming tidying about orientations

--- MITK/Modules/MIDASGui/niftkBaseSegmentorController.cxx
+++ MITK/Modules/MIDASGui/niftkBaseSegmentorController.cxx
@@ -256,14 +256,14 @@ void BaseSegmentorController::ApplyDisplayOptions(mitk::DataNode* node)
 //-----------------------------------------------------------------------------
 int BaseSegmentorController::GetReferenceImageSliceAxis()
 {
-  int referenceImageAxis = -1;
+  int referenceImageSliceAxis = -1;
   const mitk::Image* referenceImage = this->GetReferenceImage();
   ImageOrientation orientation = this->GetOrientation();
   if (referenceImage && orientation != IMAGE_ORIENTATION_UNKNOWN)
   {
-    referenceImageAxis = GetThroughPlaneAxis(referenceImage, orientation);
+    referenceImageSliceAxis = GetThroughPlaneAxis(referenceImage, orientation);
   }
-  return referenceImageAxis;
+  return referenceImageSliceAxis;
 }
 
 
