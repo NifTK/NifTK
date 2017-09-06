cdacb20f2fdac61c48df0ffe63ee342353e46a6e
Stephen Thompson <s.thompson@ucl.ac.uk>
2017-09-06 14:45:59 +0100

    Issue #5208 added test for transformed rays

--- MITK/Modules/OpenCV/Testing/mitkProjectCameraRaysTest.cxx
+++ MITK/Modules/OpenCV/Testing/mitkProjectCameraRaysTest.cxx
@@ -20,6 +20,7 @@
 #include <mitkTestingMacros.h>
 #include <mitkLogMacros.h>
 #include <mitkProjectCameraRays.h>
+#include <mitkOpenCVMaths.h>
 #include <cmath>
 
 /**
@@ -36,14 +37,27 @@ int mitkProjectCameraRaysTest(int argc, char * argv[])
 
   projector->SetIntrinsicFileName(cameraCalibrationFile);
 
-
   int returnStatus = projector->Project();
-  if ( returnStatus ) 
-  {
-    projector->WriteOutput ( argv[2] );
-  }
+  MITK_TEST_CONDITION_REQUIRED ( returnStatus == true , "Testing that project returned true when no lens to world given." );
 
-  MITK_TEST_CONDITION_REQUIRED ( returnStatus == true , "Testing that project returned true" );
+  std::vector < std::pair < cv::Point3d , cv::Point3d > > rays = projector->GetRays();
+
+  MITK_TEST_CONDITION_REQUIRED ( mitk::NearlyEqual ( rays[0].first, cv::Point3d ( 0.0, 0.0, 0.0 ) ,1e-6 ) , "Testing first ray origin is zero" );
+  MITK_TEST_CONDITION_REQUIRED ( mitk::NearlyEqual ( rays[3].second , cv::Point3d (-241.78379777,
+          -139.74497300, 500 ), 1e-6 ), "Testing fourth ray tip position" );
+
+  projector->SetLensToWorldFileName(argv[2]);
+  returnStatus = projector->Project();
+  MITK_TEST_CONDITION_REQUIRED ( returnStatus == true , "Testing that project returned true when lens to world given" );
+  rays = projector->GetRays();
+  MITK_TEST_CONDITION_REQUIRED ( mitk::NearlyEqual ( rays[0].first, cv::Point3d ( -129.59307861 , 461.2430114746, -1999.4892578 )
+        ,1e-6 ) , "Testing first ray origin is at origin of transform" );
+  MITK_TEST_CONDITION_REQUIRED ( mitk::NearlyEqual ( rays[3].second , cv::Point3d (-4.67841779e+02, 1.35454289e+01, -1.88484736e+03 ), 1e-3 ), "Testing fourth ray tip position after transformation" <<  rays[3].second );
+
+  if ( returnStatus )
+  {
+    projector->WriteOutput ( argv[3] );
+  }
 
   MITK_TEST_END();
 }
