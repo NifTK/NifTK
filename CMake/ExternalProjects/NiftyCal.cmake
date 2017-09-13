204de9621434d2f16f7044c3e629fba06ec4651b
Matt Clarkson <m.clarkson@ucl.ac.uk>
2017-09-13 20:32:55 +0100

    Issue #5239: Update NiftyCal to fix include path issue in NiftyCal/52.

--- CMake/ExternalProjects/NiftyCal.cmake
+++ CMake/ExternalProjects/NiftyCal.cmake
@@ -24,7 +24,7 @@ endif()
 if(BUILD_NiftyIGI)
 
   set(location "https://cmiclab.cs.ucl.ac.uk/CMIC/NiftyCal.git")
-  set(version "140152428b")
+  set(version "fb84b236b1")
 
   niftkMacroDefineExternalProjectVariables(NiftyCal ${version} ${location})
   set(proj_DEPENDENCIES OpenCV Eigen AprilTags ITK)
