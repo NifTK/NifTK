cbb31779565cdde01be4a76a8693c8e2a2b9c289
Matt Clarkson <m.clarkson@ucl.ac.uk>
2017-10-12 12:36:13 +0100

    Issue #5272: Fix niftkExtractDataFromMITKScene to not load via SceneIO, and use niftkCore to load IO factories.

--- MITK/Applications/ExtractDataFromMITKScene/niftkExtractDataFromMITKScene.cxx
+++ MITK/Applications/ExtractDataFromMITKScene/niftkExtractDataFromMITKScene.cxx
@@ -1,72 +1,72 @@
 #include <iostream>
 #include <fstream>
 #include <typeinfo>
+#include <mitkVector.h>
 
 #include <mitkIOUtil.h>
-#include <mitkSceneIO.h>
+#include <mitkStandaloneDataStorage.h>
 #include <niftkExtractDataFromMITKSceneCLP.h>
-#include <itkNifTKImageIOFactory.h>
 
 int main(int argc, char **argv)
 {
-   PARSE_ARGS;
+  PARSE_ARGS;
 
-   int returnStatus = EXIT_FAILURE;
+  int returnStatus = EXIT_FAILURE;
 
-   try
-   {
-      if ( input.length() != 0 )
+  try
+  {
+    if ( input.length() != 0 )
+    {
+      // Read the specified MITK scene
+      mitk::StandaloneDataStorage::Pointer dataStorage = mitk::StandaloneDataStorage::New();
+      mitk::IOUtil::Load(input, *static_cast<mitk::DataStorage*>(dataStorage.GetPointer()));
+
+      // Extract and save the point nodes
+      mitk::DataStorage::SetOfObjects::ConstPointer nodes = dataStorage->GetAll();
+      for (unsigned int i = 0; i < nodes->size(); i++)
       {
-         // Read the specified MITK scene
-         mitk::SceneIO::Pointer sceneIO = mitk::SceneIO::New();
-         mitk::DataStorage::Pointer dataStorage = sceneIO->LoadScene(input);
-
-         // Extract and save the point nodes
-         mitk::DataStorage::SetOfObjects::ConstPointer nodes = dataStorage->GetAll();
-         for (unsigned int i = 0; i < nodes->size(); i++)
+         if(datatype.compare("pointset")==0)
          {
-            if(datatype.compare("pointset")==0)
+            mitk::PointSet::Pointer ps = dynamic_cast<mitk::PointSet *>(nodes->at(i)->GetData());
+            if (ps.IsNull())
             {
-               mitk::PointSet::Pointer ps = dynamic_cast<mitk::PointSet *>(nodes->at(i)->GetData());
-               if (ps.IsNull())
-               {
-                  continue;
-               }
-               std::string nodeName = nodes->at(i)->GetName();
-               std::cout << "Saving the point set named: " << nodeName << std::endl;
-
-               mitk::IOUtil::Save(ps, output + "_" + nodeName + ".mps");
+               continue;
             }
-            else if(datatype.compare("image")==0)
-            {
-               mitk::Image::Pointer img = dynamic_cast<mitk::Image *>(nodes->at(i)->GetData());
-               if (img.IsNull())
-               {
-                  continue;
-               }
-               std::string nodeName = nodes->at(i)->GetName();
-               std::cout << "Saving the image named: " << nodeName << std::endl;
+            std::string nodeName = nodes->at(i)->GetName();
+            std::cout << "Saving the point set named: " << nodeName << std::endl;
 
-               mitk::IOUtil::Save(img, output + "_" + nodeName + ".nii.gz");
-            }
-            else{
-               MITK_ERROR << "Unsupported datatype: " << datatype;
-               returnStatus = -3;
-
-            }
+            mitk::IOUtil::Save(ps, output + "_" + nodeName + ".mps");
          }
-         returnStatus = EXIT_SUCCESS;
+         else if(datatype.compare("image")==0)
+         {
+            mitk::Image::Pointer img = dynamic_cast<mitk::Image *>(nodes->at(i)->GetData());
+            if (img.IsNull())
+            {
+               continue;
+            }
+            std::string nodeName = nodes->at(i)->GetName();
+            std::cout << "Saving the image named: " << nodeName << std::endl;
+
+            mitk::IOUtil::Save(img, output + "_" + nodeName + ".nii.gz");
+         }
+         else
+         {
+            MITK_ERROR << "Unsupported datatype: " << datatype;
+            returnStatus = -3;
+         }
       }
-   }
-   catch (std::exception& e)
-   {
-     MITK_ERROR << "Caught std::exception:" << e.what();
-     returnStatus = -1;
-   }
-   catch (...)
-   {
-     MITK_ERROR << "Caught unknown exception:";
-     returnStatus = -2;
-   }
-   return returnStatus;
+      returnStatus = EXIT_SUCCESS;
+    }
+  }
+  catch (std::exception& e)
+  {
+    MITK_ERROR << "Caught std::exception:" << e.what();
+    returnStatus = -1;
+  }
+  catch (...)
+  {
+    MITK_ERROR << "Caught unknown exception:";
+    returnStatus = -2;
+  }
+  return returnStatus;
 }
